12b2b89a9d1663d09f92c4ac477de1b104a2f6cf
akashgond3112 <akashgond3112@gmail.com>
2025-05-27 17:09:48 +0100

    add BinaryTreeVerticalOrderTraversal implementation for vertical traversal of binary trees

---
+++ src/main/revision/meta/medium/BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,97 @@
+package main.revision.meta.medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.TreeMap;
+
+/**
+ * @author agond
+ * @date May 27, 2025
+ * @time 3:03:10 PM
+ */
+public class BinaryTreeVerticalOrderTraversal {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Tuple {
+
+        TreeNode node;
+        int row;
+        int col;
+
+        Tuple(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new Treemap<>();
+
+        Queue<Tuple> q = new LinkedList<Tuple>();
+
+        q.offer(new Tuple(root, 0, 0));
+
+        while (!q.isEmpty()) {
+            Tuple t = q.poll();
+            TreeNode node = t.node;
+            int x = t.row;
+            int y = t.col;
+
+            if (!map.containsKey(x)) {
+                map.put(x, new TreeMap<>());
+            }
+
+            if (!map.get(x).containsKey(y)) {
+                map.get(x).put(y, new PriorityQueue<>());
+            }
+
+            map.get(x).get(y).offer(node.val);
+
+            if (node.left != null) {
+                q.offer(new Tuple(node.left, x - 1, y + 1));
+            }
+
+            if (node.right != null) {
+                q.offer(new Tuple(node.right, x + 1, y + 1));
+            }
+        }
+
+        List<List<Integer>> list = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            list.add(new ArrayList<>());
+
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    list.get(list.size() - 1).add(nodes.poll());
+                }
+            }
+        }
+
+        return list;
+    }
+
+}
