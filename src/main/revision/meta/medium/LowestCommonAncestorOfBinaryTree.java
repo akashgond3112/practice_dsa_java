af1b90a2ce15fcb289614e6edd5737faede2fde4
akashgond3112 <akashgond3112@gmail.com>
2025-05-28 18:47:23 +0100

    refactor settings.json to enable detailed quick suggestions configuration; add LowestCommonAncestorOfBinaryTree implementation

---
+++ src/main/revision/meta/medium/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,39 @@
+package main.revision.meta.medium;
+
+/**
+ * @author agond
+ * @date May 27, 2025
+ * @time 6:44:11 PM
+ */
+public class LowestCommonAncestorOfBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+
+    }
+
+}
