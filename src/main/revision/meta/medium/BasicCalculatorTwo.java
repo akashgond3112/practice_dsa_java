61651d5504ee251c1625615c332011b395e331a9
akashgond3112 <akashgond3112@gmail.com>
2025-06-09 20:16:02 +0100

    add implementation of BasicCalculatorTwo to evaluate mathematical expressions

---
+++ src/main/revision/meta/medium/BasicCalculatorTwo.java
@@ -0,0 +1,54 @@
+/**
+ * @author agond
+ * @date Jun 09, 2025
+ * @time 7:27:03 PM
+ */
+package main.revision.meta.medium;
+
+import java.util.Stack;
+
+public class BasicCalculatorTwo {
+
+    // Stack Approach
+    public int calculate(String s) {
+        int currentNo = 0;
+        char lastOperation = '+';
+        char currentChar;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < s.length() - 1; i++) {
+
+            currentChar = s.charAt(i);
+
+            if (Character.isDigit(currentChar)) {
+
+                currentNo = (currentNo * 10) + (currentChar - '0');
+            }
+
+            if (!Character.isDigit(currentChar) && currentChar != ' ' || i == s.length() - 1) {
+
+                if (lastOperation == '-') {
+                    stack.push(-currentNo);
+                } else if (lastOperation == '+') {
+                    stack.push(currentNo);
+                } else if (lastOperation == '*') {
+                    int tmp = stack.pop();
+                    stack.push(tmp * currentNo);
+                } else if (lastOperation == '/') {
+                    int tmp = stack.pop();
+                    stack.push(tmp / currentNo);
+                }
+                lastOperation = currentChar;
+                currentNo = 0;
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+}
