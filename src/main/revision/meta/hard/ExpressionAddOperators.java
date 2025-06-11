69ed485aca04579f1bd2669a1e2da054887e8acd
akashgond3112 <akashgond3112@gmail.com>
2025-06-11 19:08:36 +0100

    add implementation of ExpressionAddOperators to evaluate expressions with added operators

--- src/main/revision/meta/hard/ExpressionAddOperators.java
+++ src/main/revision/meta/hard/ExpressionAddOperators.java
@@ -10,6 +10,63 @@ import java.util.*;
 public class ExpressionAddOperators {
 
     public List<String> addOperators(String num, int target) {
+
+        List<String> result = new ArrayList<>();
+        if (num == null || num.isEmpty()) {
+            return result;
+        }
+
+        backtrack(result, num, target, 0, "", 0, 0);
+
+        return result;
+    }
+
+    private void backtrack(List<String> result, String num, int target, int index, String expr, long evaluated,
+            long lastNumber) {
+
+        if (index == num.length()) {
+            if (evaluated == target) {
+                result.add(expr);
+            }
+            return;
+        }
+
+        for (int i = 0; i < num.length(); i++) {
+
+            if (i > index && num.charAt(i) == '0') {
+                break;
+            }
+
+            String curChar = num.substring(index, i + 1);
+            long curNum = Long.parseLong(curChar);
+
+            if (index == 0) {
+                backtrack(result, num, target, i + 1,
+                        curChar,
+                        curNum,
+                        curNum);
+            } else {
+                // Add
+                backtrack(result, num, target, i + 1,
+                        expr + '+' + curChar,
+                        evaluated + curNum,
+                        curNum);
+                // sub
+                backtrack(result, num, target, i + 1,
+                        expr + '-'
+                                + curChar,
+                        evaluated - curNum,
+                        -curNum);
+
+                // mul
+                backtrack(result, num, target, i + 1,
+                        expr + '*'
+                                + curChar,
+                        evaluated - lastNumber + (lastNumber * curNum),
+                        lastNumber * curNum);
+            }
+
+        }
     }
 
 }
