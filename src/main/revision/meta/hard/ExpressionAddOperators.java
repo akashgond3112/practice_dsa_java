e66ff99fba0240f749a218b377c882f6b20d7a12
akashgond3112 <akashgond3112@gmail.com>
2025-06-11 18:50:44 +0100

    add implementation of MergeIntervals to merge overlapping intervals

---
+++ src/main/revision/meta/hard/ExpressionAddOperators.java
@@ -0,0 +1,15 @@
+/**
+ * @author agond
+ * @date Jun 11, 2025
+ * @time 6:39:38 PM
+ */
+package main.revision.meta.hard;
+
+import java.util.*;
+
+public class ExpressionAddOperators {
+
+    public List<String> addOperators(String num, int target) {
+    }
+
+}
---
+++ src/main/revision/meta/medium/MergeIntervals.java
@@ -0,0 +1,40 @@
+/**
+ * @author agond
+ * @date Jun 11, 2025
+ * @time 6:02:01 PM
+ */
+package main.revision.meta.medium;
+
+import java.util.*;
+
+public class MergeIntervals {
+
+    public int[][] merge(int[][] intervals) {
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] newInterval = intervals[0];
+
+        result.add(newInterval);
+
+        for (int[] interval : intervals) {
+
+            if (interval[0] < newInterval[1]) {
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+
+            } else {
+                newInterval = interval;
+                result.add(newInterval);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+}
