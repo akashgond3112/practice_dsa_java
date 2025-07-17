b72424b86f9f4bed2f570db524a7fe7157cf2f78
akashgond3112 <akashgond3112@gmail.com>
2025-07-17 17:25:10 +0100

    Implement containsNearbyDuplicate method to check for nearby duplicates within a given range

--- src/main/revision/meta/easy/ContainsDuplicateII.java
+++ src/main/revision/meta/easy/ContainsDuplicateII.java
@@ -5,6 +5,28 @@
  */
 package main.revision.meta.easy;
 
+import java.util.*;
+
 public class ContainsDuplicateII {
 
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        int startIndex = 0;
+        int endIndex = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        while (endIndex < nums.length) {
+
+            if (!set.add(nums[endIndex])) {
+                return true;
+            }
+
+            if (set.size() >= k) {
+                set.remove(nums[startIndex++]);
+            }
+            endIndex++;
+        }
+        return false;
+    }
 }
