cb267bbc876d1ab07a15fb63180319ce84ea2186
akashgond3112 <akashgond3112@gmail.com>
2025-10-13 19:18:25 +0100

    Add DiameterOfBinaryTree class to calculate the diameter of a binary tree

---
+++ src/main/revision/october/meta/easy/DiameterOfBinaryTree.java
@@ -0,0 +1,32 @@
+package main.revision.october.meta.easy;
+
+import main.revision.october.meta.TreeNode;
+import java.util.*;
+
+public class DiameterOfBinaryTree {
+    public int diameterOfBinaryTree(TreeNode root) {
+        Map<TreeNode, Integer> map = new HashMap<>();
+        Stack<TreeNode> st = new Stack<>();
+        int diameter = 0;
+        if (root != null) {
+            st.push(root);
+        }
+        while (!st.isEmpty()) {
+            TreeNode node = st.peek();
+            if (node.left != null && !map.containsKey(node.left)) {
+                st.push(node.left);
+            } else if (node.right != null && !map.containsKey(node.right)) {
+                st.push(node.right);
+            } else {
+                int leftDepth = map.getOrDefault(node.left, 0);
+                int rightDepth = map.getOrDefault(node.right, 0);
+
+                map.put(node, 1 + Math.max(leftDepth, rightDepth));
+
+                diameter = Math.max(diameter, leftDepth + rightDepth);
+            }
+        }
+
+        return diameter;
+    }
+}
