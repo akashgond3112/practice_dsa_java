b0aaa01c6dba2d6959d510d939ab92dbc156da3d
akashgond3112 <akashgond3112@gmail.com>
2025-10-19 07:42:04 +0100

    Fix carry handling and improve string concatenation in addStrings method

--- src/main/revision/october/meta/easy/AddStrings.java
+++ src/main/revision/october/meta/easy/AddStrings.java
@@ -22,12 +22,49 @@ public class AddStrings {
 
             sum += carry;
 
-            sb.append(sum % 10 + '0');
+            sb.append(sum % 10);
             carry = sum / 10;
         }
 
         if (carry != null) {
-            sb.append(carry + '0');
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+
+    }
+
+    // Revised on 18.10.2025
+    public String addStrings_rev_1(String num1, String num2) {
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        StringBuilder sb = new StringBuilder();
+        Integer carry = null;
+
+        while (i >= 0 || j >= 0) {
+
+            int sum = 0;
+
+            if (i >= 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+
+            sum += carry;
+            sb.append(sum % 10);
+
+            carry = sum / 10;
+        }
+
+        if (carry > 0) {
+            sb.append(carry);
         }
 
         return sb.reverse().toString();
