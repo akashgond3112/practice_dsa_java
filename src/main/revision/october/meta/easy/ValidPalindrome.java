b23738f0a47a4535a14f99290e9aaf556b9b7ef4
akashgond3112 <akashgond3112@gmail.com>
2025-10-20 19:39:13 +0100

    Add revised method for palindrome validation in ValidPalindrome class

--- src/main/revision/october/meta/easy/ValidPalindrome.java
+++ src/main/revision/october/meta/easy/ValidPalindrome.java
@@ -26,4 +26,28 @@ public class ValidPalindrome {
 
         return true;
     }
+
+    // Revised on 18.10.2025
+    private boolean isValidPalindromeRev_2(String s, int left, int right) {
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
