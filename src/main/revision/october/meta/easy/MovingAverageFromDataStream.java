3e10a2714ea807f231307912abc3a864d99523c2
akashgond3112 <akashgond3112@gmail.com>
2025-10-20 19:38:48 +0100

    Add detailed documentation and a revision method to MovingAverageFromDataStream class

--- src/main/revision/october/meta/easy/MovingAverageFromDataStream.java
+++ src/main/revision/october/meta/easy/MovingAverageFromDataStream.java
@@ -2,6 +2,23 @@ package main.revision.october.meta.easy;
 
 import java.util.*;
 
+/**
+ * MovingAverageFromDataStream maintains a moving average of the last 'size' integers added.
+ * It uses a queue to store the window of elements and efficiently updates the sum as new elements arrive.
+ *
+ * Time Complexity:
+ *   - nextInt(int val): O(1) per operation (queue operations and sum update are constant time)
+ *
+ * Space Complexity:
+ *   - O(size), where 'size' is the window size (queue stores up to 'size' elements)
+ *
+ * Example usage:
+ *   MovingAverageFromDataStream ma = new MovingAverageFromDataStream(3);
+ *   ma.nextInt(1); // returns average of [1]
+ *   ma.nextInt(10); // returns average of [1, 10]
+ *   ma.nextInt(3); // returns average of [1, 10, 3]
+ *   ma.nextInt(5); // returns average of [10, 3, 5]
+ */
 public class MovingAverageFromDataStream {
 
     int size;
@@ -27,4 +44,18 @@ public class MovingAverageFromDataStream {
         return windowSum * 1.0 / size;
     }
 
+    // Revision 19/10/2025
+    double nextIntRevisionThirdrdDay(int val) {
+
+        q.offer(val);
+        windowSum += val;
+
+        if (q.size() > size) {
+            int cur = q.poll();
+
+            windowSum -= cur;
+        }
+
+        return windowSum * 1.0 / size;
+    }
 }
