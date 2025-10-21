18f9e5be34ef7ef0b47c646ca27cac429bbee8e9
akashgond3112 <akashgond3112@gmail.com>
2025-10-21 19:14:45 +0100

    Refactor removeDuplicates method and add a revised version for clarity

--- src/main/revision/october/meta/easy/RemoveAllAdjacentDuplicatesInString.java
+++ src/main/revision/october/meta/easy/RemoveAllAdjacentDuplicatesInString.java
@@ -1,7 +1,7 @@
 package main.revision.october.meta.easy;
 
 public class RemoveAllAdjacentDuplicatesInString {
-    
+
     public String removeDuplicates(String s) {
 
         StringBuilder stack = new StringBuilder();
@@ -15,5 +15,21 @@ public class RemoveAllAdjacentDuplicatesInString {
         }
         return stack.toString();
 
+    }
+
+    // Revision on 20/10/2025
+    public String removeDuplicatesRevisionThirdDay(String s) {
+        StringBuilder stack = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            int len = stack.length();
+
+            if (!stack.isEmpty() && stack.charAt(len - 1) == c) {
+                stack.deleteCharAt(len - 1);
+            } else
+                stack.append(c);
+        }
+
+        return stack.toString();
     }
 }
