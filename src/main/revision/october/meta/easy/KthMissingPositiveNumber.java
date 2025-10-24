6d2ff574269a39f27537286a5942b1356be4f322
akashgond3112 <akashgond3112@gmail.com>
2025-10-24 12:47:36 +0100

    Add KthMissingPositiveNumber class with method to find the k-th missing positive integer

---
+++ src/main/revision/october/meta/easy/KthMissingPositiveNumber.java
@@ -0,0 +1,25 @@
+package main.revision.october.meta.easy;
+
+public class KthMissingPositiveNumber {
+
+    class Solution {
+        public int findKthPositive(int[] arr, int k) {
+
+            int left = 0;
+            int right = arr.length;
+
+            while (left < right) {
+
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] - mid - 1 < k) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            return left + k;
+        }
+    }
+}
