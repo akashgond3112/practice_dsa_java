170e8b3efb62258c766777ee523b6dfeaf2d3999
akashgond3112 <akashgond3112@gmail.com>
2025-10-27 18:19:41 +0000

    Add ClosestBinarySearchTreeValue class with method to find the closest value to a target in a BST

---
+++ src/main/revision/october/meta/easy/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,41 @@
+package main.revision.october.meta.easy;
+
+import main.revision.meta.TreeNode;
+
+public class ClosestBinarySearchTreeValue {
+
+    public class Solution {
+
+        public int closestValue(TreeNode root, double target) {
+
+            double minDifference = Double.MAX_VALUE;
+
+            int closestValue = root.val;
+            TreeNode currentNode = root;
+
+            while (root != null) {
+
+                double currentDifference = Math.abs(currentNode.val - target);
+
+                if (currentDifference < minDifference ||
+                        (currentDifference == minDifference && currentNode.val < closestValue)) {
+                    minDifference = currentDifference;
+                    closestValue = currentNode.val;
+                }
+
+                // Decide which subtree to explore next
+                if (currentNode.val > target) {
+                    // If current value is greater than target, go left
+                    currentNode = currentNode.left;
+                } else {
+                    // If current value is less than or equal to target, go right
+                    currentNode = currentNode.right;
+                }
+
+            }
+
+            return closestValue;
+        }
+    }
+
+}
