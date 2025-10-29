5bb9d214275d4b1e9e2927ff1f192275fb42c0e8
akashgond3112 <akashgond3112@gmail.com>
2025-10-29 16:00:31 +0000

    Refactor DotProductOfTwoSparseVectors class to improve structure and fix method naming

--- src/main/revision/october/meta/easy/DotProductOfTwoSparseVectors.java
+++ src/main/revision/october/meta/easy/DotProductOfTwoSparseVectors.java
@@ -6,39 +6,83 @@ import main.revision.october.meta.Pair;
 
 public class DotProductOfTwoSparseVectors {
 
-    private final List<Pair<Integer, Integer>> nonZeroes;
+    public class Solution {
+        private final List<Pair<Integer, Integer>> nonZeroes;
 
-    DotProductOfTwoSparseVectors(int[] nums) {
-        nonZeroes = new ArrayList<>();
+        Solution(int[] nums) {
+            nonZeroes = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nonZeroes.add(new Pair<Integer, Integer>(i, nums[i]));
-            }
-        }
-    }
-
-    public int dotProdcut(DotProductOfTwoSparseVectors vec) {
-        int result = 0;
-
-        int p1 = 0;
-        int p2 = 0;
-
-        while (p1 < nonZeroes.size() && p2 < vec.nonZeroes.size()) {
-            Pair<Integer, Integer> pair_1 = nonZeroes.get(p1);
-            Pair<Integer, Integer> pair_2 = vec.nonZeroes.get(p2);
-
-            if (pair_1.getKey().equals(pair_2.getKey())) {
-                result += pair_1.getValue() + pair_2.getValue();
-                p1++;
-                p2++;
-            } else if (pair_1.getKey() < pair_2.getKey()) {
-                p1++;
-            } else {
-                p2++;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    nonZeroes.add(new Pair<Integer, Integer>(i, nums[i]));
+                }
             }
         }
 
-        return result;
+        public int dotProdcut(Solution vec) {
+            int result = 0;
+
+            int p1 = 0;
+            int p2 = 0;
+
+            while (p1 < nonZeroes.size() && p2 < vec.nonZeroes.size()) {
+                Pair<Integer, Integer> pair_1 = nonZeroes.get(p1);
+                Pair<Integer, Integer> pair_2 = vec.nonZeroes.get(p2);
+
+                if (pair_1.getKey().equals(pair_2.getKey())) {
+                    result += pair_1.getValue() + pair_2.getValue();
+                    p1++;
+                    p2++;
+                } else if (pair_1.getKey() < pair_2.getKey()) {
+                    p1++;
+                } else {
+                    p2++;
+                }
+            }
+
+            return result;
+        }
     }
+
+    // Revised on 29/10/2025
+    public class SolutionRevisionFourteenDay {
+        private final List<Pair<Integer, Integer>> nonZeroes;
+
+        SolutionRevisionFourteenDay(int[] nums) {
+            nonZeroes = new ArrayList<>();
+
+            for (int i = 0; i < nums.length; i++) {
+
+                if (nums[i] != 0) {
+                    nonZeroes.add(new Pair<>(i, nums[i]));
+                }
+            }
+        }
+
+        public int dotProduct(SolutionRevisionFourteenDay vec) {
+            int result = 0;
+
+            int p1 = 0;
+            int p2 = 0;
+
+            while (p1 < nonZeroes.size() && p2 < vec.nonZeroes.size()) {
+                Pair<Integer, Integer> pair_1 = nonZeroes.get(p1);
+                Pair<Integer, Integer> pair_2 = nonZeroes.get(p2);
+
+                if (pair_1.getKey().equals(pair_1.getKey())) {
+                    result += pair_1.getValue() + pair_2.getValue();
+                    p1++;
+                    p2++;
+                } else if (pair_1.getKey() < pair_2.getKey()) {
+                    p1++;
+                } else {
+                    p2++;
+                }
+            }
+
+            return result;
+        }
+
+    }
+
 }
