583e5f6c806e30d0a65415f110bacd140667a8d6
akashgond3112 <akashgond3112@gmail.com>
2025-10-31 11:35:38 +0000

    Add Hotel class to represent hotel details with ID, name, and price per night

---
+++ src/main/interview/expedia/hotelavailabilityfiltersystem/Hotel.java
@@ -0,0 +1,13 @@
+package main.interview.expedia.hotelavailabilityfiltersystem;
+
+public class Hotel {
+    String id;
+    String name;
+    double pricePerNight;
+
+    public Hotel(String id, String name, double pricePerNight) {
+        this.id = id;
+        this.name = name;
+        this.pricePerNight = pricePerNight;
+    }
+}
