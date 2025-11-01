17c4a9667579a7630c2c35dd8bca4c4b2f65baf3
akashgond3112 <akashgond3112@gmail.com>
2025-11-01 09:09:59 +0000

    Add MoveZeroes class to implement algorithm for moving zeros in an array

---
+++ src/main/revision/october/meta/easy/MoveZeroes.java
@@ -0,0 +1,19 @@
+package main.revision.october.meta.easy;
+
+public class MoveZeroes {
+
+    class Solution {
+        public void moveZeroes(int[] nums) {
+            int left = 0;
+
+            for (int right = 0; right < nums.length; right++) {
+                if (nums[right] != 0) {
+                    int temp = nums[right];
+                    nums[right] = nums[left];
+                    nums[left] = temp;
+                    left++;
+                }
+            }
+        }
+    }
+}
