c8c562e5db615fb40702b6e595f58917af73befd
akashgond3112 <akashgond3112@gmail.com>
2025-11-01 09:10:08 +0000

    Fix revision dates in ValidWordAbbreviation class and improve formatting

--- src/main/revision/october/meta/easy/ValidWordAbbreviation.java
+++ src/main/revision/october/meta/easy/ValidWordAbbreviation.java
@@ -34,7 +34,7 @@ public class ValidWordAbbreviation {
 
     }
 
-    // Revised on 12.10.2025
+    // Revised on 11.10.2025
     public boolean isValid_Rev_2(String word, String abbr) {
         int a = 0;
         int w = 0;
@@ -62,6 +62,38 @@ public class ValidWordAbbreviation {
             w += skip;
         }
 
-        return w == word.length() && a== abbr.length();
+        return w == word.length() && a == abbr.length();
+    }
+
+    // Revised on 31.10.2025
+    public boolean isValid_Rev_4(String word, String abbr) {
+
+        int a = 0;
+        int w = 0;
+
+        if (w < word.length() && a < abbr.length()) {
+
+            if (word.charAt(w) == abbr.charAt(a)) {
+                a++;
+                w++;
+            }
+
+            if (abbr.charAt(a) == '0') {
+                return false;
+            }
+
+            if (!Character.isDigit(abbr.charAt(a)) && word.charAt(w) != abbr.charAt(a)) {
+                return false;
+            }
+
+            int skip = 0;
+            if (a < abbr.length() && Character.isDigit(abbr.charAt(a))) {
+                skip = skip * 10 + (abbr.charAt(a) - '0');
+            }
+
+            w += skip;
+        }
+
+        return w == word.length() && a == abbr.length();
     }
 }
