91471536aa2cfba41d2c8a199e123a5408e7bae9
akashgond3112 <akashgond3112@gmail.com>
2025-11-02 12:33:19 +0000

    Enhance Node and TreeNode classes by adding next and prev pointers for improved traversal

--- src/main/revision/october/meta/TreeNode.java
+++ src/main/revision/october/meta/TreeNode.java
@@ -1,9 +1,13 @@
 package main.revision.october.meta;
 
 public class TreeNode {
+    public int key;
     public int val;
     public TreeNode left;
     public TreeNode right;
+
+    public TreeNode next;
+    public TreeNode prev;
 
     TreeNode() {
     }
@@ -16,5 +20,20 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public TreeNode(int key, int value) {
+        this.key = key;
+        this.val = value;
+        this.next = null;
+        this.prev = null;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right, TreeNode next, TreeNode prev) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        this.next = next;
+        this.prev = prev;
     }
 }
