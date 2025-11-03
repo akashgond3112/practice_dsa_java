83f6a40b638a102bad0b1ea63ddeb67b3769ed8d
akashgond3112 <akashgond3112@gmail.com>
2025-11-03 19:45:15 +0000

    Add SolutionRevisionSeventhDay class to implement right side view of a binary tree

--- src/main/revision/october/meta/medium/BinaryTreeRightSideView.java
+++ src/main/revision/october/meta/medium/BinaryTreeRightSideView.java
@@ -72,4 +72,41 @@ public class BinaryTreeRightSideView {
         }
     }
 
+    // Revised on 31/10/2025
+    class SolutionRevisionSeventhDay {
+        public List<Integer> rightSideView(TreeNode root) {
+
+            List<Integer> res = new ArrayList<>();
+
+            if (root == null) {
+                return res;
+            }
+
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(root);
+
+            while (!q.isEmpty()) {
+
+                int levelSize = q.size();
+                int lastVal = 0;
+
+                for (int i = 0; i < levelSize; i++) {
+
+                    TreeNode node = q.poll();
+                    lastVal += node.val;
+
+                    if (node.left != null) {
+                        q.offer(node.left);
+                    } else if (node.right != null) {
+                        q.offer(node.right);
+                    }
+
+                    res.add(lastVal);
+                }
+            }
+
+            return res;
+        }
+    }
+
 }
