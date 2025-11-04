de4ee08956331412422712e68978ee77d5cce673
akashgond3112 <akashgond3112@gmail.com>
2025-11-04 20:40:47 +0000

    Refactor StrobogrammaticNumber class to consolidate map initialization and improve code structure

--- src/main/revision/october/meta/easy/StrobogrammaticNumber.java
+++ src/main/revision/october/meta/easy/StrobogrammaticNumber.java
@@ -3,31 +3,63 @@ package main.revision.october.meta.easy;
 import java.util.HashMap;
 
 public class StrobogrammaticNumber {
-    public boolean isStrobogrammatic(String s) {
-        HashMap<Character, Character> map = new HashMap<>();
+
+    HashMap<Character, Character> map = new HashMap<>();
+
+    StrobogrammaticNumber() {
         map.put('0', '0');
         map.put('1', '1');
         map.put('8', '8');
         map.put('6', '9');
         map.put('9', '6');
+    }
 
-        if (s.length() == 1) {
-            return "0".equals(s) || "1".equals(s) || "8".equals(s);
-        }
+    public class Solution {
+        public boolean isStrobogrammatic(String s) {
 
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-
-            if (map.containsKey(s.charAt(left)) && map.get(s.charAt(left)) != (s.charAt(right))) {
-                return false;
+            if (s.length() == 1) {
+                return "0".equals(s) || "1".equals(s) || "8".equals(s);
             }
 
-            left++;
-            right--;
-        }
+            int left = 0;
+            int right = s.length() - 1;
 
-        return true;
+            while (left < right) {
+
+                if (map.containsKey(s.charAt(left)) && map.get(s.charAt(left)) != (s.charAt(right))) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
     }
+
+    // revised on 04/11/2025
+    public class SolutionRevisedOnThirdDay {
+        public boolean isStrobogrammatic(String s) {
+
+            if (s.length() == 1) {
+                return "0".equals(s) || "1".equals(s) || "8".equals(s);
+            }
+
+            int left = 0;
+            int right = s.length() - 1;
+
+            while (left < right) {
+
+                if (map.containsKey(s.charAt(left)) && map.get(s.charAt(left)) != (s.charAt(right))) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+
 }
