153a1feef13ad1fc795316b1666f9e3d0946be4e
akashgond3112 <akashgond3112@gmail.com>
2025-11-04 20:40:24 +0000

    Add diameterOfBinaryTreeRevisionFourteenDay method to calculate the diameter of a binary tree

--- src/main/revision/october/meta/easy/DiameterOfBinaryTree.java
+++ src/main/revision/october/meta/easy/DiameterOfBinaryTree.java
@@ -60,4 +60,36 @@ public class DiameterOfBinaryTree {
 
         return diameter;
     }
+
+    // Revision on 04/11/2025
+    public int diameterOfBinaryTreeRevisionFourteenDay(TreeNode root) {
+
+        Map<TreeNode, Integer> map = new HashMap<>();
+        Stack<TreeNode> st = new Stack<>();
+        int diameter = 0;
+
+        if (root != null) {
+            st.push(root);
+        }
+
+        while (!st.isEmpty()) {
+
+            TreeNode node = st.peek();
+
+            if (node.left != null && !map.containsKey(node.left)) {
+                st.push(node.left);
+            } else if (node.right != null && !map.containsKey(node.right)) {
+                st.push(node.right);
+            } else {
+                st.pop();
+                int leftDepth = map.getOrDefault(node.left, 0);
+                int rightDepth = map.getOrDefault(node.right, 0);
+
+                map.put(node, 1 + Math.max(leftDepth, rightDepth));
+                diameter = Math.max(diameter, leftDepth + rightDepth);
+            }
+        }
+
+        return diameter;
+    }
 }
