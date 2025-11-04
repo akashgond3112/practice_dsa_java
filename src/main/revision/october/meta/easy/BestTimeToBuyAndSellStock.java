d7fab893e72d25b8108af528e6501c3ca87007c2
akashgond3112 <akashgond3112@gmail.com>
2025-11-04 17:58:43 +0000

    Add BestTimeToBuyAndSellStock class to implement a solution for maximizing profit from stock prices

---
+++ src/main/revision/october/meta/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+package main.revision.october.meta.easy;
+
+public class BestTimeToBuyAndSellStock {
+    class Solution {
+        public int maxProfit(int[] prices) {
+
+            int min = prices[0];
+            int maxProfit = 0;
+            int n = prices.length;
+
+            for (int i = 1; i < n; i++) {
+                int cost = prices[i] - min;
+                maxProfit = Math.max(maxProfit, cost);
+                min = Math.min(min, prices[i]);
+            }
+
+            return maxProfit;
+        }
+    }
+}
