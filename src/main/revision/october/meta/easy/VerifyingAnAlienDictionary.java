3d8c7027603dc5a0f84cf47cc10a28c9cc8f9611
akashgond3112 <akashgond3112@gmail.com>
2025-11-04 20:40:58 +0000

    Add SolutionRevisionSeventhDay class to implement a revised alien dictionary sorting method

--- src/main/revision/october/meta/easy/VerifyingAnAlienDictionary.java
+++ src/main/revision/october/meta/easy/VerifyingAnAlienDictionary.java
@@ -82,4 +82,46 @@ public class VerifyingAnAlienDictionary {
             return true;
         }
     }
+
+    // revised on 04/11/2025
+    class SolutionRevisionSeventhDay {
+        public boolean isAlienSorted(String[] words, String order) {
+
+            Map<Character, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < order.length(); i++) {
+                map.put(order.charAt(i), i);
+            }
+
+            for (int j = 0; j < words.length; j++) {
+
+                String word1 = words[j];
+                String word2 = words[j + 1];
+
+                int minLength = Math.min(word1.length(), word2.length());
+
+                boolean differenceFound = false;
+
+                for (int k = 0; k < minLength; k++) {
+
+                    char c1 = word1.charAt(k);
+                    char c2 = word2.charAt(k);
+
+                    if (c1 != c2) {
+                        if (map.get(c1) > map.get(c2))
+                            return false;
+                    }
+                    differenceFound = true;
+                    break;
+
+                }
+
+                if (!differenceFound && word1.length() > word2.length()) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
