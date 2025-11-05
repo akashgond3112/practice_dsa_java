aa341928cc90ee529f4e954c9307f0e3b22886e5
akashgond3112 <akashgond3112@gmail.com>
2025-11-05 20:06:25 +0000

    Add SolutionRevisonThirdDay class to implement a revised copyRandomList method with proper random pointer handling

--- src/main/revision/october/meta/medium/CopyListWithRandomPointer.java
+++ src/main/revision/october/meta/medium/CopyListWithRandomPointer.java
@@ -1,6 +1,5 @@
 package main.revision.october.meta.medium;
 
-import main.revision.meta.TreeNode;
 import main.revision.october.meta.Node;
 
 public class CopyListWithRandomPointer {
@@ -52,6 +51,55 @@ public class CopyListWithRandomPointer {
             }
 
             return dummy.next; // Return head of copy list
+        }
+    }
+
+    // revised on 04/11/2025
+    class SolutionRevisonThirdDay {
+        public Node copyRandomList(Node head) {
+
+            // Base case: empty list
+            if (head == null)
+                return null;
+
+            Node current = head;
+            while (current != null) {
+
+                Node next = current.next;
+                Node copyNode = new Node(current.val);
+
+                copyNode.next = next;
+                current.next = copyNode;
+                current = next;
+
+            }
+
+            current = head;
+            while (current != null) {
+
+                Node copyNode = current.next;
+                if (current.random != null) {
+                    copyNode.random = current.random.next;
+                } else {
+                    copyNode.random = null;
+                }
+                current = current.next.next;
+            }
+
+            Node dummy = new Node(-1);
+            Node result = dummy;
+            current = head;
+
+            while (current != null) {
+
+                result.next = current.next;
+                result = result.next;
+
+                current.next = current.next.next;
+                current = current.next;
+            }
+
+            return dummy.next;
         }
     }
 }
