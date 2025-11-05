cd6b786eced1530385e778fa778ffd1784f19b60
akashgond3112 <akashgond3112@gmail.com>
2025-11-05 20:06:32 +0000

    Add SolutionRevisionThirdDay class to implement a revised customSortString method with improved character counting

--- src/main/revision/october/meta/medium/CustomSortString.java
+++ src/main/revision/october/meta/medium/CustomSortString.java
@@ -35,4 +35,40 @@ public class CustomSortString {
             return sb.toString();
         }
     }
+
+    // revised on 03/11/2025
+    class SolutionRevisionThirdDay {
+        public String customSortString(String order, String s) {
+
+            Map<Character, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char c : order.toCharArray()) {
+
+                while (map.containsKey(c) && map.get(c) > 0) {
+                    sb.append(c);
+                    map.put(c, map.get(c) - 1);
+                }
+            }
+
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+                char c = entry.getKey();
+                int count = entry.getValue();
+
+                while (count > 0) {
+                    sb.append(c);
+                    count--;
+                }
+            }
+
+            return sb.toString();
+        }
+    }
 }
