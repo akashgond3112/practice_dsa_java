d03ef696ca76460500c87506479bdf02126a880f
akashgond3112 <akashgond3112@gmail.com>
2025-11-05 20:06:39 +0000

    Add SolutionRevisionThirdDay class to implement a revised findPeakElement method with improved binary search logic

--- src/main/revision/october/meta/medium/FindPeakElement.java
+++ src/main/revision/october/meta/medium/FindPeakElement.java
@@ -37,4 +37,63 @@ public class FindPeakElement {
             return left;
         }
     }
+
+    // revised on 29/10/2025
+    static class SolutionRevisionThirdDay {
+        /**
+         * Finds a peak element in the given array. A peak element is an element that is
+         * greater than its neighbors.
+         *
+         * @param nums the input array of integers
+         * @return the index of a peak element, or -1 if the array is null or empty
+         */
+        public int findPeakElement(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return -1; // Or throw an exception, depending on contract.
+            }
+
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < nums[mid + 1]) {
+                    // The peak is in the right half (including mid+1)
+                    left = mid + 1;
+                } else {
+                    // The peak is in the left half (including mid)
+                    right = mid;
+                }
+            }
+            // When the loop terminates, left == right, pointing to a peak element.
+            return left;
+        }
+    }
+
+    // revised on 04/11/2025
+    static class SolutionRevisionSeventhDay {
+
+        public int findPeakElement(int[] nums) {
+
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] < nums[mid + 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+    }
 }
