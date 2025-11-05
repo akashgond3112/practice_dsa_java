d3d9fa36549c7e8ad2ce8e3ae1c9c2403f4c7ed8
akashgond3112 <akashgond3112@gmail.com>
2025-11-05 20:07:03 +0000

    Add LowestCommonAncestorOfBinaryTree class to implement methods for finding the lowest common ancestor in a binary tree

---
+++ src/main/revision/october/meta/medium/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,44 @@
+package main.revision.october.meta.medium;
+
+import main.revision.october.meta.TreeNode;
+
+public class LowestCommonAncestorOfBinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+
+    // Date 15/10/2025
+    public TreeNode lowestCommonAncestorRevOne(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || p == null || q == null) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestorRevOne(root.left, p, q);
+
+        TreeNode right = lowestCommonAncestorRevOne(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+}
