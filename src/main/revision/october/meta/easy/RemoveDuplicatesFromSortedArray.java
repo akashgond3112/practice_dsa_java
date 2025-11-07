11c76ee04f7fc2438c721073a99a3f8aff3e1c47
akashgond3112 <akashgond3112@gmail.com>
2025-11-07 20:01:03 +0000

    Add RemoveDuplicatesFromSortedArray class to implement method for removing duplicates from a sorted array

---
+++ src/main/revision/october/meta/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,20 @@
+package main.revision.october.meta.easy;
+
+public class RemoveDuplicatesFromSortedArray {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+
+            int i = 0;
+
+            for (int j = 1; j < nums.length; j++) {
+
+                if (nums[j] != nums[i]) {
+                    nums[i + 1] = nums[j];
+                    i++;
+                }
+            }
+
+            return i + 1;
+        }
+    }
+}
