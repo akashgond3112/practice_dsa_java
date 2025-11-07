10f35c830a2f7f16ee9d01786031d7648734b316
akashgond3112 <akashgond3112@gmail.com>
2025-11-07 20:00:55 +0000

    Refactor SolutionrevisionOnThirdDay class to improve maxProfit method implementation

--- src/main/revision/october/meta/easy/BestTimeToBuyAndSellStock.java
+++ src/main/revision/october/meta/easy/BestTimeToBuyAndSellStock.java
@@ -17,4 +17,22 @@ public class BestTimeToBuyAndSellStock {
             return maxProfit;
         }
     }
+
+    // revised on 06/11/2025
+    class SolutionrevisionOnThirdDay {
+        public int maxProfit(int[] prices) {
+            int min = prices[0];
+            int max = 0;
+            int n = prices.length;
+
+            for (int i = 1; i < n; i++) {
+
+                int cost = prices[i] - min;
+                max = Math.max(max, cost);
+                min = Math.min(min, prices[i]);
+            }
+
+            return max;
+        }
+    }
 }
