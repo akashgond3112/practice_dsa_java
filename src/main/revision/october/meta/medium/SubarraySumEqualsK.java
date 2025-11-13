e04dc5a3539478a9872d0e8debfee16bd821134f
akashgond3112 <akashgond3112@gmail.com>
2025-11-13 17:05:55 +0000

    Add SubarraySumEqualsK class with implementations for counting continuous subarrays that sum to k

---
+++ src/main/revision/october/meta/medium/SubarraySumEqualsK.java
@@ -0,0 +1,81 @@
+package main.revision.october.meta.medium;
+
+import java.util.*;
+
+public class SubarraySumEqualsK {
+    /**
+     * Returns the count of continuous subarrays whose sum equals to k.
+     *
+     * @param nums the input array of integers
+     * @param k    the target sum for subarrays
+     * @return the number of continuous subarrays that sum to k
+     */
+    public int subarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int count = 0;
+        int prefixSum = 0;
+
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    // Revision on 20/10/2025
+    public int subarraySumRevisionSeventhDay(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    // Revision on 04/11/2025
+    public int subarraySumRevisionFourteenDay(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+}
