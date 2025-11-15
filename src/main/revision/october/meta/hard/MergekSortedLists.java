77e4b8735af63d4716138e4f506c22c18a09dbe1
akashgond3112 <akashgond3112@gmail.com>
2025-11-15 08:37:34 +0000

    Revise mergeKLists methods for consistency and clarity in implementation

--- src/main/revision/october/meta/hard/MergekSortedLists.java
+++ src/main/revision/october/meta/hard/MergekSortedLists.java
@@ -38,7 +38,7 @@ public class MergekSortedLists {
 
     }
 
-    // Revised on 08.10.2025
+    // Revised on 07.10.2025
     public ListNode mergeKLists_Rev_1(ListNode[] list) {
 
         if (list == null || list.length == 0) {
@@ -71,5 +71,72 @@ public class MergekSortedLists {
         }
 
         return node.next;
+    }
+
+    // Revised on 13.10.2025
+    public ListNode mergeKLists_Rev_2(ListNode[] list) {
+
+        if (list == null || list.length == 0) {
+            return null;
+        }
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+
+        for (ListNode l : list) {
+
+            if (l != null) {
+                pq.offer(l);
+            }
+        }
+
+        ListNode node = new ListNode(0);
+        ListNode cur = node;
+
+        while (!pq.isEmpty()) {
+
+            ListNode curNode = pq.poll();
+
+            cur.next = curNode;
+            cur = cur.next;
+
+            if (curNode.next != null) {
+                pq.offer(curNode.next);
+            }
+        }
+
+        return node.next;
+    }
+
+    // Revised on 27.10.2025
+    public ListNode mergeKLists_Rev_3(ListNode[] list) {
+
+        if (list == null || list.length == 0)
+            return null;
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+
+        for (ListNode node : list) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode curNode = new ListNode(0);
+        ListNode cur = curNode.next;
+
+        while (!pq.isEmpty()) {
+
+            ListNode node = pq.poll();
+
+            cur.next = node;
+            cur = cur.next;
+
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+        }
+
+        return curNode.next;
+
     }
 }
