e53daab2f001f6b801fd4c1053b814199ebf2d9b
akashgond3112 <akashgond3112@gmail.com>
2025-11-16 11:52:23 +0000

    Add SolutionRevisionFourteenDay class with binary search implementation for finding k-th missing positive number

--- src/main/revision/october/meta/easy/KthMissingPositiveNumber.java
+++ src/main/revision/october/meta/easy/KthMissingPositiveNumber.java
@@ -93,4 +93,25 @@ public class KthMissingPositiveNumber {
             return left + k;
         }
     }
+
+    // Revision 11/14/2025
+    class SolutionRevisionFourteenDay {
+        public int findKthPositive(int[] arr, int k) {
+
+            int left = 0;
+            int right = arr.length;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] - mid - 1 < k) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            return left + k;
+        }
+    }
 }
