65683a6a75d7a7d6154452745f071ec6201319b6
akashgond3112 <akashgond3112@gmail.com>
2025-11-16 11:52:08 +0000

    Add addStringsRevisionFourteenDay method for improved string addition logic

--- src/main/revision/october/meta/easy/AddStrings.java
+++ src/main/revision/october/meta/easy/AddStrings.java
@@ -108,4 +108,40 @@ public class AddStrings {
         return sb.reverse().toString();
 
     }
+
+    // Revised on 11/7/2025
+    public String addStringsRevisionFourteenDay(String num1, String num2) {
+
+        int i = num1.length();
+        int j = num2.length();
+
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+
+        while (i >= 0 && j >= 0) {
+            int sum = 0;
+
+            if (i > 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+
+            if (j > 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+
+            sum += carry;
+            sb.append(sum % 10);
+            carry = sum / 10;
+
+        }
+
+        if (carry > 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+
+    }
 }
