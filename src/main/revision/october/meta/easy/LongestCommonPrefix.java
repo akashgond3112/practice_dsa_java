da182fe089804c12ed0bb3ff553495ce2e869f7c
akashgond3112 <akashgond3112@gmail.com>
2025-11-16 11:52:31 +0000

    Add SolutionRevisionFourteenDay class with updated longestCommonPrefix method implementation

--- src/main/revision/october/meta/easy/LongestCommonPrefix.java
+++ src/main/revision/october/meta/easy/LongestCommonPrefix.java
@@ -48,4 +48,28 @@ public class LongestCommonPrefix {
             return sb.toString();
         }
     }
+
+    // Revision on 11/15/2025
+    class SolutionRevisionFourteenDay {
+        public String longestCommonPrefix(String[] strs) {
+
+            Arrays.sort(strs);
+
+            char[] first = strs[0].toCharArray();
+            char[] last = strs[strs.length - 1].toCharArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < first.length; i++) {
+
+                if (first[i] == last[i]) {
+                    sb.append(first[i]);
+                } else {
+                    break;
+                }
+            }
+
+            return sb.toString();
+        }
+    }
 }
