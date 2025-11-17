c86dae2cff85f20dd3c041a9ab592788eb426913
akashgond3112 <akashgond3112@gmail.com>
2025-11-17 17:37:00 +0000

    Refactor removeDuplicates method for improved clarity and consistency in logic

--- src/main/revision/october/meta/easy/RemoveDuplicatesFromSortedArray.java
+++ src/main/revision/october/meta/easy/RemoveDuplicatesFromSortedArray.java
@@ -5,12 +5,25 @@ public class RemoveDuplicatesFromSortedArray {
         public int removeDuplicates(int[] nums) {
 
             int i = 0;
-
             for (int j = 1; j < nums.length; j++) {
-
-                if (nums[j] != nums[i]) {
-                    nums[i + 1] = nums[j];
+                if (nums[i] != nums[j]) {
                     i++;
+                    nums[i] = nums[j];
+                }
+            }
+
+            return i + 1;
+        }
+    }
+
+    // revised on 11/9/2025 and 11/15/2025
+    class SolutionRevisionThirdAndSeventhDay {
+        public int removeDuplicates(int[] nums) {
+            int i = 0;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] != nums[j]) {
+                    i++;
+                    nums[i] = nums[j];
                 }
             }
 
