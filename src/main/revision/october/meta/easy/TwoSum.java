fee208faa691ac8c0fe6f5b55e547dcbf412de7e
akashgond3112 <akashgond3112@gmail.com>
2025-11-18 17:38:04 +0000

    Add SolutionRevisionFourteenDay class with revised twoSum implementation for improved logic

--- src/main/revision/october/meta/easy/TwoSum.java
+++ src/main/revision/october/meta/easy/TwoSum.java
@@ -21,4 +21,25 @@ public class TwoSum {
             return new int[] {};
         }
     }
+
+    // Revised on 11/11/2025
+    class SolutionRevisionFourteenDay {
+        public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+
+                int num = nums[i];
+                int diff = target - num;
+
+                if (prevMap.containsKey(diff)) {
+                    return new int[] { prevMap.get(diff), i };
+                }
+
+                prevMap.put(diff, i);
+            }
+
+            return new int[] {};
+        }
+    }
 }
