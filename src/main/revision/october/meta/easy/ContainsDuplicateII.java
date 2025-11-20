a9e68a84bac3a2cea3aa2b66ca48ed34171d1e24
akashgond3112 <akashgond3112@gmail.com>
2025-11-20 15:39:03 +0000

    Add ContainsDuplicateII class to implement a solution for finding nearby duplicates in an array

---
+++ src/main/revision/october/meta/easy/ContainsDuplicateII.java
@@ -0,0 +1,29 @@
+package main.revision.october.meta.easy;
+
+import java.util.HashSet;
+
+public class ContainsDuplicateII {
+
+    class Solution {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+            int startIndex = 0;
+            int endIndex = 0;
+
+            HashSet<Integer> set = new HashSet<>();
+
+            while (endIndex < nums.length) {
+
+                if (!set.add(nums[endIndex])) {
+                    return true;
+                }
+
+                if (set.size() >= k) {
+                    set.remove(nums[startIndex++]);
+                }
+                endIndex++;
+            }
+            return false;
+        }
+    }
+}
