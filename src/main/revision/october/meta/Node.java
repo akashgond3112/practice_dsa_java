652db0d5087d55c0e585845f1ebacc50d9e7198f
akashgond3112 <akashgond3112@gmail.com>
2025-11-22 09:21:37 +0000

    Add neighbors field and constructors to Node class for graph representation

--- src/main/revision/october/meta/Node.java
+++ src/main/revision/october/meta/Node.java
@@ -1,5 +1,8 @@
 package main.revision.october.meta;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
     public int val;
     public Node left;
@@ -7,10 +10,22 @@ public class Node {
     public Node parent;
     public Node next;
     public Node random;
+    public List<Node> neighbors;
 
     public Node(int val) {
         this.val = val;
         this.next = null;
         this.random = null;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
     }
 }
