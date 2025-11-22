9651910fcccc0f76eb8f99c12d359c805fd1155c
akashgond3112 <akashgond3112@gmail.com>
2025-11-22 09:22:05 +0000

    Add SolutionRevisionSeventhDay class with complete customSortString implementation

--- src/main/revision/october/meta/medium/CustomSortString.java
+++ src/main/revision/october/meta/medium/CustomSortString.java
@@ -71,4 +71,40 @@ public class CustomSortString {
             return sb.toString();
         }
     }
+
+    // revised on 11/9/2025
+    class SolutionRevisionSeventhDay {
+        public String customSortString(String order, String s) {
+
+            Map<Character, Integer> map = new HashMap<>();
+
+            for (char c : s.toCharArray()) {
+
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char c : order.toCharArray()) {
+
+                while (map.containsKey(c) && map.get(c) > 0) {
+                    sb.append(c);
+                    map.put(c, map.get(c) - 1);
+                }
+            }
+
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+                char c = entry.getKey();
+                int count = entry.getValue();
+
+                while (count > 0) {
+                    sb.append(c);
+                    count--;
+                }
+            }
+
+            return sb.toString();
+        }
+    }
 }
