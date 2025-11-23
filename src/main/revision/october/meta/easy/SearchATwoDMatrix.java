f30e76b4a59863f6be493827453300550e38b392
akashgond3112 <akashgond3112@gmail.com>
2025-11-23 11:01:24 +0000

    Rename SolutionRevisionFourteenDay class to SolutionRevisionSeventhDay and adjust implementation order in TwoSum.java

---
+++ src/main/revision/october/meta/easy/SearchATwoDMatrix.java
@@ -0,0 +1,56 @@
+package main.revision.october.meta.easy;
+
+public class SearchATwoDMatrix {
+
+    class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+
+            int rowIndx = searchPotentialRow(matrix, target);
+            if (rowIndx != -1)
+                return binarySearchOverRow(rowIndx, matrix, target);
+            else {
+                return false;
+
+            }
+        }
+
+        private boolean binarySearchOverRow(int rowIndx, int[][] matrix, int target) {
+            int low = 0;
+            int high = matrix[rowIndx].length - 1;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (matrix[rowIndx][mid] == target)
+                    return true;
+                else if (matrix[rowIndx][mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return false;
+        }
+
+        private int searchPotentialRow(int[][] matrix, int target) {
+            int low = 0;
+            int high = matrix.length - 1;
+
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+
+                if (matrix[mid][0] <= target && target <= matrix[mid][matrix[0].length]) {
+
+                    return mid;
+                }
+
+                else if (matrix[mid][0] < target)
+                    low = mid + 1;
+                else if (matrix[mid][0] > target)
+                    high = mid + -1;
+            }
+
+            return -1;
+        }
+    }
+}
--- src/main/revision/october/meta/easy/TwoSum.java
+++ src/main/revision/october/meta/easy/TwoSum.java
@@ -23,7 +23,7 @@ public class TwoSum {
     }
 
     // Revised on 11/11/2025
-    class SolutionRevisionFourteenDay {
+    class SolutionRevisionSeventhDay {
         public int[] twoSum(int[] nums, int target) {
             HashMap<Integer, Integer> prevMap = new HashMap<>();
 
@@ -37,6 +37,26 @@ public class TwoSum {
                 }
 
                 prevMap.put(diff, i);
+            }
+
+            return new int[] {};
+        }
+    }
+
+    // Revised on 11/22/2025
+    class SolutionRevisionFourteenDay {
+        public int[] twoSum(int[] nums, int target) {
+
+            HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+
+                int diff = target - num;
+
+                if (prevMap.containsKey(diff)) {
+                    return new int[] { prevMap.get(diff), i };
+                }
             }
 
             return new int[] {};
