16ab1c278ddcf69628a8d981730d94a54ded32b2
akashgond3112 <akashgond3112@gmail.com>
2025-11-23 11:01:05 +0000

    Refactor JumpGame to simplify canJump method and remove unnecessary variables

--- src/main/dsa/dp/medium/JumpGame.java
+++ src/main/dsa/dp/medium/JumpGame.java
@@ -22,39 +22,17 @@ package main.dsa.dp.medium;
 public class JumpGame {
 
 	public static boolean canJump(int[] nums) {
-		if (nums.length <= 1)
-			return true;  // If we are already at the last index or there's only one element
-
-		int jumps = 0;         // To keep track of the number of jumps made
-		int currentEnd = 0;    // The farthest point we can reach with the current jump
-		int farthest = 0;      // The farthest point we can reach with the next jump
-
-		for (int i = 0; i < nums.length - 1; i++) {
-			farthest = Math.max(farthest, i + nums[i]);  // Update the farthest we can go from index i
-
-			if (i == currentEnd) {  // If we've reached the end of our current jump range
-				jumps++;            // We need another jump
-				currentEnd = farthest;  // Update the current end to the farthest point we can reach
-
-				// If we've already reached the last index, we can stop
-				if (currentEnd >= nums.length - 1) {
-					return true;
-				}
-			}
+		int farthest = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (i > farthest) return false;
+			farthest = Math.max(farthest, i + nums[i]);
+			if (farthest >= nums.length - 1) return true;
 		}
-		return false;
+		return true;
 	}
 
 	public static void main(String[] args) {
-		int[] heights = { 2, 3, 1, 1, 4 };  // Example heights of stones
-		int n = heights.length;  // Index of the last stone
-
-		// Initialize dp array with -1 (indicating that no value is computed yet)
-		int[] dp = new int[n + 1];
-		for (int i = 0; i <= n; i++) {
-			dp[i] = -1;
-		}
-
+		int[] heights = { 2, 3, 1, 1, 4 };
 		System.out.println(canJump(heights));
 	}
 }
