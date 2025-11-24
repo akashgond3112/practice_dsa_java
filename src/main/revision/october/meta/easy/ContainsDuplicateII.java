773a3613b5d20d4a1009fd53997bfaff4a3acde0
akashgond3112 <akashgond3112@gmail.com>
2025-11-24 17:42:18 +0000

    Add SolutionRevisionThirdDay class with implementation for containsNearbyDuplicate method

--- src/main/revision/october/meta/easy/ContainsDuplicateII.java
+++ src/main/revision/october/meta/easy/ContainsDuplicateII.java
@@ -26,4 +26,28 @@ public class ContainsDuplicateII {
             return false;
         }
     }
+
+    // revised on 11/23/2025
+    class SolutionRevisionThirdDay {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+            int startIndex = 0;
+            int endIndex = 0;
+
+            HashSet<Integer> set = new HashSet<>();
+
+            while (endIndex < nums.length) {
+
+                if (!set.add(nums[endIndex])) {
+                    return true;
+                }
+
+                if (set.size() >= k) {
+                    set.remove(nums[startIndex++]);
+                }
+                endIndex++;
+            }
+            return false;
+        }
+    }
 }
