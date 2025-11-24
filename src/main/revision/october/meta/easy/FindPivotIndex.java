d6e05dc3e6202a0a6692ddc928d7a699dd0b4526
akashgond3112 <akashgond3112@gmail.com>
2025-11-24 20:04:02 +0000

    Add SolutionRevisionOnThirdDay class with implementation for pivotIndex method

--- src/main/revision/october/meta/easy/FindPivotIndex.java
+++ src/main/revision/october/meta/easy/FindPivotIndex.java
@@ -23,4 +23,28 @@ public class FindPivotIndex {
             return -1;
         }
     }
+
+    // revise on 11/24/2025
+    class SolutionRevisionOnThirdDay {
+
+        public int pivotIndex(int[] nums) {
+
+            int totalSum = 0;
+
+            for (int num : nums)
+                totalSum += num;
+
+            int leftSum = 0;
+            for (int i = 0; i < nums.length; i++) {
+
+                totalSum -= nums[i];
+                if (totalSum == leftSum)
+                    return i;
+
+                leftSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
 }
