e76a47a61da7bb6f5cccbd6e22b2946ff4f54d10
akashgond3112 <akashgond3112@gmail.com>
2025-11-24 17:42:09 +0000

    Add CanPlaceFlowers class with implementation for flower placement logic

---
+++ src/main/revision/october/meta/easy/CanPlaceFlowers.java
@@ -0,0 +1,27 @@
+package main.revision.october.meta.easy;
+
+public class CanPlaceFlowers {
+
+    class Solution {
+        public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+            int total = 0;
+
+            for (int i = 0; i < flowerbed.length; i++) {
+
+                if (flowerbed[i] == 0) {
+
+                    int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+                    int prev = (i == 0) ? 0 : flowerbed[i - 1];
+
+                    if (next == 0 && prev == 0) {
+                        flowerbed[i] = 1;
+                        total++;
+                    }
+                }
+            }
+
+            return total == 1;
+        }
+    }
+}
