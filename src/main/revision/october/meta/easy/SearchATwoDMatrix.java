2608e33a41b5ed1ea66701d5fe65192a610a4e96
akashgond3112 <akashgond3112@gmail.com>
2025-11-25 20:03:04 +0000

    Refactor binarySearchOverRow method for improved readability and consistency in placement within SearchATwoDMatrix class

--- src/main/revision/october/meta/easy/SearchATwoDMatrix.java
+++ src/main/revision/october/meta/easy/SearchATwoDMatrix.java
@@ -14,23 +14,6 @@ public class SearchATwoDMatrix {
             }
         }
 
-        private boolean binarySearchOverRow(int rowIndx, int[][] matrix, int target) {
-            int low = 0;
-            int high = matrix[rowIndx].length - 1;
-
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-                if (matrix[rowIndx][mid] == target)
-                    return true;
-                else if (matrix[rowIndx][mid] > target) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-            return false;
-        }
-
         private int searchPotentialRow(int[][] matrix, int target) {
             int low = 0;
             int high = matrix.length - 1;
@@ -51,6 +34,23 @@ public class SearchATwoDMatrix {
             }
 
             return -1;
+        }
+
+        private boolean binarySearchOverRow(int rowIndx, int[][] matrix, int target) {
+            int low = 0;
+            int high = matrix[rowIndx].length - 1;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (matrix[rowIndx][mid] == target)
+                    return true;
+                else if (matrix[rowIndx][mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return false;
         }
     }
 }
