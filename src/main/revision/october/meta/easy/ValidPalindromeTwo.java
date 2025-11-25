9174e41b2e33edf67289f923fefe380bdb322f03
akashgond3112 <akashgond3112@gmail.com>
2025-11-25 17:10:46 +0000

    Add validPalindromeRevisionThirtyDay method for enhanced palindrome validation

--- src/main/revision/october/meta/easy/ValidPalindromeTwo.java
+++ src/main/revision/october/meta/easy/ValidPalindromeTwo.java
@@ -70,4 +70,22 @@ public class ValidPalindromeTwo {
         return true;
     }
 
+    // Revised on 11/24/2025
+    public boolean validPalindromeRevisionThirtyDay(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return isValidPalindrome(s, left + 1, right) || isValidPalindrome(s, left, right - 1);
+            }
+        }
+
+        return true;
+    }
+
 }
