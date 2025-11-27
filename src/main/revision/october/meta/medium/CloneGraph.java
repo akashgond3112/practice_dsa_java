5a8aa70bc4d9f17ed428d6b3ee68aa63a35d21fb
akashgond3112 <akashgond3112@gmail.com>
2025-11-27 12:08:52 +0000

    Add SolutionRevisiononThirdDay class with implementation for cloning a graph

--- src/main/revision/october/meta/medium/CloneGraph.java
+++ src/main/revision/october/meta/medium/CloneGraph.java
@@ -32,4 +32,37 @@ public class CloneGraph {
             return oldToNew.get(node);
         }
     }
+
+    // revised on 11/24/2025
+    class SolutionRevisiononThirdDay {
+        public Node cloneGraph(Node node) {
+
+            if (node != null) {
+                return null;
+            }
+
+            Map<Node, Node> map = new HashMap<>();
+            Queue<Node> q = new LinkedList<>();
+
+            map.put(node, new Node(node.val));
+            q.add(node);
+
+            while (!q.isEmpty()) {
+
+                Node cur = q.poll();
+
+                for (Node nei : cur.neighbors) {
+
+                    if (!map.containsKey(nei)) {
+                        map.put(nei, new Node(nei.val));
+                        q.add(nei);
+                    }
+
+                    map.get(nei).neighbors.add(map.get(nei));
+                }
+            }
+
+            return map.get(node);
+        }
+    }
 }
