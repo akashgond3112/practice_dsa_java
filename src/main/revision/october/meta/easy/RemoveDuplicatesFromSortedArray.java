4ee45a6a57c1dca7ee1d5e8ccf261ff1767586da
akashgond3112 <akashgond3112@gmail.com>
2025-11-29 15:01:11 +0000

    Add SolutionRevisionFourteenDay class with implementation for removeDuplicates method

--- src/main/revision/october/meta/easy/RemoveDuplicatesFromSortedArray.java
+++ src/main/revision/october/meta/easy/RemoveDuplicatesFromSortedArray.java
@@ -30,4 +30,19 @@ public class RemoveDuplicatesFromSortedArray {
             return i + 1;
         }
     }
+
+    // revised on 11/9/2025 and 11/15/2025
+    class SolutionRevisionFourteenDay {
+        public int removeDuplicates(int[] nums) {
+            int i = 0;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] != nums[j]) {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+
+            return i + 1;
+        }
+    }
 }
