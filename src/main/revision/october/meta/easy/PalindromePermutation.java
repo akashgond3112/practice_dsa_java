ca0fc4caaba4de44a9fdd9a2d5a6ac225a9b90f2
akashgond3112 <akashgond3112@gmail.com>
2025-11-29 15:01:03 +0000

    Add PalindromePermutation class with implementation for canPermutePalindrome method

---
+++ src/main/revision/october/meta/easy/PalindromePermutation.java
@@ -0,0 +1,32 @@
+package main.revision.october.meta.easy;
+
+public class PalindromePermutation {
+
+    public class Solution {
+
+        public boolean canPermutePalindrome(String str) {
+            if (str == null || str.isEmpty()) {
+                return true;
+            }
+            
+            // Count frequency of each character
+            int[] charCount = new int[128]; // ASCII characters
+            for (char c : str.toCharArray()) {
+                charCount[c]++;
+            }
+            
+            // At most one character can have odd frequency for palindrome permutation
+            int oddCount = 0;
+            for (int count : charCount) {
+                if (count % 2 != 0) {
+                    oddCount++;
+                }
+                if (oddCount > 1) {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
