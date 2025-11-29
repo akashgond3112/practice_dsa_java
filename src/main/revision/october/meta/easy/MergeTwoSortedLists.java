3f430a708c545ec370846ba8381578494d94588d
akashgond3112 <akashgond3112@gmail.com>
2025-11-29 15:00:54 +0000

    Add SolutionRevisionOnThirdDay class with implementation for mergeTwoLists method

--- src/main/revision/october/meta/easy/MergeTwoSortedLists.java
+++ src/main/revision/october/meta/easy/MergeTwoSortedLists.java
@@ -29,4 +29,32 @@ public class MergeTwoSortedLists {
             return dummy.next;
         }
     }
+
+    // revised on 11/29/2025
+    class SolutionRevisionOnThirdDay {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+            ListNode dummy = new ListNode(0);
+            ListNode node = dummy;
+
+            while (list1 != null && list2 != null) {
+
+                if (list1.val < list2.val) {
+                    node.next = list1;
+                    list1 = list1.next;
+                } else {
+                    node.next = list2;
+                    list2 = list2.next;
+                }
+            }
+
+            if (list1 != null) {
+                node.next = list1;
+            } else {
+                node.next = list2;
+            }
+
+            return dummy.next;
+        }
+    }
 }
