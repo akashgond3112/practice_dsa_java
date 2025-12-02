423e59af7321b38ab111318c8596b73602fb0e12
akashgond3112 <akashgond3112@gmail.com>
2025-12-02 10:49:43 +0000

    Add SolutionRevisionFourteenDay class with implementation for findPeakElement method

--- src/main/revision/october/meta/medium/FindPeakElement.java
+++ src/main/revision/october/meta/medium/FindPeakElement.java
@@ -96,4 +96,30 @@ public class FindPeakElement {
             return left;
         }
     }
+
+    // revised on 04/11/2025
+    static class SolutionRevisionFourteenDay {
+
+        public int findPeakElement(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] < nums[mid + 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+    }
 }
