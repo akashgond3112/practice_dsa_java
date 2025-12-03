e1455743a8c5fb28bb651aa2ebd9321b7b3ae070
akashgond3112 <akashgond3112@gmail.com>
2025-12-03 11:05:23 +0000

    Add SolutionRevisedOnThirdDay class with implementation for isMonotonic method

--- src/main/revision/october/meta/easy/MonotonicArray.java
+++ src/main/revision/october/meta/easy/MonotonicArray.java
@@ -25,4 +25,28 @@ public class MonotonicArray {
 
         }
     }
+
+    // revised on 11/30/2025
+    class SolutionRevisedOnThirdDay {
+        public boolean isMonotonic(int[] nums) {
+            boolean isDec = true;
+            boolean isInc = true;
+
+            for (int i = 1; i < nums.length; i++) {
+
+                if (nums[i] > nums[i - 1]) {
+                    isDec = false;
+                }
+
+                if (nums[i] < nums[i - 1]) {
+                    isInc = false;
+                }
+
+                if (!isDec && !isInc)
+                    return false;
+            }
+
+            return true;
+        }
+    }
 }
