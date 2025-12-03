ba16be4a80414b85ca1dd9eb1881cdc4a7187e9a
akashgond3112 <akashgond3112@gmail.com>
2025-12-03 11:04:56 +0000

    Add SolutionRevisedOnSeventhDay class with implementation for canPlaceFlowers method

--- src/main/revision/october/meta/easy/CanPlaceFlowers.java
+++ src/main/revision/october/meta/easy/CanPlaceFlowers.java
@@ -24,4 +24,28 @@ public class CanPlaceFlowers {
             return total == 1;
         }
     }
+
+    // revised on 12/2/2025
+    class SolutionRevisedOnSeventhDay {
+        public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+            int total = 0;
+
+            for (int i = 0; i < flowerbed.length; i++) {
+
+                if (flowerbed[i] == 0) {
+
+                    int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+                    int prev = (i == 0) ? 0 : flowerbed[i - 1];
+
+                    if (next == 0 && prev == 0) {
+                        flowerbed[i] = 1;
+                        total++;
+                    }
+                }
+            }
+
+            return total == 1;
+        }
+    }
 }
