ca429fe97711c9b693142d3d70b4fd919075d237
akashgond3112 <akashgond3112@gmail.com>
2025-12-03 11:05:08 +0000

    Add SolutionRevisionOnSeventhDay class with updated pivotIndex method implementation

--- src/main/revision/october/meta/easy/FindPivotIndex.java
+++ src/main/revision/october/meta/easy/FindPivotIndex.java
@@ -47,4 +47,27 @@ public class FindPivotIndex {
             return -1;
         }
     }
+
+    // revise on 11/30/2025
+    class SolutionRevisionOnSeventhDay {
+        public int pivotIndex(int[] nums) {
+            int totalSum = 0;
+
+            for (int num : nums) {
+                totalSum += num;
+            }
+
+            int leftSum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                totalSum -= nums[i];
+
+                if (totalSum == leftSum) {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+
+            return totalSum;
+        }
+    }
 }
