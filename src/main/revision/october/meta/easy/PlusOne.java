98dca7b33ff499cca42a1a14105dc7e6b65427fd
akashgond3112 <akashgond3112@gmail.com>
2025-12-04 16:30:10 +0000

    Add PlusOne class with implementation for plusOne method to increment an array of digits

---
+++ src/main/revision/october/meta/easy/PlusOne.java
@@ -0,0 +1,37 @@
+package main.revision.october.meta.easy;
+
+public class PlusOne {
+    class Solution {
+        /**
+         * Adds one to the number represented by the input array of digits.
+         *
+         * @param digits the array of digits representing a non-negative integer
+         * @return an array of digits representing the integer after adding one
+         */
+        public int[] plusOne(int[] digits) {
+            int carry = 1;
+
+            for (int i = digits.length - 1; i >= 0; i--) {
+
+                int cur = digits[i] + carry;
+
+                if (cur >= 10) {
+                    digits[i] = cur % 10;
+                    carry = cur / 10;
+                } else {
+                    digits[i] = cur;
+                    carry = 0;
+                }
+            }
+
+            if (carry > 0) {
+                int[] result = new int[digits.length + 1];
+                result[0] = carry;
+                System.arraycopy(digits, 0, result, 1, digits.length);
+                return result;
+            }
+            
+            return digits;
+        }
+    }
+}
