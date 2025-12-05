6154f90047273201866e5d834bff68d87a25ef73
akashgond3112 <akashgond3112@gmail.com>
2025-12-05 19:23:54 +0000

    Add AddBinary class with Solution implementation for binary addition of two strings

---
+++ src/main/revision/october/meta/easy/AddBinary.java
@@ -0,0 +1,34 @@
+package main.revision.october.meta.easy;
+
+public class AddBinary {
+    class Solution {
+        public String addBinary(String num1, String num2) {
+            int i = num1.length() - 1;
+            int j = num2.length() - 1;
+
+            int carry = 0;
+            StringBuilder sb = new StringBuilder();
+
+            while (i >= 0 || j >= 0) {
+
+                int sum = carry;
+                if (i >= 0) {
+                    sum += num1.charAt(i--) - '0';
+                }
+
+                if (j >= 0) {
+                    sum += num2.charAt(j--) - '0';
+                }
+
+                sb.append(sum % 2);
+                carry = sum / 2;
+            }
+
+            if (carry != 0) {
+                sb.append(carry);
+            }
+
+            return sb.reverse().toString();
+        }
+    }
+}
