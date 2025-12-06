c09e1457f9a7ca9c74b71d88923ae450f3a74436
akashgond3112 <akashgond3112@gmail.com>
2025-12-06 08:03:37 +0000

    Add SolutionRevisedOnSeventhDay class with implementation for checking monotonic arrays

--- src/main/revision/october/meta/easy/MonotonicArray.java
+++ src/main/revision/october/meta/easy/MonotonicArray.java
@@ -49,4 +49,30 @@ public class MonotonicArray {
             return true;
         }
     }
+
+    // revised on 12/6/2025
+    class SolutionRevisedOnSeventhDay {
+        public boolean isMonotonic(int[] nums) {
+            boolean isInc = true;
+            boolean isDec = true;
+
+            for (int i = 0; i < nums.length; i++) {
+
+                if (nums[i] > nums[i - 1]) {
+                    isDec = false;
+                }
+
+                if (nums[i] < nums[i - 1]) {
+                    isInc = false;
+                }
+
+                if (!isDec && !isInc) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
 }
