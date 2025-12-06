d74ebe7b8f4849421e5c5831af72c418fb2e040a
akashgond3112 <akashgond3112@gmail.com>
2025-12-06 08:03:44 +0000

    Refactor SolutionRevisionOnThirdDay class for improved plusOne method implementation

--- src/main/revision/october/meta/easy/PlusOne.java
+++ src/main/revision/october/meta/easy/PlusOne.java
@@ -30,7 +30,38 @@ public class PlusOne {
                 System.arraycopy(digits, 0, result, 1, digits.length);
                 return result;
             }
-            
+
+            return digits;
+        }
+    }
+
+    // revised on 12/6/2025
+    class SolutionRevisionOnThirdDay {
+
+        public int[] plusOne(int[] digits) {
+
+            int carry = 1;
+
+            for (int i = digits.length - 1; i > 0; i--) {
+
+                int cur = digits[i] + carry;
+
+                if (cur >= 10) {
+                    digits[i] = cur % 10;
+                    carry = cur / 10;
+                } else {
+                    digits[i] = cur;
+                    carry = 0;
+                }
+            }
+
+            if (carry > 0) {
+                int[] result = new int[digits.length + 1];
+                result[0] = carry;
+                System.arraycopy(digits, 0, result, 1, digits.length);
+                return result;
+            }
+
             return digits;
         }
     }
