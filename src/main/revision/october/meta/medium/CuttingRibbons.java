ca422c7ca3e857e8f1df252d24df52e65cb54e2f
akashgond3112 <akashgond3112@gmail.com>
2025-12-07 14:16:40 +0000

    Add CuttingRibbons class with solution for maximizing ribbon lengths

---
+++ src/main/revision/october/meta/medium/CuttingRibbons.java
@@ -0,0 +1,41 @@
+package main.revision.october.meta.medium;
+
+public class CuttingRibbons {
+
+    public static class Solution {
+
+        public static int getMaxLength(int[] nums, int k) {
+
+            int maxLength = 0;
+            for (int num : nums) {
+                maxLength += num;
+            }
+
+            int left = 1;
+            int right = maxLength;
+
+            while (left <= right) {
+
+                int mid = left + (right - left) / 2;
+
+                if (cutRibbons(nums, k, mid)) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            return right;
+        }
+
+        private static boolean cutRibbons(int[] nums, int k, int mid) {
+            int count = 0;
+
+            for (int num : nums) {
+                count += num / mid;
+            }
+
+            return count >= k;
+        }
+    }
+}
