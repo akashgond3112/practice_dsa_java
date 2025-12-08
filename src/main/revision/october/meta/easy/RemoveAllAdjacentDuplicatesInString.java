17571aa0fb79c56d4fcac09b3d0ae7b6e00aa818
akashgond3112 <akashgond3112@gmail.com>
2025-12-08 14:01:59 +0000

    Add removeDuplicatesRevisionThirtyDay method to handle adjacent duplicates

--- src/main/revision/october/meta/easy/RemoveAllAdjacentDuplicatesInString.java
+++ src/main/revision/october/meta/easy/RemoveAllAdjacentDuplicatesInString.java
@@ -71,4 +71,23 @@ public class RemoveAllAdjacentDuplicatesInString {
 
         return sb.toString();
     }
+
+    // Revision on 12/8/2025
+    public String removeDuplicatesRevisionThirtyDay(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+
+            int len = sb.length();
+
+            if (!sb.isEmpty() && sb.charAt(len - 1) == c) {
+                sb.deleteCharAt(len - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
 }
