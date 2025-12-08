34a5660a967e6ac592bac68bc0e5887903cb8dc7
akashgond3112 <akashgond3112@gmail.com>
2025-12-08 14:02:37 +0000

    Add SolutionRevisedOnThirdDay class with optimized ribbon cutting logic

--- src/main/revision/october/meta/medium/CuttingRibbons.java
+++ src/main/revision/october/meta/medium/CuttingRibbons.java
@@ -38,4 +38,41 @@ public class CuttingRibbons {
             return count >= k;
         }
     }
+
+    // revision on 12/8/2025
+    public static class SolutionRevisedOnThirdDay {
+
+        public static int getMaxLength(int[] nums, int k) {
+
+            int maxLength = 0;
+            for (int num : nums) {
+                maxLength += num;
+            }
+
+            int left = 1;
+            int right = maxLength;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (cutRibbons(nums, k, mid)) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            return right;
+        }
+
+        private static boolean cutRibbons(int[] nums, int k, int mid) {
+
+            int count = 0;
+            for (int num : nums) {
+                count += num / mid;
+            }
+
+            return count >= k;
+        }
+    }
 }
