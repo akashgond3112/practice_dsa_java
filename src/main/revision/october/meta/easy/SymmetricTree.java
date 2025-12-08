2ae0a7677927b4fa7486a24f78cd82140237554a
akashgond3112 <akashgond3112@gmail.com>
2025-12-08 14:02:12 +0000

    Add SolutionRevisedOnSeventhDay class with corrected symmetry check logic

--- src/main/revision/october/meta/easy/SymmetricTree.java
+++ src/main/revision/october/meta/easy/SymmetricTree.java
@@ -74,4 +74,42 @@ public class SymmetricTree {
             return true;
         }
     }
+
+    // revised on 12/8/2025
+    class SolutionRevisedOnSeventhDay {
+        public boolean isSymmetric(TreeNode root) {
+
+            if (root == null) {
+                return false;
+            }
+
+            Queue<TreeNode> left = new LinkedList<>();
+            Queue<TreeNode> right = new LinkedList<>();
+
+            left.add(root.left);
+            right.add(root.right);
+
+            while (!left.isEmpty() && !right.isEmpty()) {
+
+                TreeNode cl = left.poll();
+                TreeNode cR = right.poll();
+
+                if (cl == null || cR == null) {
+                    return false;
+                }
+
+                if (cl.val != cR.val) {
+                    return false;
+                }
+
+                left.add(cl.left);
+                left.add(cl.right);
+
+                right.add(cR.right);
+                right.add(cR.left);
+            }
+
+            return true;
+        }
+    }
 }
