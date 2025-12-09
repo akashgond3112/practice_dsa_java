2b6bf41bd3906c2531a4a8d472ca7a8a1f16fce0
akashgond3112 <akashgond3112@gmail.com>
2025-12-09 09:47:26 +0000

    Add SolutionRevisedOnSevethDay class with improved logic for exclusive time calculation

--- src/main/revision/october/meta/medium/ExclusiveTimeOfFunctions.java
+++ src/main/revision/october/meta/medium/ExclusiveTimeOfFunctions.java
@@ -85,4 +85,43 @@ public class ExclusiveTimeOfFunctions {
             return result;
         }
     }
+
+    // revised on 12/7/2025
+    class SolutionRevisedOnSevethDay {
+        public int[] exclusiveTime(int n, List<String> logs) {
+
+            int[] result = new int[n];
+
+            Stack<Pair> st = new Stack<>();
+
+            for (String log : logs) {
+
+                String[] split = log.split(":");
+
+                int functionId = Integer.parseInt(split[0]);
+                boolean isStarted = split[1].equals("start");
+                int currentTimeStamp = Integer.parseInt(split[2]);
+
+                if (isStarted) {
+
+                    if (!st.isEmpty()) {
+
+                        Pair cur = st.peek();
+                        result[cur.functionId] += currentTimeStamp + cur.startTime;
+                    }
+
+                    st.push(new Pair(functionId, currentTimeStamp));
+                } else {
+                    Pair completed = st.peek();
+                    result[completed.functionId] += currentTimeStamp - completed.startTime + 1;
+
+                    if (!st.isEmpty()) {
+                        st.peek().startTime = currentTimeStamp + 1;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
 }
