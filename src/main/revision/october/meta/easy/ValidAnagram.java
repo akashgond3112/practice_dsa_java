13d3a66cfa31750c5608417997151b6fe81e417d
akashgond3112 <akashgond3112@gmail.com>
2025-12-09 09:47:18 +0000

    Add SolutionRevisedOnThirdDay class with implementation for anagram validation

--- src/main/revision/october/meta/easy/ValidAnagram.java
+++ src/main/revision/october/meta/easy/ValidAnagram.java
@@ -21,4 +21,30 @@ public class ValidAnagram {
             return true;
         }
     }
+
+    // revised on 12/9/2025
+    public class SolutionRevisedOnThirdDay {
+        public boolean isAnagram(String s, String t) {
+
+            if (s.length() != t.length()) {
+                return false;
+            }
+
+            int[] count = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                count[t.charAt(i) - 'a']--;
+            }
+
+            for (int val : count) {
+                if (val != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+    }
 }
