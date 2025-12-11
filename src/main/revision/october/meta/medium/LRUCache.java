de5228ddafc48ed329713506f1c9655fa09f0222
akashgond3112 <akashgond3112@gmail.com>
2025-12-11 08:29:00 +0000

    Add SolutionRevisionOnSeventhDay and SolutionRevisionOnFourteenDay classes with LRUCache implementation

--- src/main/revision/october/meta/medium/LRUCache.java
+++ src/main/revision/october/meta/medium/LRUCache.java
@@ -146,4 +146,146 @@ public class LRUCache {
             }
         }
     }
+
+    // revised on 11/11/2025
+    class SolutionRevisionOnSeventhDay {
+
+        private final Map<Integer, TreeNode> map;
+        private final int capacity;
+
+        private final TreeNode head;
+        private final TreeNode tail;
+
+        SolutionRevisionOnSeventhDay(int capacity) {
+            this.capacity = capacity;
+            this.map = new HashMap<>();
+
+            this.head = new TreeNode(0, 0);
+            this.tail = new TreeNode(0, 0);
+
+            head.next = tail;
+            head.prev = head;
+        }
+
+        public void removeNode(TreeNode node) {
+
+            TreeNode prev = node.prev;
+            TreeNode next = node.next;
+
+            prev.next = next;
+            next.prev = prev;
+        }
+
+        public void addAfterHead(TreeNode node) {
+
+            node.next = head.next;
+            node.prev = head;
+
+            head.next.prev = node;
+            head.next = node;
+        }
+
+        public int get(int key) {
+
+            if (!map.containsKey(key)) {
+                return -1;
+            }
+
+            TreeNode node = map.get(key);
+            removeNode(node);
+            addAfterHead(node);
+            return node.val;
+        }
+
+        public void put(int key, int val) {
+
+            if (map.containsKey(key)) {
+                TreeNode node = map.get(key);
+                removeNode(node);
+                addAfterHead(node);
+            } else {
+
+                if (map.size() == capacity) {
+
+                    TreeNode lru = map.get(key);
+                    map.remove(lru.key);
+                    removeNode(lru);
+                }
+
+                TreeNode node = new TreeNode(key, val);
+                map.put(key, node);
+                addAfterHead(node);
+            }
+        }
+    }
+
+    // revised on 11/25/2025
+    class SolutionRevisionOnFourteenDay {
+
+        Map<Integer, TreeNode> map;
+        int capacity;
+
+        TreeNode head;
+        TreeNode tail;
+
+        SolutionRevisionOnFourteenDay(int capacity) {
+            this.map = new HashMap<>();
+            this.capacity = capacity;
+
+            this.head = new TreeNode(0, 0);
+            this.tail = new TreeNode(0, 0);
+        }
+
+        public void removeNode(TreeNode node) {
+
+            TreeNode prev = node.prev;
+            TreeNode next = node.next;
+
+            prev.next = next;
+            next.prev = prev;
+        }
+
+        public void afterHead(TreeNode node) {
+
+            node.next = head.next;
+            node.prev = head;
+
+            head.next.prev = node;
+            head.next = node;
+        }
+
+        public int get(int key) {
+
+            if (!map.containsKey(key)) {
+                return -1;
+            }
+
+            TreeNode node = map.get(key);
+            removeNode(node);
+            afterHead(node);
+
+            return node.val;
+        }
+
+        public void put(int key, int val) {
+
+            if (map.containsKey(key)) {
+                TreeNode node = map.get(key);
+                removeNode(node);
+                afterHead(node);
+            } else {
+
+                if (map.size() == capacity) {
+                    TreeNode node = tail.prev;
+                    map.remove(node.key);
+                    removeNode(node);
+                }
+
+                TreeNode node = new TreeNode(key, val);
+                map.put(key, node);
+                afterHead(node);
+            }
+        }
+    }
+
 }
