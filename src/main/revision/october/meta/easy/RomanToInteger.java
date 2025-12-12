0efd10a7df68bba2e7834fc3207771e99212be18
akashgond3112 <akashgond3112@gmail.com>
2025-12-12 14:31:04 +0000

    Add RomanToInteger class with implementation for converting Roman numerals to integers

---
+++ src/main/revision/october/meta/easy/RomanToInteger.java
@@ -0,0 +1,36 @@
+package main.revision.october.meta.easy;
+
+import java.util.*;
+
+public class RomanToInteger {
+
+    private static final Map<Character, Integer> ROMAN_VALUES = new HashMap<>();
+
+    static {
+        ROMAN_VALUES.put('I', 1);
+        ROMAN_VALUES.put('V', 5);
+        ROMAN_VALUES.put('X', 10);
+        ROMAN_VALUES.put('L', 50);
+        ROMAN_VALUES.put('C', 100);
+        ROMAN_VALUES.put('D', 500);
+        ROMAN_VALUES.put('M', 1000);
+    }
+
+    public int romanToInt(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int currentVal = ROMAN_VALUES.get(s.charAt(i));
+            // Check if there is a next character and if its value is greater
+            if (i < s.length() - 1 && currentVal < ROMAN_VALUES.get(s.charAt(i + 1))) {
+                result -= currentVal;
+            } else {
+                result += currentVal;
+            }
+        }
+        return result;
+    }
+}
