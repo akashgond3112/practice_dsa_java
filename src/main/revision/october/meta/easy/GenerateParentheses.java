0a71a88c03ff4b1963fb20674c226d1baab73c99
akashgond3112 <akashgond3112@gmail.com>
2025-12-12 14:30:50 +0000

    Add SolutionRevisionOnFourteenDay class with implementation for generating parentheses

--- src/main/revision/october/meta/easy/GenerateParentheses.java
+++ src/main/revision/october/meta/easy/GenerateParentheses.java
@@ -93,4 +93,37 @@ public class GenerateParentheses {
             }
         }
     }
+
+    // revision on 12/12/2025
+    class SolutionRevisionOnFourteenDay {
+        public List<String> generateParenthesis(int n) {
+
+            List<String> result = new ArrayList<>();
+
+            int opening = 0;
+            int closing = 0;
+
+            dfs(result, n, opening, closing, "");
+
+            return result;
+
+        }
+
+        private static void dfs(List<String> result, int n, int opening, int closing, String str) {
+
+            if (opening == n && closing == n) {
+                result.add(str);
+                return;
+            }
+
+            if (opening < n) {
+                dfs(result, n, opening + 1, closing, str + "(");
+            }
+
+            if (closing < n) {
+                dfs(result, n, opening, closing + 1, str + ")");
+            }
+
+        }
+    }
 }
