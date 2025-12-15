e4bfaf442b3645663d07ec9fd5898887e32a2fa9
akashgond3112 <akashgond3112@gmail.com>
2025-12-15 11:56:50 +0000

    Add SolutionRevisedOnSeventhDay class with isAnagram method implementation

--- src/main/revision/october/meta/easy/ValidAnagram.java
+++ src/main/revision/october/meta/easy/ValidAnagram.java
@@ -47,4 +47,29 @@ public class ValidAnagram {
 
         }
     }
+
+    // revised on 12/15/2025
+    public class SolutionRevisedOnSeventhDay {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+
+            int[] count = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                count[t.charAt(i) - 'a']--;
+            }
+
+            for (int val : count) {
+                if (val != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+    }
 }
