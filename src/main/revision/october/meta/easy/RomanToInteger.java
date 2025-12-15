f2681c3e0ca4344cd3cb366d943a1b67a988d2bc
akashgond3112 <akashgond3112@gmail.com>
2025-12-15 11:56:31 +0000

    Refactor RomanToInteger class to improve structure and enhance readability of the romanToInt method

--- src/main/revision/october/meta/easy/RomanToInteger.java
+++ src/main/revision/october/meta/easy/RomanToInteger.java
@@ -16,21 +16,48 @@ public class RomanToInteger {
         ROMAN_VALUES.put('M', 1000);
     }
 
-    public int romanToInt(String s) {
-        if (s == null || s.isEmpty()) {
-            return 0;
-        }
-
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int currentVal = ROMAN_VALUES.get(s.charAt(i));
-            // Check if there is a next character and if its value is greater
-            if (i < s.length() - 1 && currentVal < ROMAN_VALUES.get(s.charAt(i + 1))) {
-                result -= currentVal;
-            } else {
-                result += currentVal;
+    public class Solution {
+        public int romanToInt(String s) {
+            if (s == null || s.isEmpty()) {
+                return 0;
             }
+
+            int result = 0;
+            for (int i = 0; i < s.length(); i++) {
+                int currentVal = ROMAN_VALUES.get(s.charAt(i));
+                // Check if there is a next character and if its value is greater
+                if (i < s.length() - 1 && currentVal < ROMAN_VALUES.get(s.charAt(i + 1))) {
+                    result -= currentVal;
+                } else {
+                    result += currentVal;
+                }
+            }
+            return result;
         }
-        return result;
     }
+
+    // revised on 12/15/2025
+    public class SolutionRevisedOnThirdDay {
+        public int romanToInt(String s) {
+            if (s == null || s.isEmpty()) {
+                return 0;
+            }
+
+            int result = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                int curVal = ROMAN_VALUES.get(s.charAt(i));
+
+                if (i < s.length() - 1 && curVal < ROMAN_VALUES.get(s.charAt(i + 1))) {
+                    result -= curVal;
+                } else {
+                    result += curVal;
+                }
+            }
+
+            return result;
+        }
+    }
+
 }
