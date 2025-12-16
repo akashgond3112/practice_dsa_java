017d5855d61703dec42a98ff60de4defb4cd4473
akashgond3112 <akashgond3112@gmail.com>
2025-12-16 11:44:27 +0000

    Add SolutionRevisionThirtyDay class with implementation for alien dictionary sorting validation

--- src/main/revision/october/meta/easy/VerifyingAnAlienDictionary.java
+++ src/main/revision/october/meta/easy/VerifyingAnAlienDictionary.java
@@ -162,4 +162,43 @@ public class VerifyingAnAlienDictionary {
             return true;
         }
     }
+
+    // revised on 12/16/2025
+    class SolutionRevisionThirtyDay {
+        public boolean isAlienSorted(String[] words, String order) {
+            Map<Character, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < order.length(); i++) {
+                map.put(order.charAt(i), i);
+            }
+
+            for (int j = 0; j < words.length; j++) {
+
+                String w1 = words[j];
+                String w2 = words[j + 1];
+
+                int min = Math.min(w1.length(), w2.length());
+                boolean diff = false;
+
+                for (int k = 0; k < min; k++) {
+                    char c1 = w1.charAt(k);
+                    char c2 = w2.charAt(k);
+
+                    if (c1 != c2) {
+                        if (map.get(c1) > map.get(c2)) {
+                            return false;
+                        }
+                    }
+                    diff = true;
+                    break;
+                }
+
+                if (!diff && w1.length() > w2.length()) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
