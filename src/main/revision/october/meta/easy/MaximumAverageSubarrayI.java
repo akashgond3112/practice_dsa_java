cdf9772ce83463bb8fa376487be6e22e68e60fd8
akashgond3112 <akashgond3112@gmail.com>
2025-12-17 17:08:55 +0000

    Add SolutionRevisedOnThirdDay class with implementation for maximum average subarray

--- src/main/revision/october/meta/easy/MaximumAverageSubarrayI.java
+++ src/main/revision/october/meta/easy/MaximumAverageSubarrayI.java
@@ -34,4 +34,33 @@ public class MaximumAverageSubarrayI {
         }
 
     }
+
+    // revised on 12/17/2025
+    class SolutionRevisedOnThirdDay {
+        public double findMaxAverage(int[] nums, int k) {
+
+            int sum = 0;
+            for (int i = 0; i < k; i++) {
+                sum += nums[i];
+            }
+
+            int maxSum = sum;
+
+            int start = 0;
+            int end = k;
+
+            while (end < nums.length) {
+
+                sum -= nums[start];
+                start++;
+
+                sum += nums[end];
+                end++;
+
+                maxSum = Math.max(maxSum, sum);
+            }
+
+            return (double) maxSum / k;
+        }
+    }
 }
