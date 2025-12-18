7a4743e81e161cd8495c9eba120935700d3fe6fa
akashgond3112 <akashgond3112@gmail.com>
2025-12-18 09:24:08 +0000

    Add SolutionRevisionThirtyDay class with implementation for peak element finding using binary search

--- src/main/revision/october/meta/medium/FindPeakElement.java
+++ src/main/revision/october/meta/medium/FindPeakElement.java
@@ -122,4 +122,30 @@ public class FindPeakElement {
             return left;
         }
     }
+
+    // revised on 12/17/2025
+    static class SolutionRevisionThirtyDay {
+
+        public int findPeakElement(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] < nums[mid + 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+    }
 }
