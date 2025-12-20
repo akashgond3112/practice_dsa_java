39ae75df5cda2ea0724abf5ce3d05f7fd0810d1c
akashgond3112 <akashgond3112@gmail.com>
2025-12-20 10:17:34 +0000

    Add IntersectionOfTwoArrays class with implementation to find the intersection of two arrays

---
+++ src/main/revision/october/meta/easy/IntersectionOfTwoArrays.java
@@ -0,0 +1,30 @@
+package main.revision.october.meta.easy;
+
+import java.util.*;
+
+public class IntersectionOfTwoArrays {
+
+    class Solution {
+        public int[] intersection(int[] nums1, int[] nums2) {
+            // Use single HashSet for O(1) lookup - optimal approach
+            // Time: O(n + m), Space: O(min(n, m))
+            Set<Integer> set = new HashSet<>();
+            List<Integer> result = new ArrayList<>();
+
+            // Add all elements from nums1 to set - O(n)
+            for (int num : nums1) {
+                set.add(num);
+            }
+
+            // Check intersection and remove from set to avoid duplicates - O(m)
+            for (int num : nums2) {
+                if (set.remove(num)) {
+                    result.add(num);
+                }
+            }
+
+            // Convert to array - O(k) where k is intersection size
+            return result.stream().mapToInt(Integer::intValue).toArray();
+        }
+    }
+}
