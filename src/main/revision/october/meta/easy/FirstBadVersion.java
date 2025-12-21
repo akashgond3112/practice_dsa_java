9e533d8da884e0f247595b16486d2e8427af9227
akashgond3112 <akashgond3112@gmail.com>
2025-12-21 11:44:08 +0000

    Add revised solution for FirstBadVersion problem with updated logic and structure

--- src/main/revision/october/meta/easy/FirstBadVersion.java
+++ src/main/revision/october/meta/easy/FirstBadVersion.java
@@ -27,4 +27,24 @@ public class FirstBadVersion {
             return left; // Left ya right dono pehla bad version point karenge
         }
     }
+
+    // revied on 12/21/2025
+    public class SolutionRevisedOnTirdDay extends VersionControl {
+        public int firstBadVersion(int n) {
+            int left = 0;
+            int right = n;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (isBadVersion(mid)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
 }
