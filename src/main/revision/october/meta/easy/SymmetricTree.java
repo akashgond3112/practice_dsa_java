94e7e3c6e8d17353cc02f05efa311335753fb2fa
akashgond3112 <akashgond3112@gmail.com>
2025-12-22 10:15:14 +0000

    Refactor isSymmetric method in multiple Solution classes for improved clarity and consistency

--- src/main/revision/october/meta/easy/SymmetricTree.java
+++ src/main/revision/october/meta/easy/SymmetricTree.java
@@ -1,5 +1,6 @@
 package main.revision.october.meta.easy;
 
+import main.dsa.nonlinear.tree.Node;
 import main.revision.october.meta.TreeNode;
 import java.util.*;
 
@@ -7,109 +8,76 @@ public class SymmetricTree {
 
     class Solution {
         public boolean isSymmetric(TreeNode root) {
+            return root == null || checkIsSymmetric(root.left, root.right);
+        }
 
-            if (root == null)
+        private static boolean checkIsSymmetric(TreeNode left, TreeNode right) {
+
+            if (left == null && right == null) {
                 return true;
-
-            Queue<TreeNode> left = new LinkedList<>();
-            Queue<TreeNode> right = new LinkedList<>();
-
-            left.add(root.left);
-            right.add(root.right);
-
-            while (!left.isEmpty() && !right.isEmpty()) {
-
-                TreeNode l = left.poll();
-                TreeNode r = right.poll();
-
-                if (l == null || r == null)
-                    return false;
-
-                if (l.val != r.val)
-                    return false;
-
-                left.add(l.left);
-                left.add(l.right);
-
-                right.add(r.right);
-                right.add(r.left);
             }
 
-            return true;
+            if (left.val != right.val)
+                return false;
 
+            return checkIsSymmetric(left.left, right.right) && checkIsSymmetric(left.right, right.left);
         }
     }
 
     // revised on 12/2/2025
     class SolutionRevisedOnThirdDay {
         public boolean isSymmetric(TreeNode root) {
-            if (root == null)
-                return false;
+            return root == null || checkIsSymmetric(root.left, root.right);
+        }
 
-            Queue<TreeNode> left = new LinkedList<>();
-            Queue<TreeNode> right = new LinkedList<>();
+        private static boolean checkIsSymmetric(TreeNode left, TreeNode right) {
 
-            left.add(root.left);
-            right.add(root.right);
-
-            while (!left.isEmpty() && !right.isEmpty()) {
-
-                TreeNode cL = left.poll();
-                TreeNode cR = right.poll();
-
-                if (cL == null || cR == null) {
-                    return false;
-                }
-
-                if (cL.val != cR.val)
-                    return false;
-
-                left.add(cL.left);
-                left.add(cL.right);
-
-                right.add(cR.right);
-                right.add(cR.left);
+            if (left == null && right == null) {
+                return true;
             }
 
-            return true;
+            if (left.val != right.val)
+                return false;
+
+            return checkIsSymmetric(left.left, right.right) && checkIsSymmetric(left.right, right.left);
         }
     }
 
     // revised on 12/8/2025
     class SolutionRevisedOnSeventhDay {
         public boolean isSymmetric(TreeNode root) {
+            return root == null || checkIsSymmetric(root.left, root.right);
+        }
 
-            if (root == null) {
+        private static boolean checkIsSymmetric(TreeNode left, TreeNode right) {
+
+            if (left == null && right == null) {
+                return true;
+            }
+
+            if (left.val != right.val)
                 return false;
+
+            return checkIsSymmetric(left.left, right.right) && checkIsSymmetric(left.right, right.left);
+        }
+    }
+
+    // revised on 12/22/2025
+    class SolutionRevisedOnFourteenDay {
+        public boolean isSymmetric(TreeNode root) {
+            return root == null || checkIsSymmetric(root.left, root.right);
+        }
+
+        private static boolean checkIsSymmetric(TreeNode left, TreeNode right) {
+
+            if (left == null && right == null) {
+                return true;
             }
 
-            Queue<TreeNode> left = new LinkedList<>();
-            Queue<TreeNode> right = new LinkedList<>();
+            if (left.val != right.val)
+                return false;
 
-            left.add(root.left);
-            right.add(root.right);
-
-            while (!left.isEmpty() && !right.isEmpty()) {
-
-                TreeNode cl = left.poll();
-                TreeNode cR = right.poll();
-
-                if (cl == null || cR == null) {
-                    return false;
-                }
-
-                if (cl.val != cR.val) {
-                    return false;
-                }
-
-                left.add(cl.left);
-                left.add(cl.right);
-
-                right.add(cR.right);
-                right.add(cR.left);
-            }
-
-            return true;
+            return checkIsSymmetric(left.left, right.right) && checkIsSymmetric(left.right, right.left);
         }
     }
 }
