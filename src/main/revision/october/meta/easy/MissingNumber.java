cb43f84a7eed98b4db55f0ee63e5bcfb42e43018
akashgond3112 <akashgond3112@gmail.com>
2025-12-24 10:03:36 +0000

    Add missingNumber method implementation in Solution class of MissingNumber

--- src/main/revision/october/meta/easy/MissingNumber.java
+++ src/main/revision/october/meta/easy/MissingNumber.java
@@ -5,6 +5,30 @@ public class MissingNumber {
     class Solution {
         public int missingNumber(int[] nums) {
 
+            // Ek variable initialize karte hain XOR result store karne ke liye
+            int xor = 0;
+
+            // 0 se nums.length tak ke saare numbers ko XOR karte hain
+            // Yeh ensure karta hai ki range [0, n] ke saare numbers include ho
+            for (int i = 0; i <= nums.length; i++) {
+                xor = xor ^ i;
+            }
+
+            // Input array ke saare numbers ko XOR karte hain
+            // Yeh un numbers ko cancel kar deta hai jo range aur array dono mein hain
+            for (int num : nums) {
+                xor = xor ^ num;
+            }
+
+            // XOR mein jo value bachi hai, wahi missing number hai
+            return xor;
+        }
+    }
+
+    // revision on 12/24/2025
+    class SolutionRevisedOnThirdDay {
+        public int missingNumber(int[] nums) {
+
             // Initialize a variable to store the XOR result
             int xor = 0;
 
