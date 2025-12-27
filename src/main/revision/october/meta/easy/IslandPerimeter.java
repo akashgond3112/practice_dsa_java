ceda1c27cb20974e32b9f96a76997777e35c809e
akashgond3112 <akashgond3112@gmail.com>
2025-12-27 08:08:51 +0000

    Refactor dfs method in IslandPerimeter class for improved readability and consistency; add new SolutionRevisedOnFourteenDay class with complete implementation

--- src/main/revision/october/meta/easy/IslandPerimeter.java
+++ src/main/revision/october/meta/easy/IslandPerimeter.java
@@ -18,13 +18,14 @@ public class IslandPerimeter {
 
         private int dfs(int[][] grid, int r, int c) {
             // If we go out of bounds, we've found a perimeter edge
-            if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) {
+            if (r < 0
+                    || r >= grid.length
+                    || c < 0
+                    || c >= grid[0].length
+                    || grid[r][c] == 0) {
                 return 1;
             }
-            // If we hit water, we've found a perimeter edge
-            if (grid[r][c] == 0) {
-                return 1;
-            }
+
             // If we've already visited this cell, it doesn't contribute to the perimeter
             if (grid[r][c] == -1) {
                 return 0;
@@ -63,11 +64,11 @@ public class IslandPerimeter {
 
         private int dfs(int[][] grid, int r, int c) {
 
-            if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) {
-                return 1;
-            }
-
-            if (grid[r][c] == 0) {
+            if (r < 0
+                    || r >= grid.length
+                    || c < 0
+                    || c >= grid[0].length
+                    || grid[r][c] == 0) {
                 return 1;
             }
 
@@ -108,13 +109,14 @@ public class IslandPerimeter {
 
         private int dfs(int[][] grid, int r, int c) {
             // If we go out of bounds, we've found a perimeter edge
-            if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) {
+            if (r < 0
+                    || r >= grid.length
+                    || c < 0
+                    || c >= grid[0].length
+                    || grid[r][c] == 0) {
                 return 1;
             }
-            // If we hit water, we've found a perimeter edge
-            if (grid[r][c] == 0) {
-                return 1;
-            }
+
             // If we've already visited this cell, it doesn't contribute to the perimeter
             if (grid[r][c] == -1) {
                 return 0;
@@ -129,6 +131,52 @@ public class IslandPerimeter {
             perimeter += dfs(grid, r - 1, c); // Up
             perimeter += dfs(grid, r, c + 1); // Right
             perimeter += dfs(grid, r, c - 1); // Left
+
+            return perimeter;
+        }
+    }
+
+    // revised on 12/27/2025
+    public class SolutionRevisedOnFourteenDay {
+        public int islandPerimeter(int[][] grid) {
+
+            if (grid == null || grid.length == 0 || grid[0].length == 0) {
+                return 0;
+            }
+
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == 1) {
+                        return dfs(grid, i, j);
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        private int dfs(int[][] grid, int r, int c) {
+
+            if (r < 0 ||
+                    c < 0 ||
+                    r >= grid.length ||
+                    c >= grid[0].length ||
+                    grid[r][c] == 0) {
+                return 1;
+            }
+
+            if (grid[r][c] == -1) {
+                return 0;
+            }
+
+            grid[r][c] = -1;
+
+            int perimeter = 0;
+
+            perimeter += dfs(grid, r + 1, c);
+            perimeter += dfs(grid, r - 1, c);
+            perimeter += dfs(grid, r, c + 1);
+            perimeter += dfs(grid, r, c - 1);
 
             return perimeter;
         }
