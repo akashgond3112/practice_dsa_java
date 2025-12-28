1dd506f8b6e6d5608d4d2980745f3dc58df23a7f
akashgond3112 <akashgond3112@gmail.com>
2025-12-28 11:10:16 +0000

    Add new SolutionRevisedOnSeventhDay class with complete implementation of mergeTrees method; handle null cases and merge logic

--- src/main/revision/october/meta/easy/MergeTwoBinaryTrees.java
+++ src/main/revision/october/meta/easy/MergeTwoBinaryTrees.java
@@ -52,4 +52,25 @@ public class MergeTwoBinaryTrees {
             return root1;
         }
     }
+
+    // revised on 12/28/2025
+    class SolutionRevisedOnSeventhDay {
+        public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+
+            if (root1 == null) {
+                return root2;
+            }
+
+            if (root2 == null) {
+                return root1;
+            }
+
+            root1.val = root1.val + root2.val;
+
+            root1.left = mergeTrees(root1.left, root2.left);
+            root1.right = mergeTrees(root1.right, root2.right);
+
+            return root1;
+        }
+    }
 }
