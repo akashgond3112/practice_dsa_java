20dd696c1553240b5447fe27680928e7a86bdfc2
akashgond3112 <akashgond3112@gmail.com>
2026-01-01 09:51:47 +0000

    Refactor MergeIntervals class: streamline merge logic and enhance clarity in SolutionRevisionSeventhDay and SolutionRevisionFourteenDay methods

--- src/main/revision/october/meta/medium/MergeIntervals.java
+++ src/main/revision/october/meta/medium/MergeIntervals.java
@@ -62,6 +62,7 @@ public class MergeIntervals {
         }
     }
 
+    // Revised on 10/28/2025
     public class SolutionRevisionSeventhDay {
 
         public int[][] merge(int[][] intervals) {
@@ -75,15 +76,77 @@ public class MergeIntervals {
             List<int[]> res = new ArrayList<>();
             res.add(intervals[0]);
 
-            int[] newInterval = intervals[0];
+            int[] curInterval = intervals[0];
 
             for (int[] interval : intervals) {
 
-                if (interval[0] <= newInterval[1]) {
-                    newInterval[1] = Math.max(interval[1], newInterval[1]);
+                if (interval[0] <= curInterval[1]) {
+                    curInterval[1] = Math.max(interval[1], curInterval[1]);
                 } else {
-                    newInterval = interval;
-                    res.add(newInterval);
+                    curInterval = interval;
+                    res.add(curInterval);
+                }
+            }
+
+            return res.toArray(new int[res.size()][]);
+        }
+    }
+
+    // Revised on 11/11/2025
+    public class SolutionRevisionFourteenDay {
+
+        public int[][] merge(int[][] intervals) {
+
+            if (intervals.length <= 1) {
+                return intervals;
+            }
+
+            Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+            List<int[]> res = new ArrayList<>();
+
+            res.add(intervals[0]);
+
+            int[] curInterval = intervals[0];
+
+            for (int[] interval : intervals) {
+
+                if (interval[0] <= curInterval[1]) {
+                    curInterval[1] = Math.max(interval[1], curInterval[1]);
+                } else {
+                    curInterval = interval;
+                    res.add(curInterval);
+                }
+            }
+
+            return res.toArray(new int[res.size()][]);
+        }
+    }
+
+    // Revised on 12/10/2025
+    public class SolutionRevisionThirtyDay {
+
+        public int[][] merge(int[][] intervals) {
+
+            if (intervals.length <= 1) {
+                return intervals;
+            }
+
+            Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+            List<int[]> res = new ArrayList<>();
+
+            res.add(intervals[0]);
+
+            int[] curInterval = intervals[0];
+
+            for (int[] interval : intervals) {
+
+                if (interval[0] <= curInterval[1]) {
+                    curInterval[1] = Math.max(interval[1], curInterval[1]);
+                } else {
+                    curInterval = interval;
+                    res.add(curInterval);
                 }
             }
 
