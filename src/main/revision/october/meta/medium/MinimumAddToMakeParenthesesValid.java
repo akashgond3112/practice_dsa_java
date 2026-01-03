16f4c2ef8064d61595a32622c0f104d2c240dc8b
akashgond3112 <akashgond3112@gmail.com>
2026-01-03 08:26:42 +0000

    Implement minAddToMakeValid method in Solution class: add logic to count unmatched parentheses and return the total needed to make the string valid

--- src/main/revision/october/meta/medium/MinimumAddToMakeParenthesesValid.java
+++ src/main/revision/october/meta/medium/MinimumAddToMakeParenthesesValid.java
@@ -2,26 +2,29 @@ package main.revision.october.meta.medium;
 
 public class MinimumAddToMakeParenthesesValid {
 
+    // Yeh Solution class hai jo minimum parentheses add karne ka count batata hai
     public class Solution {
         public int minAddToMakeValid(String s) {
 
-            int opening = 0;
-            int closing = 0;
+            int opening = 0; // Yeh count karega kitne '(' abhi tak unmatched hain
+            int closing = 0; // Yeh count karega kitne ')' extra hain jo match nahi hue
 
+            // Har character ko string me check karenge
             for (int i = 0; i < s.length(); i++) {
                 char c = s.charAt(i);
 
                 if (c == '(') {
-                    opening++;
+                    opening++; // Agar '(' mila toh opening badha do
                 } else {
-
+                    // Agar ')' mila aur koi unmatched '(' hai toh match kar do
                     if (opening > 0 && c == ')') {
-                        opening--;
+                        opening--; // Ek '(' match ho gaya, toh opening kam kar do
                     } else {
-                        closing++;
+                        closing++; // Nahi toh yeh extra ')' hai, closing badha do
                     }
                 }
             }
+            // Total unmatched '(' aur ')' ka sum hi answer hai
             return opening + closing;
         }
     }
@@ -70,6 +73,60 @@ public class MinimumAddToMakeParenthesesValid {
                     }
                 }
             }
+            return opening + closing;
+        }
+    }
+
+    // Revision on 11/15/2025
+    public class SolutionRevisionFourteenDay {
+        public int minAddToMakeValid(String s) {
+            int opening = 0;
+            int closing = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                char c = s.charAt(i);
+
+                if (c == '(') {
+                    opening++;
+                } else {
+
+                    if (opening > 0 && c == ')') {
+                        opening--;
+                    } else {
+                        closing++;
+                    }
+                }
+
+            }
+
+            return opening + closing;
+        }
+    }
+
+    // Revision on 12/14/2025
+    public class SolutionRevisionThirtyDay {
+        public int minAddToMakeValid(String s) {
+            int opening = 0;
+            int closing = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                char c = s.charAt(i);
+
+                if (c == '(') {
+                    opening++;
+                } else {
+
+                    if (opening > 0 && c == ')') {
+                        opening--;
+                    } else {
+                        closing++;
+                    }
+                }
+
+            }
+
             return opening + closing;
         }
     }
