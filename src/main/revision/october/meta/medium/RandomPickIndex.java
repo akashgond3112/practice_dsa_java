a5a5f61c98ea12f996c11493941a8002f4086cd2
akashgond3112 <akashgond3112@gmail.com>
2026-01-05 15:08:35 +0000

    Add RandomPickIndex class with Solution implementations: implement random index picking using reservoir sampling for target values.

---
+++ src/main/revision/october/meta/medium/RandomPickIndex.java
@@ -0,0 +1,103 @@
+package main.revision.october.meta.medium;
+
+import java.util.Random;
+
+public class RandomPickIndex {
+
+    class Solution {
+
+        private final int[] nums;
+        private final Random rand;
+
+        public Solution(int[] nums) {
+            this.nums = nums;
+            this.rand = new Random();
+        }
+
+        /**
+         * Picks a random index of target value using reservoir sampling.
+         *
+         * Time Complexity: O(n) where n is the length of nums array Space Complexity:
+         * O(1) as we only store a single
+         * result
+         *
+         * @param target the value to randomly pick
+         * @return random index of target, or -1 if target is not found
+         */
+        public int pick(int target) {
+            int result = -1;
+            int count = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+
+                if (nums[i] == target) {
+                    count++;
+                }
+
+                if (rand.nextInt(count) == 0) {
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    // revised on 12/24/2025
+    class SolutionRevisedOnThirdDay {
+        private final int[] nums;
+        private final Random rand;
+
+        public SolutionRevisedOnThirdDay(int[] nums) {
+            this.nums = nums;
+            this.rand = new Random();
+        }
+
+        public int pick(int target) {
+            int result = -1;
+            int count = 0;
+            int i = 0;
+
+            for (int num : nums) {
+                if (num == target) {
+                    count++;
+                }
+                if (rand.nextInt(count) == 0) {
+                    result = i;
+                }
+                i++;
+            }
+
+            return result;
+        }
+    }
+
+    // revised on 12/30/2025
+    class SolutionRevisedOnSeventhDay {
+        private final int[] nums;
+        private final Random rand;
+
+        public SolutionRevisedOnSeventhDay(int[] nums) {
+            this.nums = nums;
+            this.rand = new Random();
+        }
+
+        public int pick(int target) {
+            int result = -1;
+            int count = 0;
+            int i = 0;
+
+            for (int num : nums) {
+                if (num == target)
+                    count++;
+
+                if (rand.nextInt(count) == 0) {
+                    result = i;
+                }
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
