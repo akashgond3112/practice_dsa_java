d695e92bef50803df2da3fafb68dccb2fee120ef
akashgond3112 <akashgond3112@gmail.com>
2026-01-05 15:08:28 +0000

    Refactor myPow method in Pow class: encapsulate logic within Solution class and correct spelling of SolutionRevisionOnThirtyDay.

--- src/main/revision/october/meta/medium/Pow.java
+++ src/main/revision/october/meta/medium/Pow.java
@@ -1,22 +1,47 @@
 package main.revision.october.meta.medium;
 
 public class Pow {
-    public double myPow(double x, int n) {
+    public class Solution {
+        public double myPow(double x, int n) {
 
-        if (n == 0) {
-            return 1;
-        }
+            if (n == 0) {
+                return 1;
+            }
 
-        if (n < 0) {
-            return 1 / (x * myPow(x, -(n + 1)));
-        }
+            if (n < 0) {
+                return 1 / (x * myPow(x, -(n + 1)));
+            }
 
-        double result = myPow(x, n / 2);
+            double result = myPow(x, n / 2);
 
-        if (n % 2 == 0) {
-            return result * result;
-        } else {
-            return result * result * x;
+            if (n % 2 == 0) {
+                return result * result;
+            } else {
+                return result * result * x;
+            }
         }
     }
+
+    // revised on 11/29/2025
+    public class SloutionRevisionOnThirtyDay {
+        public double myPow(double x, int n) {
+
+            if (n == 0) {
+                return 1;
+            }
+
+            if (n < 0) {
+                return 1 / (x * myPow(x, -(n + 1)));
+            }
+
+            double result = myPow(x, n / 2);
+
+            if (n % 2 == 0) {
+                return result * result;
+            } else {
+                return result * result * x;
+            }
+        }
+    }
+
 }
