3890b9a19f79c95b0b3dbaa61c463a4ac14ff070
akashgond3112 <akashgond3112@gmail.com>
2026-01-06 09:46:11 +0000

    Add SolutionRevisionThirtyDay class: implement random index picking with corrected prefix sum initialization and binary search logic.

--- src/main/revision/october/meta/medium/RandomPickWithWeight.java
+++ src/main/revision/october/meta/medium/RandomPickWithWeight.java
@@ -125,4 +125,41 @@ public class RandomPickWithWeight {
         }
     }
 
+    // revised on 04/11/2025
+    public class SolutionRevisionThirtyDay {
+        private final int[] prefixSum;
+        private final int totalWeight;
+        private final Random rnd;
+
+        public SolutionRevisionThirtyDay(int[] w) {
+            this.prefixSum = new int[w.length];
+            prefixSum[0] = 0;
+
+            for (int i = 0; i < w.length; i++) {
+                prefixSum[i] = prefixSum[i - 1] + w[i];
+            }
+
+            this.totalWeight = prefixSum[w.length - 1];
+            rnd = new Random();
+        }
+
+        public int pickIndex() {
+
+            int target = rnd.nextInt(totalWeight);
+
+            int left = 0;
+            int right = prefixSum.length - 1;
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+
+                if (prefixSum[mid] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+    }
 }
