e7e3d69c56b27453fa85cb1db34ce507201b19c0
akashgond3112 <akashgond3112@gmail.com>
2026-01-08 10:01:59 +0000

    Add SumRootToLeafNumbers class: implement DFS logic to calculate sum from root to leaf nodes.

---
+++ src/main/revision/october/meta/medium/SumRootToLeafNumbers.java
@@ -0,0 +1,110 @@
+package main.revision.october.meta.medium;
+
+import main.revision.october.meta.TreeNode;
+
+public class SumRootToLeafNumbers {
+
+    public class Solution {
+        public int sumNumbers(TreeNode root) {
+            // DFS traversal start karte hain root node se, aur initial currentSum 0 set
+            // karte hain
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode node, int currentSum) {
+            // Agar node null hai, iska matlab hum leaf ke baad pohanch gaye hain, to 0
+            // return karte hain
+            if (node == null) {
+                return 0;
+            }
+
+            // Current node ka value currentSum me add karte hain, 10 se multiply karke
+            currentSum = currentSum * 10 + node.val;
+
+            // Agar current node leaf node hai (left aur right dono null hain), to
+            // currentSum return karte hain
+            if (node.left == null && node.right == null) {
+                return currentSum;
+            }
+
+            // Left aur right subtree ke liye recursive call karte hain
+            int leftSum = dfs(node.left, currentSum);
+            int rightSum = dfs(node.right, currentSum);
+
+            // Dono subtree ka sum return karte hain
+            return leftSum + rightSum;
+        }
+    }
+
+    // revised on 12/9/2025
+    public class SolutionRevisedOnThirdDay {
+
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode node, int currentSum) {
+            if (node == null) {
+                return 0;
+            }
+
+            if (node.left == null || node.right == null) {
+                return currentSum;
+            }
+
+            int leftSum = dfs(node.left, currentSum);
+            int rightSum = dfs(node.right, currentSum);
+
+            return leftSum + rightSum;
+        }
+    }
+
+    // revised on 12/15/2025
+    public class SolutionRevisedOnFourteenDay {
+
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode node, int currentSum) {
+            if (node == null) {
+                return 0;
+            }
+
+            if (node.left == null || node.right == null) {
+                return currentSum;
+            }
+
+            int leftSum = dfs(node.left, currentSum);
+            int rightSum = dfs(node.right, currentSum);
+
+            return leftSum + rightSum;
+        }
+    }
+
+    // revised on 12/29/2025
+    class SolutionReviseOnFourteenDay {
+        public int sumNumbers(TreeNode root) {
+
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode node, int currentSum) {
+
+            if (node == null) {
+                return 0;
+            }
+
+            currentSum = currentSum * 10 + node.val;
+
+            if (node.left == null || node.right == null) {
+                return currentSum;
+            }
+
+            int leftSum = dfs(node.left, currentSum);
+            int rightSum = dfs(node.right, currentSum);
+
+            return leftSum + rightSum;
+        }
+    }
+}
