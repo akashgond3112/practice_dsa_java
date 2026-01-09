01d16e2d85f83f9b53947c2998db28aa27e5fff8
akashgond3112 <akashgond3112@gmail.com>
2026-01-09 17:49:26 +0000

    Add SolutionRevisedOnDaySeventh class: implement logic to find missing numbers using index marking.

--- src/main/revision/october/meta/easy/FindAllNumbersDisappearedInAnArray.java
+++ src/main/revision/october/meta/easy/FindAllNumbersDisappearedInAnArray.java
@@ -55,12 +55,35 @@ public class FindAllNumbersDisappearedInAnArray {
     }
 
     // revised on 12/26/2025
+    class SolutionRevisedOnDaySeventh {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            List<Integer> result = new ArrayList<>();
+
+            for (int num : nums) {
+                int index = Math.abs(num) - 1;
+                if (nums[index] > 0) {
+                    nums[index] = -nums[index];
+                }
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > 0) {
+                    result.add(i + 1);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    // revised on 1/9/2026
     class SolutionRevisedOnDayFourteen {
         public List<Integer> findDisappearedNumbers(int[] nums) {
             List<Integer> result = new ArrayList<>();
 
             for (int num : nums) {
                 int index = Math.abs(num) - 1;
+
                 if (nums[index] > 0) {
                     nums[index] = -nums[index];
                 }
