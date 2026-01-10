dea197de43ee7c7adedabc14f2a300cdf1cd505e
akashgond3112 <akashgond3112@gmail.com>
2026-01-10 10:01:05 +0000

    Refactor SolutionRevisedOnFourteenDay class: implement firstBadVersion method using binary search logic.

--- src/main/revision/october/meta/easy/FirstBadVersion.java
+++ src/main/revision/october/meta/easy/FirstBadVersion.java
@@ -1,7 +1,5 @@
 package main.revision.october.meta.easy;
 
-import main.revision.october.meta.easy.FirstBadVersion.VersionControl;
-
 public class FirstBadVersion {
 
     /*
@@ -57,6 +55,28 @@ public class FirstBadVersion {
             int right = n;
 
             while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (isBadVersion(mid)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
+
+    // revied on 1/10/2026
+    public class SolutionRevisedOnFourteenDay extends VersionControl {
+        public int firstBadVersion(int n) {
+
+            int left = 0;
+            int right = n;
+
+            while (left < right) {
+
                 int mid = left + (right - left) / 2;
 
                 if (isBadVersion(mid)) {
