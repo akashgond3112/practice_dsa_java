6640a49785de7f67b08ed43899a33755902c876c
akashgond3112 <akashgond3112@gmail.com>
2026-01-11 12:41:39 +0000

    Refactor ContainsDuplicateII class: improve containsNearbyDuplicate method with enhanced logic and comments for better understanding

--- src/main/revision/october/meta/easy/ContainsDuplicateII.java
+++ src/main/revision/october/meta/easy/ContainsDuplicateII.java
@@ -5,25 +5,41 @@ import java.util.HashSet;
 public class ContainsDuplicateII {
 
     class Solution {
+
+        /**
+         * Big O Notation Details:
+         * - Time Complexity: O(n), kyunki har element ko ek baar add aur ek baar remove
+         * karte hain set se.
+         * - Space Complexity: O(k), kyunki HashSet mein maximum k elements store hote
+         * hain ek time par.
+         */
         public boolean containsNearbyDuplicate(int[] nums, int k) {
 
+            // Initialize startIndex and endIndex pointers
             int startIndex = 0;
             int endIndex = 0;
 
+            // HashSet to store the current window of elements
             HashSet<Integer> set = new HashSet<>();
 
+            // Traverse the array
             while (endIndex < nums.length) {
 
+                // Agar window size k se zyada ho jaye, toh startIndex ka element remove karo
                 if (set.size() > k) {
                     set.remove(nums[startIndex++]);
                 }
 
+                // Agar current element already set mein hai, toh duplicate mil gaya
                 if (!set.add(nums[endIndex])) {
                     return true;
                 }
 
+                // Window ko aage badhao
                 endIndex++;
             }
+
+            // Agar koi duplicate nahi mila, toh false return karo
             return false;
         }
     }
@@ -73,6 +89,32 @@ public class ContainsDuplicateII {
 
                 end++;
             }
+            return false;
+        }
+    }
+
+    // revised on 1/11/2026
+    class SolutionRevisedOnDayThirty {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+            int startIndex = 0;
+            int endIndex = 0;
+
+            HashSet<Integer> set = new HashSet<>();
+
+            while (endIndex < nums.length) {
+
+                if (set.size() < k) {
+                    set.remove(nums[startIndex++]);
+                }
+
+                if (!set.add(nums[endIndex++])) {
+                    return true;
+                }
+
+                endIndex++;
+            }
+
             return false;
         }
     }
