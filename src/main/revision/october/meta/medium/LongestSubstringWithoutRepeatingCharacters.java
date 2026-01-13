4cf4a40fb3b487169e370095abf0047b26d6179f
akashgond3112 <akashgond3112@gmail.com>
2026-01-13 18:08:59 +0000

    Add SolutionRevisedOnDayFourteen class: implement lengthOfLongestSubstring method with optimized logic for finding the length of the longest substring without repeating characters

--- src/main/revision/october/meta/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/revision/october/meta/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -69,4 +69,27 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
         }
     }
+
+    // revised on 1/11/2026
+    class SolutionRevisedOnDayFourteen {
+        public int lengthOfLongestSubstring(String s) {
+
+            Map<Character, Integer> map = new HashMap<>();
+            int left = 0;
+            int result = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+                char c = s.charAt(right);
+
+                if (map.containsKey(c)) {
+                    left = Math.max(left, map.get(c) + 1);
+                }
+
+                map.put(c, right);
+                result = Math.max(result, (right - left) + 1);
+            }
+
+            return result;
+        }
+    }
 }
