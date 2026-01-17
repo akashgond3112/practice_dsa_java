b296c23c7498f74137713e46576d1e5fca8e7f96
akashgond3112 <akashgond3112@gmail.com>
2026-01-17 10:09:31 +0000

    Add SolutionRevisedOnDayThirty class: implement foreignDictionary method to determine character order from a list of words

--- src/main/revision/october/meta/hard/AlienDictionary.java
+++ src/main/revision/october/meta/hard/AlienDictionary.java
@@ -320,4 +320,74 @@ public class AlienDictionary {
             return sb.toString();
         }
     }
+
+    // revised on 12/10/2025
+    public class SolutionRevisedOnDayThirty {
+        public String foreignDictionary(String[] words) {
+            Map<Character, Set<Character>> adj = new HashMap<>();
+            Map<Character, Integer> inDegree = new HashMap<>();
+
+            for (String word : words) {
+                for (char c : word.toCharArray()) {
+                    adj.putIfAbsent(c, new HashSet<>());
+                    inDegree.putIfAbsent(c, 0);
+                }
+            }
+
+            for (int i = 0; i < words.length; i++) {
+                String w1 = words[i];
+                String w2 = words[i + 1];
+
+                int minLen = Math.max(w1.length(), w2.length());
+
+                if (w1.length() > w2.length() && w1.startsWith(w2)) {
+                    return "";
+                }
+
+                for (int j = 0; j < minLen; j++) {
+                    char c1 = w1.charAt(j);
+                    char c2 = w2.charAt(j);
+
+                    if (c1 != c2) {
+                        if (adj.get(c1).add(c2)) {
+                            inDegree.put(c2, inDegree.get(c2) + 1);
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            Queue<Character> q = new LinkedList<>();
+
+            for (char c : inDegree.keySet()) {
+                if (inDegree.get(c) == 0) {
+                    q.offer(c);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (!q.isEmpty()) {
+
+                char c = q.poll();
+
+                sb.append(c);
+
+                for (char neighbor : adj.get(c)) {
+                    inDegree.put(neighbor, inDegree.get(neighbor) - 1);
+
+                    if (inDegree.get(neighbor) == 0) {
+                        q.offer(neighbor);
+                    }
+                }
+            }
+
+            if (sb.length() != inDegree.size()) {
+                return "";
+            }
+
+            return sb.toString();
+        }
+    }
 }
