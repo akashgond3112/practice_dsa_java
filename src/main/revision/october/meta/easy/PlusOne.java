25fe79869d8a40f440e7b886778d898b7d4a564b
akashgond3112 <akashgond3112@gmail.com>
2026-01-24 10:00:11 +0000

    Add SolutionRevisedOnDayThirty class: implement plusOne method to handle carry correctly and return updated array

--- src/main/revision/october/meta/easy/PlusOne.java
+++ src/main/revision/october/meta/easy/PlusOne.java
@@ -126,4 +126,34 @@ public class PlusOne {
         }
 
     }
+
+    // revised on 1/24/2026
+    class SolutionRevisedOnDayThirty {
+        public int[] plusOne(int[] digits) {
+
+            int carry = 0;
+
+            for (int i = digits.length - 1; i > 0; i++) {
+
+                int cur = digits[i] + carry;
+
+                if (carry >= 10) {
+                    digits[i] = cur % 10;
+                    carry = cur / 10;
+                } else {
+                    digits[i] = cur % 10;
+                    carry = 0;
+                }
+            }
+
+            if (carry > 0) {
+                int[] result = new int[digits.length + 1];
+                result[0] = 0;
+                System.arraycopy(digits, 0, result, 1, digits.length);
+                return result;
+            }
+
+            return digits;
+        }
+    }
 }
