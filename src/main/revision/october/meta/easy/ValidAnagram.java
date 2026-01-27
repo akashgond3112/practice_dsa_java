d239d040cdedf5c14c488e8cc90c14b92b4f0100
akashgond3112 <akashgond3112@gmail.com>
2026-01-27 16:03:22 +0000

    Add SolutionRevisedOnDayThirty class: implement isAnagram method to check if two strings are anagrams

--- src/main/revision/october/meta/easy/ValidAnagram.java
+++ src/main/revision/october/meta/easy/ValidAnagram.java
@@ -97,4 +97,28 @@ public class ValidAnagram {
 
         }
     }
+
+    // revised on 1/27/2026
+    class SolutionRevisedOnDayThirty {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+
+            int[] count = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                count[t.charAt(i) - 'a']--;
+            }
+
+            for (int val : count) {
+                if (val != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
 }
