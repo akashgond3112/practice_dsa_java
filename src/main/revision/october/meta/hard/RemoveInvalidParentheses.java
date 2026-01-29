7bfe8638824915157a202e266192624a10a6e033
akashgond3112 <akashgond3112@gmail.com>
2026-01-29 19:58:27 +0000

    Refactor RemoveInvalidParentheses class: improve solution methods and fix variable naming

--- src/main/revision/october/meta/hard/RemoveInvalidParentheses.java
+++ src/main/revision/october/meta/hard/RemoveInvalidParentheses.java
@@ -4,62 +4,175 @@ import java.util.*;
 
 public class RemoveInvalidParentheses {
 
-    List<String> res = new ArrayList<>();
-    Map<String, Integer> map = new HashMap<>();
+    class Solution {
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
 
-    public List<String> removeInvalidParentheses(String s) {
+        public List<String> removeInvalidParentheses(String s) {
 
-        solve(s, getMinInValid(s));
+            solve(s, getMinInvalid(s));
 
-        return res;
+            return res;
+        }
+
+        private void solve(String s, int i) {
+            // Check if already visited
+            if (map.containsKey(s)) {
+                return;
+            }
+            map.put(s, 1);
+
+            if (i < 0) {
+                return;
+            }
+
+            if (i == 0) {
+                if (getMinInvalid(s) == 0) {
+                    res.add(s);
+                }
+                return;
+            }
+
+            for (int j = 0; j < s.length(); j++) {
+                String left = s.substring(0, j);
+                String right = s.substring(j + 1);
+                solve(left + right, i - 1);
+            }
+        }
+
+        private int getMinInvalid(String s) {
+            Stack<Character> st = new Stack<>();
+            int i = 0;
+
+            while (i < s.length()) {
+
+                if (s.charAt(i) == '(') {
+                    st.push('(');
+                } else if (s.charAt(i) == ')') {
+
+                    if (st.size() > 0 && st.peek() == '(') {
+                        st.pop();
+                    } else {
+                        st.push(')');
+                    }
+                }
+                i++;
+            }
+
+            return st.size();
+        }
     }
 
-    private int getMinInValid(String s) {
-        Stack<Character> st = new Stack<>();
-        int i = 0;
+    // revised on 31/10/2025
+    class SolutionRevisionFourteenDay {
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
 
-        while (i < s.length()) {
+        public List<String> removeInvalidParentheses(String s) {
 
-            if (s.charAt(i) == '(') {
-                st.push('(');
-            } else if (s.charAt(i) == ')') {
+            solve(s, getMinInValid(s));
 
-                if (st.size() > 0 && st.peek() == '(') {
-                    st.pop();
-                } else {
-                    st.push(')');
+            return res;
+        }
+
+        private int getMinInValid(String s) {
+            int open = 0;
+            int close = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    open++;
+                } else if (s.charAt(i) == ')') {
+                    if (open > 0) {
+                        open--;
+                    } else {
+                        close++;
+                    }
                 }
             }
-            i++;
+
+            return open + close;
         }
 
-        return st.size();
+        private void solve(String s, int i) {
+            // Check if already visited
+            if (map.containsKey(s)) {
+                return;
+            }
+            map.put(s, 1);
+
+            if (i < 0) {
+                return;
+            }
+
+            if (getMinInValid(s) == 0) {
+                if (getMinInValid(s) == 0) {
+                    res.add(s);
+                }
+                return;
+            }
+
+            for (int j = 0; j < s.length(); j++) {
+                String left = s.substring(0, j);
+                String right = s.substring(j + 1);
+                solve(left + right, i - 1);
+            }
+        }
     }
 
-    private void solve(String s, int j) {
+    // revised on 11/29/2025
+    class SolutionRevisionThirtyDay {
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
 
-        if (map.get(s) != 0) {
-            return;
-        } else {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+        public List<String> removeInvalidParentheses(String s) {
+
+            solve(s, getMinInValid(s));
+            return res;
         }
 
-        if (j < 0) {
-            return;
+        private int getMinInValid(String s) {
+            int open = 0;
+            int close = 0;
+
+            for (char c : s.toCharArray()) {
+
+                if (c == '(') {
+                    open++;
+                } else if (c == ')') {
+                    if (open > 0) {
+                        open--;
+                    } else {
+                        close++;
+                    }
+                }
+            }
+
+            return open + close;
         }
 
-        if (j == 0) {
+        private void solve(String s, int invalidPar) {
+
+            if (map.containsKey(s))
+                return;
+
+            map.put(s, 1);
+
+            if (invalidPar < 0) {
+                return;
+            }
+
             if (getMinInValid(s) == 0) {
                 res.add(s);
             }
-            return;
-        }
 
-        for (int i = 0; i < s.length(); i++) {
-            String left = s.substring(0, i);
-            String right = s.substring(i + 1);
-            solve(left + right, j - 1);
+            for (int i = 0; i < s.length(); i++) {
+
+                String left = s.substring(0, i);
+                String right = s.substring(1 + 1);
+
+                solve(left + right, i - 1);
+            }
         }
-        return;
     }
 }
