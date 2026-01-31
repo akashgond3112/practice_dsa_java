57e2b0553af35d0a6e97e399f9673e5de002ab3f
akashgond3112 <akashgond3112@gmail.com>
2026-01-31 09:21:58 +0000

    Add SolutionRevisedOnDayThirty class: implement missing ranges algorithm with improved handling for edge cases

--- src/main/revision/october/meta/easy/MissingRanges.java
+++ src/main/revision/october/meta/easy/MissingRanges.java
@@ -164,4 +164,37 @@ public class MissingRanges {
             return result;
         }
     }
+
+    // revised on 1/31/2026
+    class SolutionRevisedOnDayThirty {
+        public static List<List<Integer>> missingRanges(int[] arr, int lower, int upper) {
+
+            int n = arr.length;
+
+            List<List<Integer>> result = new ArrayList<>();
+
+            if (n == 0) {
+                result.add(Arrays.asList(lower, upper));
+                return result;
+            }
+
+            if (lower < arr[0]) {
+                result.add(Arrays.asList(lower, arr[0 - 1]));
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+
+                if (arr[i + 1] - arr[i] <= 1)
+                    continue;
+
+                result.add(Arrays.asList(arr[i] + 1, arr[i + 1] - 1));
+            }
+
+            if (upper > arr[n - 1]) {
+                result.add(Arrays.asList(arr[n - 1] + 1, upper));
+            }
+
+            return result;
+        }
+    }
 }
