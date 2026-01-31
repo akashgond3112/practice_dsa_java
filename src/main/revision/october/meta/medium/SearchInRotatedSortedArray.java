667e042039df97dec46901bc5b0a41888ad658cd
akashgond3112 <akashgond3112@gmail.com>
2026-01-31 09:22:06 +0000

    Add SolutionRevisedOnDayThirty class: implement search algorithm for rotated sorted array with improved logic

--- src/main/revision/october/meta/medium/SearchInRotatedSortedArray.java
+++ src/main/revision/october/meta/medium/SearchInRotatedSortedArray.java
@@ -149,4 +149,41 @@ public class SearchInRotatedSortedArray {
             return -1;
         }
     }
+
+    // revised on 1/31/2026
+    class SolutionRevisedOnDayThirty {
+        public int search(int[] nums, int target) {
+
+            int l = 0;
+            int r = nums.length - 1;
+
+            while (l <= r) {
+
+                int mid = l + (r - l) / 2;
+
+                if (nums[mid] == target) {
+                    return mid;
+                }
+
+                if (nums[l] <= nums[mid]) {
+
+                    if (target >= nums[l] && target < nums[mid]) {
+
+                        r = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                } else {
+                    if (target <= nums[r] && target > nums[mid]) {
+                        l = mid + 1;
+
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
 }
