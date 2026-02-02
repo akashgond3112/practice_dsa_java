56ae8f4a3b78d6b2e5fd24f6c6ac4cc53a156545
akashgond3112 <akashgond3112@gmail.com>
2026-02-02 08:27:30 +0000

    Add SolutionRevisedOnDayThirty class: implement longest consecutive ones algorithm with sliding window approach

--- src/main/revision/october/meta/medium/MaxConsecutiveOnesIII.java
+++ src/main/revision/october/meta/medium/MaxConsecutiveOnesIII.java
@@ -121,4 +121,33 @@ public class MaxConsecutiveOnesIII {
             return maxOnes;
         }
     }
+
+    // revised on 2/2/2026
+    public class SolutionRevisedOnDayThirty {
+        public int longestOnes(int[] nums, int k) {
+
+            int zeroCount = 0;
+            int start = 0;
+            int maxOnes = 0;
+
+            for (int end = 0; end < nums.length; end++) {
+
+                if (nums[end] == 0) {
+                    zeroCount++;
+                }
+
+                while (zeroCount > k) {
+                    if (nums[start] == 0) {
+                        zeroCount--;
+                    }
+
+                    start++;
+                }
+
+                maxOnes = Math.max(maxOnes, end - start + 1);
+            }
+
+            return maxOnes;
+        }
+    }
 }
