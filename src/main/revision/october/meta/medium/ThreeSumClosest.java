2477d375468aefbc8ce407515b2425694b9985c4
akashgond3112 <akashgond3112@gmail.com>
2026-02-03 11:21:17 +0000

    Add SolutionRevisedOnDayThirty class: implement threeSumClosest method with two-pointer approach

--- src/main/revision/october/meta/medium/ThreeSumClosest.java
+++ src/main/revision/october/meta/medium/ThreeSumClosest.java
@@ -156,4 +156,40 @@ public class ThreeSumClosest {
             return resultSum;
         }
     }
+
+    // revised on 2/3/2026
+    public class SolutionRevisedOnDayThirty {
+        public int threeSumClosest(int[] nums, int target) {
+
+            Arrays.sort(nums);
+            int resultSum = nums[0] + nums[1] + nums[2];
+
+            int n = nums.length;
+
+            for (int i = 0; i < n - 2; i++) {
+
+                int left = i + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    int sum = nums[i] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        return target;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+
+                    if (Math.abs(sum - target) < Math.abs(target - resultSum)) {
+                        resultSum = sum;
+                    }
+                }
+            }
+
+            return resultSum;
+        }
+    }
 }
