9efd7ca1cbac04776daae18ada58d3ab70300b34
akashgond3112 <akashgond3112@gmail.com>
2026-02-03 11:21:07 +0000

    Add SolutionRevisedOnDayThirty class: implement isPalindrome method with complete logic

--- src/main/revision/october/meta/easy/PalindromeNumber.java
+++ src/main/revision/october/meta/easy/PalindromeNumber.java
@@ -114,4 +114,32 @@ public class PalindromeNumber {
             return true;
         }
     }
+
+    // revised on 2/3/2026
+    public class SolutionRevisedOnDayThirty {
+        public boolean isPalindrome(int x) {
+
+            if (x < 0) {
+                return false;
+            }
+
+            long div = 1;
+
+            while (x >= 10 * div) {
+                div *= 10;
+            }
+
+            while (x != 0) {
+
+                if (x / div != x % 10) {
+                    return false;
+                }
+
+                x = (int) (x % div) / 10;
+                div /= 10;
+            }
+
+            return true;
+        }
+    }
 }
