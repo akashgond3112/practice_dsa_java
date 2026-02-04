bf93baf7b0eb5171776604b621a590c7f26699a5
akashgond3112 <akashgond3112@gmail.com>
2026-02-04 09:13:23 +0000

    Add SolutionRevisedOnDayThirty class: implement findMaxAverage method using sliding window technique

--- src/main/revision/october/meta/easy/MaximumAverageSubarrayI.java
+++ src/main/revision/october/meta/easy/MaximumAverageSubarrayI.java
@@ -119,4 +119,33 @@ public class MaximumAverageSubarrayI {
             return (double) maxSum / k;
         }
     }
+
+    // revised on 2/4/2026
+    public class SolutionRevisedOnDayThirty {
+        public double findMaxAverage(int[] nums, int k) {
+
+            int sum = 0;
+            for (int i = 0; i < k; i++) {
+                sum += nums[i];
+            }
+
+            int maxSum = sum;
+
+            int start = 0;
+            int end = k;
+
+            while (end < nums.length) {
+
+                sum -= nums[start];
+                start++;
+
+                sum += nums[end];
+                end--;
+
+                maxSum = Math.max(maxSum, sum);
+            }
+
+            return (double) maxSum / k;
+        }
+    }
 }
