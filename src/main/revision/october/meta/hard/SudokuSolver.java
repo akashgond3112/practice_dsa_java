9f41412e237a37b2d96954707d4179bd6876a941
akashgond3112 <akashgond3112@gmail.com>
2026-02-07 08:18:30 +0000

    Add SolutionRevisedOnDayThirty class: implement solveSudoku and isValidEntry methods for solving Sudoku puzzles

--- src/main/revision/october/meta/hard/SudokuSolver.java
+++ src/main/revision/october/meta/hard/SudokuSolver.java
@@ -1,3 +1,4 @@
+
 package main.revision.october.meta.hard;
 
 public class SudokuSolver {
@@ -234,6 +235,53 @@ public class SudokuSolver {
                 }
             }
 
+            return true;
+        }
+    }
+
+    // revised on 2/7/2026
+    class SolutionRevisedOnDayThirty {
+        public void solveSudoku(char[][] board) {
+            solve(board);
+        }
+
+        private boolean solve(char[][] board) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j] == '.') {
+                        for (char c = '1'; c < '9'; c++) {
+                            if (isValidEntry(board, i, j, c)) {
+                                board[i][j] = c;
+                                if (solve(board)) {
+                                    return true;
+                                } else {
+                                    board[i][j] = '.';
+                                }
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private boolean isValidEntry(char[][] board, int row, int col, char c) {
+
+            for (int i = 0; i < 9; i++) {
+
+                if (board[i][col] == c)
+                    return false;
+                if (board[row][i] == c)
+                    return false;
+
+                int boxRow = 3 * (row / 3) + i / 3;
+                int boxCol = 3 * (col / 3) + i % 3;
+
+                if (board[boxRow][boxCol] == c)
+                    return false;
+
+            }
             return true;
         }
     }
