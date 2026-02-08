20173d1b1496d0e4baddb82e854d5a8af3d1b587
akashgond3112 <akashgond3112@gmail.com>
2026-02-08 09:57:54 +0000

    Add SolutionRevisedOnDayThirty class: implement firstBadVersion method for identifying the first bad version in a sequence

--- src/main/revision/october/meta/easy/FirstBadVersion.java
+++ src/main/revision/october/meta/easy/FirstBadVersion.java
@@ -89,4 +89,26 @@ public class FirstBadVersion {
             return left;
         }
     }
+
+    // revised on 2/8/2026
+    class SolutionRevisedOnDayThirty extends VersionControl {
+        public int firstBadVersion(int n) {
+
+            int left = 0;
+            int right = n;
+
+            while (left < right) {
+
+                int mid = left + (right - left) / 2;
+
+                if (isBadVersion(mid)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
 }
