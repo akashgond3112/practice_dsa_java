973d3cbeadb164d3df6fc4f662a042388b22cd37
akashgond3112 <akashgond3112@gmail.com>
2026-02-10 17:46:51 +0000

    Add SolutionRevisedOnDayThirty class: implement intersection method for finding the intersection of two arrays

--- src/main/revision/october/meta/easy/IntersectionOfTwoArrays.java
+++ src/main/revision/october/meta/easy/IntersectionOfTwoArrays.java
@@ -87,4 +87,25 @@ public class IntersectionOfTwoArrays {
             return result.stream().mapToInt(Integer::intValue).toArray();
         }
     }
+
+    // revised on 2/10/2026
+    class SolutionRevisedOnDayThirty {
+        public int[] intersection(int[] nums1, int[] nums2) {
+
+            Set<Integer> set = new HashSet<>();
+            List<Integer> list = new ArrayList<>();
+
+            for (int num : nums1) {
+                set.add(num);
+            }
+
+            for (int num : nums2) {
+                if (set.remove(num)) {
+                    list.add(num);
+                }
+            }
+            return list.stream().mapToInt(Integer::intValue).toArray();
+
+        }
+    }
 }
