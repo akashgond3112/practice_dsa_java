c689b4dcc80ea194f719dc26f2c21027976afac9
akashgond3112 <akashgond3112@gmail.com>
2026-03-17 16:48:08 +0000

    Add TreeNode class: implement a versatile tree node structure with key-value pairs and linked references

---
+++ src/main/revision/march/TreeNode.java
@@ -0,0 +1,39 @@
+package main.revision.march;
+
+public class TreeNode {
+    public int key;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode next;
+    public TreeNode prev;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int key, int value) {
+        this.key = key;
+        this.val = value;
+        this.next = null;
+        this.prev = null;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right, TreeNode next, TreeNode prev) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        this.next = next;
+        this.prev = prev;
+    }
+}
