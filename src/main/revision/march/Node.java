89ccc955e2625f1ea8424fc17a7864daed6d1ee8
akashgond3112 <akashgond3112@gmail.com>
2026-03-17 08:25:33 +0000

    Add Node class: implement a versatile node structure for tree and graph representations

---
+++ src/main/revision/march/Node.java
@@ -0,0 +1,31 @@
+package main.revision.march;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+    public Node next;
+    public Node random;
+    public List<Node> neighbors;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
