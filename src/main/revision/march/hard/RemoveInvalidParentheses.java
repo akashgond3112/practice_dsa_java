3885fa56cfb5029f61067acbf50e54767ed8d959
akashgond3112 <akashgond3112@gmail.com>
2026-03-18 16:46:34 +0000

    Add RemoveInvalidParentheses class: implement a solution to remove invalid parentheses from a string

---
+++ src/main/revision/march/hard/RemoveInvalidParentheses.java
@@ -0,0 +1,63 @@
+package main.revision.march.hard;
+
+import java.util.*;
+
+public class RemoveInvalidParentheses {
+
+    // 18/03/2026
+    class SolutionOnDayFirst {
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        public List<String> removeInvalidParentheses(String s) {
+
+            solve(s, getMinInvalid(s));
+
+            return res;
+        }
+
+        private int getMinInvalid(String s) {
+            int open = 0;
+            int close = 0;
+
+            for (char c : s.toCharArray()) {
+
+                if (c == '(') {
+                    open++;
+                } else if (c == ')') {
+                    if (open > 0) {
+                        open--;
+                    }
+                    close++;
+                }
+            }
+
+            return open + close;
+        }
+
+        private void solve(String s, int invalid) {
+
+            if (map.containsKey(s)) {
+                return;
+            }
+
+            map.put(s, 1);
+
+            if (invalid < 0) {
+                return;
+            }
+
+            if (getMinInvalid(s) == 0) {
+                res.add(s);
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+
+                String left = s.substring(0, i);
+                String right = s.substring(i + 1);
+
+                solve(left + right, i - 1);
+            }
+        }
+    }
+}
