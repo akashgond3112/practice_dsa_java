edadeb0f640144f5c5d93ab42c5e1ee842e7e71d
akashgond3112 <akashgond3112@gmail.com>
2026-03-19 07:31:53 +0000

    Add TrappingRainWater class: implement a solution to calculate trapped rainwater

---
+++ src/main/revision/march/hard/TrappingRainWater.java
@@ -0,0 +1,38 @@
+package main.revision.march.hard;
+
+public class TrappingRainWater {
+
+    // 19/03/2026
+    class SolutionOnDayFirst {
+        public int trap(int[] height) {
+
+            int total = 0;
+            int leftMax = height[0];
+            int rightMax = height[height.length - 1];
+
+            int left = 1;
+            int right = height.length - 2;
+
+            while (left < right) {
+
+                if (leftMax < rightMax) {
+                    if (height[left] >= leftMax) {
+                        leftMax = height[left];
+                    } else {
+                        total += leftMax - height[left];
+                    }
+                    left++;
+                } else {
+                    if (height[right] >= rightMax) {
+                        rightMax = height[right];
+                    } else {
+                        total += rightMax - height[right];
+                    }
+                    right--;
+                }
+
+            }
+            return total;
+        }
+    }
+}
