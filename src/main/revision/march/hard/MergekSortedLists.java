c64ca3459419cf229a6bbf7ec682cbe87c6ba62f
akashgond3112 <akashgond3112@gmail.com>
2026-03-19 07:31:41 +0000

    Add SolutionRevisedOnDayThird class: implement an alternative solution to merge k sorted linked lists

--- src/main/revision/march/hard/MergekSortedLists.java
+++ src/main/revision/march/hard/MergekSortedLists.java
@@ -37,4 +37,39 @@ public class MergekSortedLists {
             return dummy.next;
         }
     }
+
+    // 19/03/2026
+    class SolutionRevisedOnDayThird {
+        public ListNode mergeKLists(ListNode[] lists) {
+
+            if (lists == null || lists.length == 0) {
+                return null;
+            }
+
+            PriorityQueue<ListNode> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.val));
+
+            for (ListNode node : lists) {
+                if (node != null) {
+                    pq.offer(node);
+                }
+            }
+
+            ListNode dummy = new ListNode(0);
+            ListNode cur = dummy;
+
+            while (!pq.isEmpty()) {
+
+                ListNode node = pq.poll();
+
+                cur.next = node;
+                cur = cur.next;
+
+                if (node.next != null) {
+                    pq.offer(node.next);
+                }
+            }
+
+            return dummy.next;
+        }
+    }
 }
