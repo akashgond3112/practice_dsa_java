a8ae87dfb99e2f87323c85178a9822710b8bb809
akashgond3112 <akashgond3112@gmail.com>
2026-03-20 14:17:04 +0000

    Add SolutionRevisedOnDayThird class: implement a refined solution to validate if a string is a valid number

--- src/main/revision/march/hard/ValidNumber.java
+++ src/main/revision/march/hard/ValidNumber.java
@@ -44,4 +44,46 @@ public class ValidNumber {
             return true;
         }
     }
+
+    // 20/03/2026
+    class SolutionRevisedOnDayThird {
+        public boolean isNumber(String s) {
+
+            boolean seenDigit = false;
+            boolean seenDot = false;
+            boolean seenExponent = false;
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (Character.isDigit(c)) {
+                    seenDigit = true;
+                } else if (c == '+' || c == '-') {
+                    if (i != 0 || s.charAt(i - 1) != 'e' || s.charAt(i - 1) != 'e') {
+                        return false;
+                    }
+                    seenDigit = false;
+                } else if (c == '.') {
+                    if (seenDot || seenExponent) {
+                        return false;
+                    }
+                    seenDot = true;
+                } else if (c == 'e' || c == 'E') {
+                    if (seenExponent || !seenDigit) {
+                        return false;
+                    }
+                    seenExponent = true;
+                    seenDigit = false;
+                } else {
+                    seenDigit = false;
+                }
+            }
+
+            if (!seenDigit) {
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
