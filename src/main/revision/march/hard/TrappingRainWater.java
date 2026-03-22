afc1bbf7e5b6568518e17f4d4f1502c4d875e9d1
akashgond3112 <akashgond3112@gmail.com>
2026-03-22 15:34:30 +0000

    Add SolutionRevisedOnDayThird class: implement a solution to trap rainwater using a two-pointer approach

--- src/main/revision/march/hard/TrappingRainWater.java
+++ src/main/revision/march/hard/TrappingRainWater.java
@@ -35,4 +35,38 @@ public class TrappingRainWater {
             return total;
         }
     }
+
+    // 22/03/2026
+    class SolutionRevisedOnDayThird {
+        public int trap(int[] height) {
+
+            int total = 0;
+            int leftMax = height[0];
+            int rightMax = height[height.length - 1];
+
+            int left = 1;
+            int right = height.length - 2;
+
+            while (left < right) {
+
+                if (leftMax < rightMax) {
+
+                    if (height[left] >= leftMax) {
+                        leftMax = height[left];
+                    } else {
+                        total += leftMax - height[left];
+                    }
+                } else {
+
+                    if (height[right] >= rightMax) {
+                        rightMax = height[right];
+                    } else {
+                        total += rightMax - height[right];
+                    }
+                }
+            }
+
+            return total;
+        }
+    }
 }
