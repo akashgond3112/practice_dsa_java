cdd23d1d8ff001afc01732e3a8b1a1d1c8e22d9d
akashgond3112 <akashgond3112@gmail.com>
2026-03-26 05:43:39 +0000

    Add SolutionRevisedOnDayFifth class: implement enhanced isNumber method with improved validation for signs, dots, and exponents

--- src/main/revision/march/hard/ValidNumber.java
+++ src/main/revision/march/hard/ValidNumber.java
@@ -86,4 +86,52 @@ public class ValidNumber {
             return true;
         }
     }
+
+    // 26/03/2026
+    class SolutionRevisedOnDayFifth {
+        public boolean isNumber(String s) {
+
+            boolean seenDigit = false;
+            boolean seenDot = false;
+            boolean seenExponent = false;
+
+            for (int i = 0; i < s.length(); i++) {
+
+                char c = s.charAt(i);
+
+                if (Character.isDigit(c)) {
+                    seenDigit = true;
+                } else if (c == '+' || c == '-') {
+
+                    if (i != 0 || s.charAt(i - 1) != 'e' || s.charAt(i - 1) != 'E') {
+                        return false;
+                    }
+
+                    seenDigit = false;
+                } else if (c == '.') {
+                    if (seenDot || !seenDigit) {
+                        return false;
+                    }
+
+                    seenDot = true;
+                } else if (c == 'e' || c == 'E') {
+                    if (seenExponent || !seenDigit) {
+                        return false;
+                    }
+
+                    seenExponent = true;
+                    seenDigit = false;
+                } else {
+                    seenDigit = false;
+                }
+
+            }
+
+            if (!seenDigit) {
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
