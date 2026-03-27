68e6261a45bceb25df117d59cd56827e32b18e9b
akashgond3112 <akashgond3112@gmail.com>
2026-03-27 11:22:18 +0000

    Refactor MinimumWindowSubstring class: improve minWindow method with enhanced logic for calculating minimum window substring

--- src/main/revision/march/hard/MinimumWindowSubstring.java
+++ src/main/revision/march/hard/MinimumWindowSubstring.java
@@ -8,55 +8,116 @@ public class MinimumWindowSubstring {
     class SolutionOnDayFirst {
         public String minWindow(String s, String t) {
 
-            if (t.length() > s.length()) {
-                return "";
-            }
+            int n = s.length();
+            int m = t.length();
 
             Map<Character, Integer> map = new HashMap<>();
 
-            for (char c : t.toCharArray()) {
+            for (int i = 0; i < m; i++) {
+                char c = t.charAt(i);
 
                 map.put(c, map.getOrDefault(c, 0) + 1);
             }
 
+            int left = 0;
+            int right = 0;
             int required = map.size();
             int formed = 0;
-            int min = Integer.MAX_VALUE;
+
+            Map<Character, Integer> windowCounts = new HashMap<>();
+
+            int minLength = Integer.MAX_VALUE;
             int start = 0;
-            int left = 0;
 
-            Map<Character, Integer> windowsCount = new HashMap<>();
-
-            for (int right = 0; right < s.length(); right++) {
+            while (right < n) {
 
                 char c = s.charAt(right);
+                windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
 
-                windowsCount.put(c, windowsCount.getOrDefault(c, 0) + 1);
-
-                if (map.containsKey(c) && windowsCount.get(c) == map.get(c)) {
+                if (map.containsKey(c) && windowCounts.get(c).intValue() == map.get(c).intValue()) {
                     formed++;
-
-                    while (left <= right && formed == required) {
-
-                        char ch = s.charAt(left);
-
-                        if (left - right + 1 < min) {
-                            min = left - right + 1;
-                            start = left;
-                        }
-
-                        windowsCount.put(ch, windowsCount.get(ch) - 1);
-
-                        if (map.containsKey(ch) && windowsCount.get(ch) < map.get(ch)) {
-                            formed--;
-                        }
-                        left++;
-                    }
                 }
+
+                while (left <= right && formed == required) {
+                    char ch = s.charAt(left);
+
+                    // calculate window size
+                    if (right - left + 1 < minLength) {
+                        minLength = right - left + 1;
+                        start = left;
+                    }
+
+                    windowCounts.put(ch, windowCounts.getOrDefault(ch, 0) - 1);
+
+                    if (map.containsKey(ch) && windowCounts.get(ch) < map.get(ch)) {
+                        formed--;
+                    }
+
+                    left++;
+                }
+
+                right++;
             }
 
-            return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
+            return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
 
+        }
+    }
+
+    // 27/03/2026
+    class SolutionRevisedOnDayFirst {
+        public String minWindow(String s, String t) {
+
+            int n = s.length();
+            int m = t.length();
+
+            Map<Character, Integer> map = new HashMap<>();
+
+            for (char c : t.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+            int left = 0;
+            int right = 0;
+            int required = map.size();
+            int formed = 0;
+
+            Map<Character, Integer> count = new HashMap<>();
+
+            int minLength = Integer.MAX_VALUE;
+            int start = 0;
+
+            while (right < n) {
+
+                char c = s.charAt(right);
+                count.put(c, count.getOrDefault(c, 0) + 1);
+
+                if (count.containsKey(c) && count.get(c) == map.get(c)) {
+                    formed++;
+                }
+
+                while (left <= right && formed == required) {
+
+                    char cur = s.charAt(left);
+
+                    if (right - left + 1 < minLength) {
+                        minLength = right - left + 1;
+                        start = left;
+                    }
+
+                    count.put(cur, count.getOrDefault(cur, 0) - 1);
+
+                    if (map.containsKey(cur) && count.get(cur) < map.get(cur)) {
+                        formed--;
+                    }
+                    left++;
+
+                }
+                right++;
+
+            }
+
+            return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
         }
     }
 }
