f96328080906cab1411fab75bcd7cb6e3b75a3b3
akashgond3112 <akashgond3112@gmail.com>
2026-03-27 11:33:27 +0000

    Add SolutionRevisedOnDayFifth class: implement removeInvalidParentheses method with improved logic for handling invalid parentheses

--- src/main/revision/march/hard/RemoveInvalidParentheses.java
+++ src/main/revision/march/hard/RemoveInvalidParentheses.java
@@ -119,4 +119,60 @@ public class RemoveInvalidParentheses {
             }
         }
     }
+
+    // 27/03/2026
+    class SolutionRevisedOnDayFifth {
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        public List<String> removeInvalidParentheses(String s) {
+
+            solve(s, getMinInvalid(s));
+
+            return res;
+        }
+
+        private int getMinInvalid(String s) {
+            int open = 0;
+            int close = 0;
+
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    open++;
+                } else {
+                    if (open > 0) {
+                        close--;
+                    }
+                    close++;
+                }
+            }
+
+            return open + close;
+        }
+
+        private void solve(String s, int invalid) {
+
+            if (map.containsKey(s)) {
+                return;
+            }
+
+            map.put(s, 1);
+
+            if (invalid < 0) {
+                return;
+            }
+
+            if (getMinInvalid(s) == 0) {
+                res.add(s);
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+
+                String left = s.substring(0, i);
+                String right = s.substring(i + 1);
+
+                solve(left + right, i - 1);
+            }
+        }
+    }
 }
