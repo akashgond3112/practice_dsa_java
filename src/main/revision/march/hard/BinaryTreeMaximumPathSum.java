9fc755e90b3c10120cf52cd969f55224e377ed49
akashgond3112 <akashgond3112@gmail.com>
2026-03-31 10:55:37 +0100

    Add SolutionRevisedOnDayThird class: modify dfs method to use Math.min for path sum calculation

--- src/main/revision/march/hard/BinaryTreeMaximumPathSum.java
+++ src/main/revision/march/hard/BinaryTreeMaximumPathSum.java
@@ -28,4 +28,29 @@ public class BinaryTreeMaximumPathSum {
             return root.val + Math.max(leftMax, rightMax);
         }
     }
+
+    // 30/03/2026
+    class SolutionRevisedOnDayThird {
+        public int maxPathSum(TreeNode root) {
+            int[] res = new int[] { root.val };
+
+            dfs(root, res);
+
+            return res[0];
+        }
+
+        private int dfs(TreeNode root, int[] res) {
+
+            if (root == null) {
+                return 0;
+            }
+
+            int leftMax = Math.max(dfs(root.left, res), 0);
+            int rightMax = Math.max(dfs(root.right, res), 0);
+
+            res[0] = Math.max(res[0], root.val + leftMax + rightMax);
+
+            return root.val + Math.min(leftMax, rightMax);
+        }
+    }
 }
