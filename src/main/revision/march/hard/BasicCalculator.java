06fc1c54023d6f09005b968a0513efbd4ecf9c52
akashgond3112 <akashgond3112@gmail.com>
2026-04-02 14:20:13 +0100

    Add BasicCalculator class: implement calculate method for evaluating basic arithmetic expressions

---
+++ src/main/revision/march/hard/BasicCalculator.java
@@ -0,0 +1,53 @@
+package main.revision.march.hard;
+
+import java.util.*;
+
+public class BasicCalculator {
+
+    static class CalculatorState {
+        int sign;
+        int output;
+
+        CalculatorState(int sign, int output) {
+            this.sign = sign;
+            this.output = output;
+        }
+    }
+
+    // 02/04/2026
+    class SolutionOnDayFirst {
+        public int calculate(String s) {
+
+            int output = 0;
+            int currentNumber = 0;
+            int sign = 1;
+
+            Stack<CalculatorState> st = new Stack<>();
+
+            for (char c : s.toCharArray()) {
+
+                if (Character.isDigit(c)) {
+                    currentNumber = currentNumber * 10 + (c - '0');
+                } else if (c == '+' || c == '-') {
+
+                    output += currentNumber * sign;
+                    currentNumber = 0;
+                    sign = c == '+' ? 1 : -1;
+                } else if (c == '(') {
+                    st.push(new CalculatorState(sign, output));
+                    output = 0;
+                    sign = 1;
+                } else if (c == ')') {
+                    output += currentNumber * sign;
+                    currentNumber = 0;
+
+                    CalculatorState prev = st.pop();
+
+                    output += prev.output + (output * prev.sign);
+                }
+            }
+
+            return output + (currentNumber * sign);
+        }
+    }
+}
