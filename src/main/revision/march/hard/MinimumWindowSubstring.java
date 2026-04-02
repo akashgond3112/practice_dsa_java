268fdf8a52e10cdff52a60903d9e2ab2655715c9
akashgond3112 <akashgond3112@gmail.com>
2026-04-02 14:20:33 +0100

    Add SolutionRevisedOnDayThir class: implement minWindow method for finding the minimum window substring

--- src/main/revision/march/hard/MinimumWindowSubstring.java
+++ src/main/revision/march/hard/MinimumWindowSubstring.java
@@ -73,7 +73,9 @@ public class MinimumWindowSubstring {
 
             Map<Character, Integer> map = new HashMap<>();
 
-            for (char c : t.toCharArray()) {
+            for (int i = 0; i < m; i++) {
+                char c = t.charAt(i);
+
                 map.put(c, map.getOrDefault(c, 0) + 1);
             }
 
@@ -118,6 +120,61 @@ public class MinimumWindowSubstring {
             }
 
             return minLength == Integer.MAX_VALUE ? "" : s.substring(start, start + minLength);
+        }
+    }
+
+    // 02/04/2026
+    class SolutionRevisedOnDayThir {
+        public String minWindow(String s, String t) {
+
+            if (t.length() > s.length()) {
+                return "";
+            }
+
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : t.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+            int formed = 0;
+            int required = map.size();
+            int left = 0;
+            int start = 0;
+            int min = Integer.MIN_VALUE;
+
+            Map<Character, Integer> winCount = new HashMap<>();
+
+            for (int right = 0; right < s.length(); right++) {
+
+                char c = s.charAt(right);
+
+                winCount.put(c, winCount.getOrDefault(c, 0) + 1);
+
+                if (map.containsKey(c) && map.get(c) == winCount.get(c)) {
+                    formed++;
+                }
+
+                while (left <= right && formed == required) {
+                    char ch = s.charAt(left);
+
+                    if (right - left + 1 < min) {
+                        min = right - left + 1;
+                        start = left;
+                    }
+
+                    winCount.put(ch, winCount.getOrDefault(ch, 0) - 1);
+
+                    if (map.containsKey(ch) && winCount.get(ch) < map.get(ch)) {
+                        formed--;
+                    }
+                    left++;
+
+                }
+
+            }
+
+            return min == Integer.MIN_VALUE ? "" : s.substring(start, start + min);
+
         }
     }
 }
