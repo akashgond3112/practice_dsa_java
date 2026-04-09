bf0ba9669ffb596b37eb3ca62ddbcca53c8bae06
akashgond3112 <akashgond3112@gmail.com>
2026-04-09 18:30:11 +0100

    Add MedianOfTwoSortedArrays class: implement method to find median of two sorted arrays

---
+++ src/main/revision/march/hard/MedianOfTwoSortedArrays.java
@@ -0,0 +1,65 @@
+package main.revision.march.hard;
+
+public class MedianOfTwoSortedArrays {
+
+    // 08/04/2026
+    class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+            int n1 = nums1.length;
+            int n2 = nums2.length;
+
+            if (n1 > n2) {
+                findMedianSortedArrays(nums2, nums1);
+            }
+
+            int low = 0;
+            int high = n1;
+
+            int left = (n1 + n2 + 1) / 2;
+            int n = n1 + n2;
+
+            while (low < high) {
+
+                int m1 = (low + high) / 2;
+                int m2 = left - m1;
+
+                int l1 = Integer.MIN_VALUE;
+                int l2 = Integer.MIN_VALUE;
+
+                int r1 = Integer.MAX_VALUE;
+                int r2 = Integer.MAX_VALUE;
+
+                if (m1 - 1 >= 0) {
+                    l1 = nums1[m1 - 1];
+                }
+
+                if (m2 - 1 >= 0) {
+                    l2 = nums2[m2 - 1];
+                }
+
+                if (m1 < n) {
+                    r1 = nums2[m1];
+                }
+
+                if (m2 < n) {
+                    r2 = nums2[m2];
+                }
+
+                if (l1 <= r2 && l2 <= r1) {
+                    if (n % 2 == 1) {
+                        return Math.max(l1, l2);
+                    } else {
+                        return Math.max(l1, l2) + Math.min(r1, r2) / 2.0;
+                    }
+                } else if (l1 > l2) {
+                    high = m1 - 1;
+                } else {
+                    low = m1 + 1;
+                }
+            }
+
+            return 0.0;
+        }
+    }
+}
