bc29650fa43fb7b1947a1c7d73b1b3aaf2b690e1
akashgond3112 <akashgond3112@gmail.com>
2026-04-10 19:41:08 +0100

    Add SolutionRevisedOnDayFourteen class: implement isNumber method to validate numeric strings with exponent and sign handling

--- src/main/revision/march/hard/ValidNumber.java
+++ src/main/revision/march/hard/ValidNumber.java
@@ -134,4 +134,50 @@ public class ValidNumber {
             return true;
         }
     }
+
+    // 09/04/2026
+    class SolutionRevisedOnDayFourteen {
+        public boolean isNumber(String s) {
+
+            boolean seenDigit = false;
+            boolean seenExponent = false;
+            boolean seenDot = false;
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (Character.isDigit(c)) {
+                    seenDigit = true;
+                } else if (c == 'e' || c == 'E') {
+                    if (seenExponent || !seenDigit) {
+                        return false;
+                    }
+
+                    seenExponent = true;
+                    seenDigit = false;
+                } else if (c == '+' || c == '-') {
+
+                    if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                        return false;
+                    }
+                    seenDigit = false;
+                } else if (c == '.') {
+
+                    if (seenDot || seenExponent) {
+                        return false;
+                    }
+                    seenDot = true;
+                } else {
+                    seenDigit = false;
+                }
+            }
+
+            if (!seenDigit) {
+                return false;
+            }
+
+            else
+                return true;
+        }
+    }
 }
