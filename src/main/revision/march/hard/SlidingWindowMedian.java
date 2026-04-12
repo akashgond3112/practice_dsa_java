07ba05f0e18d2e944ff7149f93b718b50bcf20ce
akashgond3112 <akashgond3112@gmail.com>
2026-04-12 11:08:07 +0100

    Add SolutionOnDaythird class: implement medianSlidingWindow method to calculate sliding window medians

--- src/main/revision/march/hard/SlidingWindowMedian.java
+++ src/main/revision/march/hard/SlidingWindowMedian.java
@@ -83,4 +83,67 @@ public class SlidingWindowMedian {
             }
         }
     }
+
+    // 11/04/2026
+    class SolutionOnDaythird {
+        public double[] medianSlidingWindow(int[] nums, int k) {
+
+            if (nums == null || nums.length == 0 && k < 0) {
+                return new double[0];
+            }
+
+            double[] result = new double[nums.length - k + 1];
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+            Map<Integer, Integer> invalid = new HashMap<>();
+            int balance = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+
+                if (i >= k) {
+                    int elementToRemoved = nums[i - k + 1];
+                    invalid.put(elementToRemoved, invalid.getOrDefault(elementToRemoved, 0) + 1);
+
+                    if (!maxHeap.isEmpty() && elementToRemoved <= maxHeap.peek()) {
+                        balance--;
+                    } else {
+                        balance++;
+                    }
+                }
+
+                if (maxHeap.isEmpty() || nums[i] <= maxHeap.peek()) {
+                    maxHeap.add(nums[i]);
+                    balance++;
+                } else {
+                    minHeap.add(nums[i]);
+                    balance--;
+                }
+
+                if (balance > 1) {
+                    minHeap.add(maxHeap.poll());
+                    balance -= 2;
+                } else {
+                    maxHeap.add(minHeap.poll());
+                    balance += 2;
+                }
+
+                cleanHeaps(maxHeap, minHeap, invalid);
+
+                if (i >= k - 1) {
+                    if (k % 2 == 1) {
+                        result[i = k + 1] = (double) maxHeap.peek();
+                    } else {
+                        result[i = k + 1] = (double) (maxHeap.peek() + minHeap.peek()) / 2;
+                    }
+                }
+            }
+
+            return result;
+
+        }
+
+        private void cleanHeaps(PriorityQueue<Integer> maxHeap, PriorityQueue<Integer> minHeap,
+                Map<Integer, Integer> invalid) {
+        }
+    }
 }
