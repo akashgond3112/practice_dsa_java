dd7967d3b513abb1eee996af11e4a6d18e10dd48
akashgond3112 <akashgond3112@gmail.com>
2026-04-12 11:07:38 +0100

    Refactor SolutionRevisedOnDayFifth class: rename to SolutionRevisedOnDaySeventh and implement multiplication operator handling in addOperators method

--- src/main/revision/march/hard/ExpressionAddOperators.java
+++ src/main/revision/march/hard/ExpressionAddOperators.java
@@ -106,7 +106,7 @@ public class ExpressionAddOperators {
     }
 
     // 30/03/2026
-    class SolutionRevisedOnDayFifth {
+    class SolutionRevisedOnDaySeventh {
         public List<String> addOperators(String num, int target) {
 
             List<String> result = new ArrayList<>();
@@ -145,9 +145,56 @@ public class ExpressionAddOperators {
 
                     dfs(result, num, target, i + 1, (expr + "+" + current), eval + currentNum, currentNum);
                     dfs(result, num, target, i + 1, (expr + "-" + current), eval - currentNum, -currentNum);
-                    dfs(result, num, target, i + 1, (expr + "-" + current),
+                    dfs(result, num, target, i + 1, (expr + "*" + current),
                             (eval - lastNum + (lastNum * currentNum)),
                             lastNum * currentNum);
+
+                }
+            }
+        }
+    }
+
+    // 30/03/2026
+    class SolutionRevisedOnDayFifth {
+        public List<String> addOperators(String num, int target) {
+
+            List<String> result = new ArrayList<>();
+
+            if (num == null || num.isEmpty()) {
+                return result;
+            }
+
+            dfs(num, target, result, "", 0, 0, 0);
+
+            return result;
+        }
+
+        private void dfs(String num, int target, List<String> result, String expr, int index, long evaluated,
+                long lastNumber) {
+
+            if (index == num.length()) {
+                if (evaluated == target) {
+                    result.add(expr);
+                }
+                return;
+            }
+
+            for (int i = index; i < num.length(); i++) {
+
+                if (i > index && num.charAt(i) == '0')
+                    break;
+
+                String current = num.substring(index, i + 1);
+                long currentNum = Long.parseLong(current);
+
+                if (index == 0) {
+                    dfs(num, target, result, current, index + 1, currentNum, currentNum);
+                } else {
+                    dfs(num, target, result, (expr + "+" + current), i + 1, evaluated + currentNum, currentNum);
+                    dfs(num, target, result, (expr + "-" + current), i + 1, evaluated - currentNum, -currentNum);
+                    dfs(num, target, result, (expr + "*" + current), i + 1,
+                            evaluated - lastNumber + (lastNumber * currentNum),
+                            lastNumber * currentNum);
 
                 }
             }
