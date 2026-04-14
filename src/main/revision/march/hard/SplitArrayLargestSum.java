46b1d331e88ec0a34c80693092f37d253d404296
akashgond3112 <akashgond3112@gmail.com>
2026-04-14 19:21:30 +0100

    Add SolutionRevisedOnDayThird class: implement splitArray method to find the largest sum of split arrays

--- src/main/revision/march/hard/SplitArrayLargestSum.java
+++ src/main/revision/march/hard/SplitArrayLargestSum.java
@@ -45,4 +45,47 @@ public class SplitArrayLargestSum {
             return count <= k;
         }
     }
+
+    // 14/04/2026
+    class SolutionRevisedOnDayThird {
+        public int splitArray(int[] nums, int k) {
+
+            int low = 0;
+            int high = 0;
+            int ans = high;
+            for (int num : nums) {
+                low = Math.min(low, num);
+                high += num;
+            }
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if (isPossible(nums, k, mid)) {
+                    ans = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            return ans;
+        }
+
+        private boolean isPossible(int[] nums, int k, int mid) {
+            int count = 0;
+            int sum = 0;
+
+            for (int num : nums) {
+                if (sum + num <= mid) {
+                    sum += num;
+                } else {
+                    count++;
+                    sum = num;
+                }
+            }
+
+            return count <= k;
+        }
+    }
 }
