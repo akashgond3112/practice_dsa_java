338edd279a6ab830be3f0be8c336222d8234d2ea
akashgond3112 <akashgond3112@gmail.com>
2026-04-16 20:12:26 +0100

    Refactor MinimumWindowSubstring class: correct class names and improve minWindow method implementation for better clarity and functionality

--- src/main/revision/march/hard/MinimumWindowSubstring.java
+++ src/main/revision/march/hard/MinimumWindowSubstring.java
@@ -65,7 +65,7 @@ public class MinimumWindowSubstring {
     }
 
     // 27/03/2026
-    class SolutionRevisedOnDayFirst {
+    class SolutionRevisedOnDayThird {
         public String minWindow(String s, String t) {
 
             int n = s.length();
@@ -124,7 +124,7 @@ public class MinimumWindowSubstring {
     }
 
     // 02/04/2026
-    class SolutionRevisedOnDayThir {
+    class SolutionRevisedOnDaySeventh {
         public String minWindow(String s, String t) {
 
             if (t.length() > s.length()) {
@@ -140,7 +140,7 @@ public class MinimumWindowSubstring {
             int required = map.size();
             int left = 0;
             int start = 0;
-            int min = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
 
             Map<Character, Integer> winCount = new HashMap<>();
 
@@ -173,8 +173,68 @@ public class MinimumWindowSubstring {
 
             }
 
-            return min == Integer.MIN_VALUE ? "" : s.substring(start, start + min);
+            return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
 
+        }
+    }
+
+    // 26/04/2026
+    class SolutionRevisedOnDayFourteen {
+        public String minWindow(String s, String t) {
+
+            if (s.length() < t.length())
+                return "";
+            int startIndex = 0;
+            int endIndex = 0;
+            int max = Integer.MAX_VALUE; // Initialize max with a smaller value
+            int count = 0;
+
+            Map<Character, Integer> characterHashMap = new HashMap<>();
+
+            for (char c : t.toCharArray()) {
+                characterHashMap.put(c, characterHashMap.getOrDefault(c, 0) + 1);
+            }
+
+            count = characterHashMap.size();
+
+            int maxStart = 0; // to track Start index of substring
+            int maxEnd = 0; // to track End index of substring
+
+            while (endIndex < s.length()) {
+                char ch = s.charAt(endIndex);
+                if (characterHashMap.containsKey(ch)) {
+                    int freq = characterHashMap.get(s.charAt(endIndex));
+                    freq--;
+                    characterHashMap.put(ch, freq);
+                    if (characterHashMap.get(ch) == 0)
+                        count--;
+
+                }
+
+                while (count == 0) {
+
+                    // Get best solution
+                    if (max > endIndex - startIndex + 1) {
+                        max = endIndex - startIndex + 1;
+                        maxStart = startIndex;
+                        maxEnd = endIndex + 1;
+                    }
+
+                    char tempCharStart = s.charAt(startIndex);
+                    if (characterHashMap.containsKey(tempCharStart)) {
+                        int freq = characterHashMap.get(tempCharStart);
+                        freq++;
+                        characterHashMap.put(tempCharStart, freq);
+                        if (characterHashMap.get(tempCharStart) > 0) {
+                            count++;
+                        }
+                    }
+                    startIndex++;
+                }
+                endIndex++;
+            }
+
+            return s.substring(maxStart, maxEnd);
         }
     }
 }
