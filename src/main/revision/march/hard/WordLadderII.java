3f703e4859a4d193571223e0d4a8c54d0bdf1ee2
akashgond3112 <akashgond3112@gmail.com>
2026-04-17 19:59:17 +0100

    Add WordLadderII class: implement findLadders method to find all shortest transformation sequences from beginWord to endWord

---
+++ src/main/revision/march/hard/WordLadderII.java
@@ -0,0 +1,68 @@
+package main.revision.march.hard;
+
+import java.util.*;
+
+public class WordLadderII {
+
+    class Solution {
+        public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+
+            Set<String> set = new HashSet<>(wordList);
+
+            Queue<List<String>> queue = new LinkedList<>();
+            List<String> list = new ArrayList<>();
+            list.add(beginWord);
+            queue.add(list);
+
+            List<String> useOnLevel = new ArrayList<>();
+            useOnLevel.add(beginWord);
+            int level = 0;
+
+            List<List<String>> result = new ArrayList<>();
+
+            while (!queue.isEmpty()) {
+
+                List<String> cur = queue.poll();
+
+                if (cur.size() > level) {
+                    level++;
+                    for (String word : useOnLevel) {
+                        set.remove(word);
+                    }
+                }
+
+                String word = cur.get(cur.size() - 1);
+
+                if (word.equals(endWord)) {
+                    if (result.isEmpty()) {
+                        result.add(cur);
+                    } else if (result.get(0).size() == cur.size()) {
+                        result.add(cur);
+                    }
+                    continue;
+                }
+
+                for (int i = 0; i < word.length(); i++) {
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char[] chars = word.toCharArray();
+                        chars[i] = c;
+                        String newWord = new String(chars);
+
+                        if (set.contains(newWord)) {
+                            cur.add(newWord);
+
+                            queue.add(new ArrayList<>(cur));
+
+                            useOnLevel.add(newWord);
+
+                            cur.remove(cur.getLast());
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
