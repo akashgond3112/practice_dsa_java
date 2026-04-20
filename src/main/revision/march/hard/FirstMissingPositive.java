4ed0c4037fcfe4e147a13376f5e276a306b8d3ee
akashgond3112 <akashgond3112@gmail.com>
2026-04-20 18:53:19 +0100

    Add FirstMissingPositive class: implement firstMissingPositive method to find the smallest missing positive integer

---
+++ src/main/revision/march/hard/FirstMissingPositive.java
@@ -0,0 +1,38 @@
+package main.revision.march.hard;
+
+public class FirstMissingPositive {
+
+    // 20/04/2026
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+
+            int n = nums.length;
+            int i = 0;
+
+            while (i < n) {
+                int correctIndex = nums[i] - 1;
+
+                if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[correctIndex]) {
+                    swap(nums, i, correctIndex);
+                } else {
+                    i++;
+                }
+            }
+
+            for (i = 0; i < n; i++) {
+                if (nums[i] != i) {
+                    return i + 1;
+                }
+            }
+
+            return n + 1;
+        }
+
+        private void swap(int[] nums, int i, int correctIndex) {
+
+            int tmp = nums[i];
+            nums[i] = nums[correctIndex];
+            nums[correctIndex] = tmp;
+        }
+    }
+}
