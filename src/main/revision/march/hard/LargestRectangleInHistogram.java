85605487828d3c0ead1ce9d2d7405bd92ac4c711
akashgond3112 <akashgond3112@gmail.com>
2026-04-20 18:53:26 +0100

    Add SolutionRevisedOnDayThird class: implement largestRectangleArea method for calculating the largest rectangle in a histogram

--- src/main/revision/march/hard/LargestRectangleInHistogram.java
+++ src/main/revision/march/hard/LargestRectangleInHistogram.java
@@ -34,4 +34,35 @@ public class LargestRectangleInHistogram {
             return maxArea;
         }
     }
+
+    // 20/04/2026
+    class SolutionRevisedOnDayThird {
+        public int largestRectangleArea(int[] heights) {
+            int maxArea = 0;
+
+            Stack<Integer> st = new Stack<>();
+
+            for (int i = 0; i < heights.length; i++) {
+
+                while (!st.isEmpty()) {
+
+                    int curHeight = st.pop();
+                    int pse = st.isEmpty() ? -1 : st.peek();
+                    maxArea = Math.max(maxArea, heights[curHeight] * (i - pse - 1));
+                }
+                st.push(i);
+            }
+
+            while (!st.isEmpty()) {
+
+                int nse = heights.length;
+                int curHeight = st.pop();
+                int pse = st.isEmpty() ? -1 : st.peek();
+                maxArea = Math.max(maxArea, heights[curHeight] * (nse - pse - 1));
+            }
+
+            return maxArea;
+        }
+
+    }
 }
