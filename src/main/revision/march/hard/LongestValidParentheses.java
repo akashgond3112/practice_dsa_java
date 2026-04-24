2f3dcfa16461924adb349637e6b305ddf70b8ea0
akashgond3112 <akashgond3112@gmail.com>
2026-04-24 20:01:25 +0100

    Add SolutionRevisedOnDayThird class: implement longestValidParentheses method with improved logic for counting valid parentheses

--- src/main/revision/march/hard/LongestValidParentheses.java
+++ src/main/revision/march/hard/LongestValidParentheses.java
@@ -2,6 +2,7 @@ package main.revision.march.hard;
 
 public class LongestValidParentheses {
 
+    // 21/04/2026
     class Solution {
         public int longestValidParentheses(String s) {
 
@@ -36,6 +37,47 @@ public class LongestValidParentheses {
                     close++;
                 }
 
+                if (open == close) {
+                    len = Math.max(len, open + close);
+                } else if (open > close) {
+                    open = close = 0;
+                }
+            }
+
+            return len;
+        }
+    }
+
+    // 24/04/2026
+    class SolutionRevisedOnDayThird {
+        public int longestValidParentheses(String s) {
+            int len = 0;
+            int open = 0;
+            int close = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (c == '(') {
+                    open++;
+                } else {
+                    close--;
+                }
+                if (open == close) {
+                    len = Math.max(len, open + close);
+                } else if (open > close) {
+                    open = close = 0;
+                }
+            }
+
+            for (int i = s.length() - 1; i > 0; i--) {
+                char c = s.charAt(i);
+
+                if (c == '(') {
+                    open++;
+                } else {
+                    close--;
+                }
                 if (open == close) {
                     len = Math.max(len, open + close);
                 } else if (open > close) {
