557dd1b9b62851ae044952eb96a0aafc3b7bf205
akashgond3112 <akashgond3112@gmail.com>
2026-04-26 09:34:52 +0100

    Refactor calculate method in SolutionRevisedOnDayFourteen class: streamline state management for parentheses handling

--- src/main/revision/march/hard/BasicCalculator.java
+++ src/main/revision/march/hard/BasicCalculator.java
@@ -104,8 +104,7 @@ public class BasicCalculator {
                     currentNumber = 0;
                     sign = sign == '+' ? 1 : -1;
                 } else if (c == '(') {
-                    CalculatorState cs = new CalculatorState(output, sign);
-                    st.push(cs);
+                    st.push(new CalculatorState(sign, output));
                     output = 0;
                     sign = 1;
                 } else if (c == ')') {
@@ -113,6 +112,40 @@ public class BasicCalculator {
                     currentNumber = 0;
                     CalculatorState prev = st.pop();
                     output += prev.output + (output * prev.sign);
+                }
+            }
+
+            return output + (currentNumber * sign);
+        }
+    }
+
+    // 25/04/2026
+    class SolutionRevisedOnDayFourteen {
+        public int calculate(String s) {
+
+            int output = 0;
+            int currentNumber = 0;
+            int sign = 1;
+            Stack<CalculatorState> st = new Stack<>();
+
+            for (char c : s.toCharArray()) {
+
+                if (Character.isDigit(c)) {
+                    currentNumber += c * 10 + (c - '0');
+                } else if (c == '+' || c == '-') {
+                    output += currentNumber * sign;
+                    currentNumber = 0;
+                    sign = sign == '+' ? 1 : -1;
+                } else if (c == '(') {
+                    st.push(new CalculatorState(sign, output));
+                    output = 0;
+                    sign = 1;
+                } else if (c == ')') {
+                    CalculatorState prevState = st.pop();
+                    output += currentNumber * sign;
+                    currentNumber = 0;
+
+                    output += prevState.output + (output * prevState.sign);
                 }
             }
 
