00db849a29579cd5f357106b17392d304323cad2
akashgond3112 <akashgond3112@gmail.com>
2026-04-26 09:35:17 +0100

    Add SplitArrayWithEqualSum class: implement splitArray method to determine if an array can be split into three equal sum parts

---
+++ src/main/revision/march/hard/SplitArrayWithEqualSum.java
@@ -0,0 +1,35 @@
+package main.revision.march.hard;
+
+public class SplitArrayWithEqualSum {
+
+    // 25/04/2026
+    public class Solution {
+        public boolean splitArray(int[] nums) {
+
+            int total = 0;
+
+            for (int num : nums) {
+                total += num;
+            }
+
+            if (total % 3 != 0) {
+                return false;
+            }
+
+            int targetSum = total / 3;
+            int partionFound = 0;
+            int curSum = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                curSum += nums[i];
+
+                if (curSum == targetSum * (partionFound + 1)) {
+                    partionFound++;
+                }
+            }
+
+            return partionFound >= 3;
+        }
+
+    }
+}
