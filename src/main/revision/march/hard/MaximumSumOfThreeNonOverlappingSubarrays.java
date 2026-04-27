3acd437483b2a6df1a5d788216b0afbc8fc66b80
akashgond3112 <akashgond3112@gmail.com>
2026-04-27 19:34:11 +0100

    Add SolutionRevisedOnDayThird class: implement maxSumOfThreeSubarrays method for calculating maximum sum of three non-overlapping subarrays

--- src/main/revision/march/hard/MaximumSumOfThreeNonOverlappingSubarrays.java
+++ src/main/revision/march/hard/MaximumSumOfThreeNonOverlappingSubarrays.java
@@ -73,4 +73,79 @@ public class MaximumSumOfThreeNonOverlappingSubarrays {
             return mem[position][count];
         }
     }
+
+    // 27/04/2026
+    class SolutionRevisedOnDayThird {
+
+        public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+
+            int n = nums.length;
+
+            int[] prefixSum = new int[n + 1];
+            int[][] mem = new int[2001][3];
+
+            for (int i = 0; i < mem.length; i++) {
+                Arrays.fill(mem[i], -1);
+            }
+
+            prefixSum[0] = 0;
+
+            for (int i = 0; i < n; i++) {
+                prefixSum[i + 1] = prefixSum[i] + nums[i];
+            }
+
+            findMaxSum(nums, k, 0, 0, prefixSum, mem);
+
+            int[] path = new int[3];
+            findMaxSumPath(nums, k, 0, 0, 0, path, prefixSum, mem);
+
+            return path;
+        }
+
+        private void findMaxSumPath(int[] nums, int k, int position, int count, int pathIdx, int[] path,
+                int[] prefixSum, int[][] mem) {
+
+            if (count == 3) {
+                return;
+            }
+
+            if (position > nums.length - k) {
+                return;
+            }
+
+            int dontStart = findMaxSum(nums, k, position + 1, count + 1, prefixSum, mem);
+            int start = findMaxSum(nums, k, position + 1, count + 1, prefixSum, mem) +
+                    prefixSum[position + k] - prefixSum[position];
+
+            if (start >= dontStart) {
+                findMaxSumPath(nums, k, position + k, count + 1, pathIdx + 1, path, prefixSum, mem);
+
+            } else {
+                findMaxSumPath(nums, k, position + k, count + 1, pathIdx, path, prefixSum, mem);
+
+            }
+        }
+
+        private int findMaxSum(int[] nums, int k, int position, int count, int[] prefixSum, int[][] mem) {
+
+            if (count == 3) {
+                return 0;
+            }
+
+            if (position > nums.length - k) {
+                return 0;
+            }
+
+            if (mem[position][count] != -1)
+                return mem[position][count];
+
+            int dontStart = findMaxSum(nums, k, position + 1, count + 1, prefixSum, mem);
+            int start = findMaxSum(nums, k, position + 1, count + 1, prefixSum, mem) +
+                    prefixSum[position + k] - prefixSum[position];
+
+            mem[position][count] = Math.max(dontStart, start);
+
+            return mem[position][count];
+        }
+    }
 }
