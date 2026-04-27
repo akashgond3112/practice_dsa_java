9ef747c2e21e58c885b43b99fd07c1b599e10ae2
akashgond3112 <akashgond3112@gmail.com>
2026-04-27 19:34:28 +0100

    Add SolutionRevisedOnDaySeventh class: implement fullJustify method for improved text justification

--- src/main/revision/march/hard/TextJustification.java
+++ src/main/revision/march/hard/TextJustification.java
@@ -124,4 +124,70 @@ public class TextJustification {
             return result;
         }
     }
+
+    // 27/04/2026
+    class SolutionRevisedOnDaySeventh {
+        public List<String> fullJustify(String[] words, int maxWidth) {
+
+            List<String> result = new ArrayList<>();
+
+            int i = 0;
+            int n = words.length;
+
+            while (i < n) {
+
+                int j = i;
+                int lineLength = 0;
+
+                while (j < n && lineLength + words[j].length() + (j - i) <= maxWidth) {
+                    lineLength = lineLength + words[j].length();
+                    j++;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                int numWords = j - 1;
+                int numSpaces = maxWidth - lineLength;
+
+                if (j == n || numWords == 1) {
+
+                    for (int k = i; k < j; k++) {
+                        sb.append(words[k]);
+
+                        if (k < j - 1) {
+                            sb.append(" ");
+                        }
+                    }
+
+                    while (sb.length() < maxWidth) {
+                        sb.append(" ");
+
+                    }
+                } else {
+
+                    int spacesBetweenWords = numSpaces / (numWords - 1);
+                    int extraSpaces = numSpaces % (numWords - 1);
+
+                    for (int k = i; k < j; k++) {
+                        sb.append(words[k]);
+
+                        if (k < j - 1) {
+                            for (int s = 0; s < spacesBetweenWords; s++) {
+                                sb.append(" ");
+                            }
+
+                            while (extraSpaces > 0) {
+                                sb.append(" ");
+                                extraSpaces--;
+                            }
+                        }
+                    }
+                }
+
+                result.add(sb.toString());
+                i = j;
+            }
+
+            return result;
+        }
+    }
 }
