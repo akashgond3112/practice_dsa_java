ea6edfe0920369d2884b1ba2305a0c26236dc134
akashgond3112 <akashgond3112@gmail.com>
2026-04-29 17:32:50 +0100

    Add SolutionRevisedOnDaySeventh class: implement longestValidParentheses method to find the length of the longest valid parentheses substring

--- src/main/revision/march/hard/LongestValidParentheses.java
+++ src/main/revision/march/hard/LongestValidParentheses.java
@@ -88,4 +88,45 @@ public class LongestValidParentheses {
             return len;
         }
     }
+
+    // 29/04/2026
+    class SolutionRevisedOnDaySeventh {
+        public int longestValidParentheses(String s) {
+            int len = 0;
+            int open = 0;
+            int close = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (c == '(') {
+                    open++;
+                } else {
+                    close--;
+                }
+                if (open == close) {
+                    len = Math.max(len, open + close);
+                } else if (open > close) {
+                    open = close = 0;
+                }
+            }
+
+            for (int i = s.length() - 1; i > 0; i--) {
+                char c = s.charAt(i);
+
+                if (c == '(') {
+                    open++;
+                } else {
+                    close--;
+                }
+                if (open == close) {
+                    len = Math.max(len, open + close);
+                } else if (open > close) {
+                    open = close = 0;
+                }
+            }
+
+            return len;
+        }
+    }
 }
