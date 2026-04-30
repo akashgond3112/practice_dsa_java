b889cca8cd31fc828a80e8739e397e35ee4ece57
akashgond3112 <akashgond3112@gmail.com>
2026-04-30 18:32:29 +0100

    Add SolutionRevisedOnDayThird class: implement Tuple and FreqStack for frequency-based stack operations

--- src/main/revision/march/hard/MaximumFrequencyStack.java
+++ src/main/revision/march/hard/MaximumFrequencyStack.java
@@ -55,4 +55,55 @@ public class MaximumFrequencyStack {
             }
         }
     }
+
+    // 30/04/2026
+    public class SolutionRevisedOnDayThird {
+
+        class Tuple implements Comparable<Tuple> {
+            Integer val;
+            Integer freq;
+            Integer order;
+
+            Tuple(Integer val, Integer freq, Integer order) {
+                this.val = val;
+                this.freq = freq;
+                this.order = order;
+            }
+
+            @Override
+            public int compareTo(Tuple other) {
+                int curFreq = other.freq.compareTo(this.freq);
+
+                return curFreq != 0 ? curFreq : other.order.compareTo(this.order);
+            }
+        }
+
+        class FreqStack {
+            Map<Integer, Integer> map;
+            PriorityQueue<Tuple> heap;
+            int order;
+
+            public FreqStack() {
+                this.map = new HashMap<>();
+                this.heap = new PriorityQueue<>();
+                this.order = 0;
+            }
+
+            public void push(int val) {
+                map.put(val, map.getOrDefault(val, 0) + 1);
+
+                heap.add(new Tuple(val, map.get(val), ++order));
+            }
+
+            public int pop() {
+                Tuple tuple = heap.poll();
+                int val = tuple.val;
+
+                map.put(val, map.get(val) - 1);
+
+                return val;
+            }
+        }
+
+    }
 }
