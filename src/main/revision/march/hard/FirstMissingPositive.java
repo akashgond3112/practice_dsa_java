a6fd34da21f47b20b95520ca81cb21d621faa2f4
akashgond3112 <akashgond3112@gmail.com>
2026-04-30 18:32:11 +0100

    Add SolutionRevisedOnDaySeventh class: implement firstMissingPositive method to find the first missing positive integer

--- src/main/revision/march/hard/FirstMissingPositive.java
+++ src/main/revision/march/hard/FirstMissingPositive.java
@@ -67,4 +67,31 @@ public class FirstMissingPositive {
 
         }
     }
+
+    // 29/04/2026
+    class SolutionRevisedOnDaySeventh {
+        public int firstMissingPositive(int[] nums) {
+
+            int n = nums.length;
+            int i = 0;
+
+            while (i < n) {
+                int correctIndex = nums[i] - 1;
+
+                if (nums[i] > 0 && nums[i] <= n && nums[i] != correctIndex) {
+                    int tmp = nums[i];
+                    nums[i] = nums[correctIndex];
+                    nums[correctIndex] = tmp;
+                }
+            }
+
+            for (i = 0; i < n; i++) {
+                if (nums[i] != i) {
+                    return i + 1;
+                }
+            }
+
+            return n + 1;
+        }
+    }
 }
