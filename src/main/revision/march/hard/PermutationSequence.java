bae8defa9157ab7da3967f951c58b529fdf4b949
akashgond3112 <akashgond3112@gmail.com>
2026-05-03 09:16:59 +0100

    Add PermutationSequence class: implement getPermutation method for generating the k-th permutation sequence of numbers

---
+++ src/main/revision/march/hard/PermutationSequence.java
@@ -0,0 +1,43 @@
+package main.revision.march.hard;
+
+import java.util.*;
+
+public class PermutationSequence {
+
+    // 01/05/2026
+    class Solution {
+        public String getPermutation(int n, int k) {
+
+            int fact = 1;
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                fact *= i;
+                list.add(i);
+            }
+
+            list.add(n);
+
+            StringBuilder sb = new StringBuilder();
+
+            k = k - 1;
+
+            while (true) {
+
+                int index = k / fact;
+
+                sb.append(list.get(index));
+                list.remove(index);
+
+                if (list.isEmpty()) {
+                    break;
+                }
+
+                k = k % fact;
+                fact = fact / list.size();
+            }
+
+            return sb.toString();
+        }
+    }
+}
