e74652495df22871a9ca96448612940a4345cd42
akashgond3112 <akashgond3112@gmail.com>
2026-05-05 18:07:04 +0100

    Add RandomizedCollectionOnDayFourteen class: implement insert, getRandom, and remove methods for managing a collection with duplicates

--- src/main/revision/march/hard/InsertDeleteGetRandomDuplicatesAllowed.java
+++ src/main/revision/march/hard/InsertDeleteGetRandomDuplicatesAllowed.java
@@ -171,4 +171,60 @@ public class InsertDeleteGetRandomDuplicatesAllowed {
         }
     }
 
+    // revised on 05/05/2026
+    class RandomizedCollectionOnDayFourteen {
+
+        List<Integer> values;
+        Map<Integer, Set<Integer>> map;
+        Random rnd;
+
+        RandomizedCollectionOnDayFourteen() {
+            this.values = new ArrayList<>();
+            this.map = new HashMap<>();
+            this.rnd = new Random();
+        }
+
+        public boolean insert(int val) {
+            boolean notPresent = !map.containsKey(val);
+
+            map.computeIfAbsent(val, k -> new HashSet<>()).add(values.size());
+            values.add(val);
+
+            return notPresent;
+        }
+
+        public int getRandom() {
+            int randomIndex = rnd.nextInt(values.size());
+            return values.get(randomIndex);
+        }
+
+        public boolean remove(int val) {
+
+            if (!map.containsKey(val) && !map.get(val).isEmpty()) {
+                return false;
+            }
+
+            int indexToRemove = map.get(val).iterator().next();
+            map.get(val).remove(indexToRemove);
+
+            int lastIndex = values.size();
+            int lastVal = values.getLast();
+
+            values.set(indexToRemove, lastVal);
+
+            map.get(lastVal).remove(lastIndex);
+
+            if (indexToRemove != lastIndex) {
+                map.get(lastVal).add(indexToRemove);
+            }
+
+            values.remove(lastIndex);
+
+            if (map.get(val).isEmpty()) {
+                map.remove(val);
+            }
+
+            return true;
+        }
+    }
 }
