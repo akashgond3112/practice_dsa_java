937f127e648e71c1a0d63e9bed4c96106728ad60
akashgond3112 <akashgond3112@gmail.com>
2026-05-06 18:16:30 +0100

    Add SolutionRevisedOnDaySeventh class: implement FreqStack with push and pop methods for managing frequency of elements

--- src/main/revision/march/hard/MaximumFrequencyStack.java
+++ src/main/revision/march/hard/MaximumFrequencyStack.java
@@ -106,4 +106,59 @@ public class MaximumFrequencyStack {
         }
 
     }
+
+    // 05/06/2026
+    public class SolutionRevisedOnDaySeventh {
+
+        class Tuple implements Comparable<Tuple> {
+
+            Integer val;
+            Integer freq;
+            Integer order;
+
+            Tuple(Integer val, Integer freq, Integer order) {
+                this.val = val;
+                this.freq = freq;
+                this.order = order;
+            }
+
+            @Override
+            public int compareTo(Tuple other) {
+
+                int cur = other.freq.compareTo(this.freq);
+
+                return cur != 0 ? cur : other.order.compareTo(this.order);
+            }
+
+        }
+
+        class FreqStack {
+            Map<Integer, Integer> map;
+            PriorityQueue<Tuple> heap;
+            int order;
+
+            FreqStack() {
+                this.map = new HashMap<>();
+                this.heap = new PriorityQueue<>();
+                this.order = 0;
+            }
+
+            public void push(int val) {
+
+                map.put(val, map.getOrDefault(val, 0) + 1);
+
+                heap.add(new Tuple(val, map.get(val), ++order));
+            }
+
+            public int pop() {
+                Tuple tuple = heap.poll();
+                int val = tuple.val;
+
+                map.put(val, map.get(val) - 1);
+
+                return val;
+            }
+        }
+    }
+
 }
