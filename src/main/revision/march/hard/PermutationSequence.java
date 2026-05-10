d1ca891ca9b454bd7514f2ba9d263ce7f824605c
akashgond3112 <akashgond3112@gmail.com>
2026-05-10 12:16:28 +0100

    Add SolutionRevisedOnDaySeventh class: implement getPermutation method with corrected factorial logic

--- src/main/revision/march/hard/PermutationSequence.java
+++ src/main/revision/march/hard/PermutationSequence.java
@@ -76,4 +76,40 @@ public class PermutationSequence {
             return sb.toString();
         }
     }
+
+    // 10/05/2026
+    class SolutionRevisedOnDaySeventh {
+        public String getPermutation(int n, int k) {
+
+            int fact = 1;
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                fact *= i;
+                list.add(i);
+            }
+
+            list.add(n);
+
+            StringBuilder sb = new StringBuilder();
+
+            k = k - 1;
+
+            while (true) {
+                int index = k / fact;
+
+                sb.append(list.get(index));
+                list.remove(index);
+
+                if (list.isEmpty()) {
+                    break;
+                }
+
+                k = k % fact;
+                fact = fact / list.size();
+            }
+
+            return sb.toString();
+        }
+    }
 }
