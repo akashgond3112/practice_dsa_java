858771a68cf3655f32e73686665b97cf29cc56a4
akashgond3112 <akashgond3112@gmail.com>
2026-05-11 14:20:13 +0100

    Add SolutionRevisedOnDaySeventh class: implement isMatch method with corrected logic for wildcard matching

--- src/main/revision/march/hard/WildcardMatching.java
+++ src/main/revision/march/hard/WildcardMatching.java
@@ -60,4 +60,33 @@ public class WildcardMatching {
             return dp[pLen][sLen];
         }
     }
+
+    // 11/05/2026
+    class SolutionRevisedOnDaySeventh {
+        public boolean isMatch(String text, String pattern) {
+
+            int pLen = pattern.length();
+            int sLen = text.length();
+
+            boolean[][] dp = new boolean[pLen + 1][sLen + 1];
+            dp[0][0] = true;
+
+            for (int i = 1; i < pLen; i++) {
+                dp[i][0] = dp[i - 1][0];
+            }
+
+            for (int i = 1; i <= pLen; i++) {
+                for (int j = 1; j <= sLen; j++) {
+                    if (pattern.charAt(i) == text.charAt(j) || pattern.charAt(i) == '?') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else if (pattern.charAt(i) == '*') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+
+                    }
+                }
+            }
+
+            return dp[pLen][pLen];
+        }
+    }
 }
