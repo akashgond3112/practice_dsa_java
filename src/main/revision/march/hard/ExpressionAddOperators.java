18e015891225ab94df03c21599f2db3e1e48ebb8
akashgond3112 <akashgond3112@gmail.com>
2026-05-11 14:19:54 +0100

    Add SolutionRevisedOnDayThirty class: implement addOperators method for generating expressions that evaluate to target

--- src/main/revision/march/hard/ExpressionAddOperators.java
+++ src/main/revision/march/hard/ExpressionAddOperators.java
@@ -200,4 +200,52 @@ public class ExpressionAddOperators {
             }
         }
     }
+
+    // 11/05/2026
+    class SolutionRevisedOnDayThirty {
+        public List<String> addOperators(String num, int target) {
+
+            List<String> result = new ArrayList<>();
+
+            if (num == null || num.isEmpty()) {
+                return result;
+            }
+
+            dfs(num, target, result, "", 0, 0, 0);
+            return result;
+        }
+
+        private void dfs(String num, int target, List<String> result, String expr, int index, long evaluated,
+                long lastNumber) {
+
+            if (index == num.length()) {
+                if (evaluated == target) {
+                    result.add(expr);
+                }
+                return;
+            }
+
+            for (int i = index; i < num.length(); i++) {
+
+                if (i > index && num.charAt(i) == 0) {
+                    break;
+                }
+
+                String current = num.substring(index, i + 1);
+                long currentNum = Long.parseLong(current);
+
+                if (index == 0) {
+                    dfs(num, target, result, current, index + 1, currentNum, currentNum);
+                } else {
+                    dfs(num, target, result, (expr + "+" + current), index + 1, evaluated + currentNum, currentNum);
+                    dfs(num, target, result, (expr + "-" + current), index + 1, evaluated - currentNum, -currentNum);
+                    dfs(num, target, result, (expr + "*" + current), index + 1,
+                            evaluated - lastNumber + (lastNumber * currentNum),
+                            lastNumber * currentNum);
+
+                }
+            }
+
+        }
+    }
 }
