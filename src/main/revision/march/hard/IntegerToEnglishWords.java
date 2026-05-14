f016b745a09683a054d7ede318bb143b04cbc25c
akashgond3112 <akashgond3112@gamil.com>
2026-05-14 18:02:31 +0100

    Add SolutionRevisedOnDayThirty class: implement numberToWords method for converting integers to English words

--- src/main/revision/march/hard/IntegerToEnglishWords.java
+++ src/main/revision/march/hard/IntegerToEnglishWords.java
@@ -213,4 +213,56 @@ public class IntegerToEnglishWords {
             }
         }
     }
+
+    // 14/05/2026
+    class SolutionRevisedOnDayThirty {
+        public String numberToWords(int num) {
+
+            if (num == 0) {
+                return "";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+
+            while (num > 0) {
+
+                if (num % 1000 != 0) {
+
+                    StringBuilder tmp = new StringBuilder();
+
+                    helper(num % 1000, tmp);
+
+                    tmp.append(thousands[i]).append("");
+
+                    sb.insert(0, tmp);
+                }
+
+                num /= 100;
+                i++;
+            }
+
+            return sb.toString().trim();
+        }
+
+        private void helper(int num, StringBuilder sb) {
+
+            if (num >= 100) {
+                sb.append(units[num / 100]).append(" Hundred");
+                num %= 100;
+            }
+
+            if (num >= 10 && num <= 19) {
+                sb.append(teens[num - 10]).append(" ");
+            } else {
+                if (num >= 20) {
+                    sb.append(tens[num / 10]).append(" ");
+                    num %= 10;
+                }
+
+                sb.append(units[num]).append(" ");
+
+            }
+        }
+    }
 }
