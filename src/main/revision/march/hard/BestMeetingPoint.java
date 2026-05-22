3ec8ebd3e43f0d0c7bc936075a9ac8106d06bd0b
akashgond3112 <akashgond3112@gamil.com>
2026-05-22 17:50:17 +0100

    Add SolutionRevisedOnDayFourteen class: implement minTotalDistance and calculateTotalDistance methods for optimal meeting point calculation

--- src/main/revision/march/hard/BestMeetingPoint.java
+++ src/main/revision/march/hard/BestMeetingPoint.java
@@ -167,4 +167,60 @@ public class BestMeetingPoint {
 
         }
     }
+
+    // 22/05/2026
+    public class SolutionRevisedOnDayFourteen {
+
+        public int minTotalDistance(int[][] grid) {
+            if (grid == null || grid.length == 0 || grid[0].length == 0) {
+                return 0;
+            }
+
+            List<Integer> rows = new ArrayList<>();
+            List<Integer> cols = new ArrayList<>();
+
+            int m = grid.length;
+            int n = grid[0].length;
+
+            int totalDistance = 0;
+
+            totalDistance += calculateTotalDistance(m, n, rows, grid, true);
+            totalDistance += calculateTotalDistance(m, n, cols, grid, false);
+
+            return totalDistance;
+        }
+
+        public int calculateTotalDistance(int m, int n, List<Integer> list, int[][] grid, boolean collectRow) {
+
+            if (collectRow) {
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (grid[i][j] == 1) {
+                            list.add(i);
+                        }
+                    }
+                }
+            } else {
+                for (int j = 0; j < n; j++) {
+                    for (int i = 0; i < m; i++) {
+                        if (grid[i][j] == 1) {
+                            list.add(j);
+                        }
+                    }
+                }
+            }
+
+            // get Median
+            int distance = 0;
+            int a = 0;
+            int b = list.size() - 1;
+
+            while (a < b) {
+                distance += list.get(b) - list.get(a);
+                a++;
+                b--;
+            }
+            return distance;
+        }
+    }
 }
