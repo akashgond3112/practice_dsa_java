9976351bc48dff9fc26d888b761be1f16e62aba1
akashgond3112 <akashgond3112@gamil.com>
2026-05-23 08:29:38 +0100

    Add SolutionOnDayThirty class: implement ladderLength method for word ladder problem

--- src/main/revision/march/hard/WordLadderI.java
+++ src/main/revision/march/hard/WordLadderI.java
@@ -163,8 +163,8 @@ public class WordLadderI {
             }
 
             Queue<Pair> q = new LinkedList<>();
-            Pair p = new Pair(beginWord, 0);
             Set<String> visited = new HashSet<>(wordList);
+            q.add(new Pair(beginWord, 0));
             visited.remove(beginWord);
 
             while (!q.isEmpty()) {
@@ -191,6 +191,51 @@ public class WordLadderI {
                         if (!visited.contains(newWord)) {
                             visited.remove(newWord);
                             q.add(new Pair(newWord, dist + 1));
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    // 23/05/2026
+    class SolutionOnDayThirty {
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+            if (!wordList.contains(beginWord)) {
+                return 0;
+            }
+
+            Queue<Pair> q = new LinkedList<>();
+            q.add(new Pair(beginWord, 1));
+            Set<String> visited = new HashSet<>(wordList);
+            visited.remove(beginWord);
+
+            while (!q.isEmpty()) {
+
+                Pair p = q.poll();
+                String curWord = p.word;
+                int curDist = p.dist;
+
+                if (curWord.equals(endWord)) {
+                    return curDist;
+                }
+
+                for (int i = 0; i < curWord.length(); i++) {
+                    char[] charWord = curWord.toCharArray();
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == curWord.charAt(i))
+                            continue;
+
+                        charWord[i] = c;
+                        String newWord = new String(charWord);
+
+                        if (visited.contains(newWord)) {
+                            visited.remove(newWord);
+                            q.add(new Pair(newWord, curDist + 1));
                         }
                     }
                 }
