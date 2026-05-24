8363c2045041d2c0d83034391a3bf1578304d29a
akashgond3112 <akashgond3112@gamil.com>
2026-05-24 06:49:00 +0100

    Add SolutionRevisedOnDayThirty class: implement calculate method for basic arithmetic evaluation

--- src/main/revision/march/hard/BasicCalculator.java
+++ src/main/revision/march/hard/BasicCalculator.java
@@ -2,6 +2,8 @@ package main.revision.march.hard;
 
 import java.util.*;
 
+import main.revision.march.hard.BasicCalculator.CalculatorState;
+
 public class BasicCalculator {
 
     static class CalculatorState {
@@ -121,6 +123,40 @@ public class BasicCalculator {
 
     // 25/04/2026
     class SolutionRevisedOnDayFourteen {
+        public int calculate(String s) {
+
+            int output = 0;
+            int currentNumber = 0;
+            int sign = 1;
+            Stack<CalculatorState> st = new Stack<>();
+
+            for (char c : s.toCharArray()) {
+
+                if (Character.isDigit(c)) {
+                    currentNumber += c * 10 + (c - '0');
+                } else if (c == '+' || c == '-') {
+                    output += currentNumber * sign;
+                    currentNumber = 0;
+                    sign = sign == '+' ? 1 : -1;
+                } else if (c == '(') {
+                    st.push(new CalculatorState(sign, output));
+                    output = 0;
+                    sign = 1;
+                } else if (c == ')') {
+                    CalculatorState prevState = st.pop();
+                    output += currentNumber * sign;
+                    currentNumber = 0;
+
+                    output += prevState.output + (output * prevState.sign);
+                }
+            }
+
+            return output + (currentNumber * sign);
+        }
+    }
+
+    // 24/05/2026
+    class SolutionRevisedOnDayThirty {
         public int calculate(String s) {
 
             int output = 0;
