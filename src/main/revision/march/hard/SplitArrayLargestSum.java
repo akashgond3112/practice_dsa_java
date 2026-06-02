b83c1c62c2a3598e82523e46a65cac5c81cfd173
akashgond3112 <akashgond3112@gamil.com>
2026-06-02 17:44:33 +0100

    Add SolutionRevisedOnDayThirty class: implement splitArray method for partitioning array into k subarrays with minimal largest sum

--- src/main/revision/march/hard/SplitArrayLargestSum.java
+++ src/main/revision/march/hard/SplitArrayLargestSum.java
@@ -177,4 +177,50 @@ public class SplitArrayLargestSum {
             return count <= k;
         }
     }
+
+    // 04/05/2026
+    class SolutionRevisedOnDayThirty {
+        public int splitArray(int[] nums, int k) {
+
+            int low = 0;
+            int high = 0;
+            int ans = high;
+
+            for (int num : nums) {
+                low = Math.min(low, num);
+                high += num;
+            }
+
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+
+                if (isPossible(nums, k, mid)) {
+                    ans = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            return ans;
+        }
+
+        public boolean isPossible(int[] nums, int k, int mid) {
+
+            int count = 0;
+            int sum = 0;
+
+            for (int num : nums) {
+                if (sum + num <= mid) {
+                    sum += num;
+                } else {
+                    count++;
+                    sum = num;
+                }
+            }
+
+            return count <= k;
+        }
+    }
 }
