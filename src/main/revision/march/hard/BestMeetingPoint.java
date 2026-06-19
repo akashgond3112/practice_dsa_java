365c3d14613f83fbd27c8152246a394bb54df520
akashgond3112 <akashgond3112@gmail.com>
2026-06-19 17:46:04 +0100

    Add SolutionRevisedOnDayThirty class: implement minTotalDistance method for calculating minimum total distance in a grid

--- src/main/revision/march/hard/BestMeetingPoint.java
+++ src/main/revision/march/hard/BestMeetingPoint.java
@@ -223,4 +223,59 @@ public class BestMeetingPoint {
             return distance;
         }
     }
+
+    // 19/06/2026
+    public class SolutionRevisedOnDayThirty {
+
+        public int minTotalDistance(int[][] grid) {
+
+            if (grid == null || grid.length == 0 || grid[0].length == 0) {
+                return 0;
+            }
+
+            int m = grid.length;
+            int n = grid[0].length;
+
+            List<Integer> rows = new ArrayList<>();
+            List<Integer> cols = new ArrayList<>();
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 1) {
+                        rows.add(i);
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (grid[i][j] == 1) {
+                        rows.add(j);
+                    }
+                }
+            }
+
+            int totalDistance = 0;
+
+            totalDistance += getMedianDistance(rows);
+            totalDistance += getMedianDistance(cols);
+
+            return totalDistance;
+        }
+
+        private int getMedianDistance(List<Integer> rows) {
+            int distance = 0;
+            int i = 0;
+            int j = rows.size() - 1;
+
+            while (i < j) {
+                distance += rows.get(j) - rows.get(i);
+                i++;
+                j--;
+            }
+
+            return distance;
+        }
+
+    }
 }
