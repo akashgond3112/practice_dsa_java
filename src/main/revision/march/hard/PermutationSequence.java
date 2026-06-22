319afc844add918e6c2c8124cfc6a0959be5a9aa
akashgond3112 <akashgond3112@gmail.com>
2026-06-22 17:39:50 +0100

    Add SolutionRevisedOnDayThirty class: implement getPermutation method for generating the k-th permutation sequence

--- src/main/revision/march/hard/PermutationSequence.java
+++ src/main/revision/march/hard/PermutationSequence.java
@@ -130,4 +130,38 @@ public class PermutationSequence {
             return sb.toString();
         }
     }
+
+    // 22/06/2026
+    class SolutionRevisedOnDayThirty {
+        public String getPermutation(int n, int k) {
+            int fact = 1;
+
+            List<Integer> list = new ArrayList<>();
+
+            for (int i = 1; i < n; i++) {
+                fact *= i;
+                list.add(i);
+            }
+
+            list.add(n);
+
+            StringBuilder sb = new StringBuilder();
+            k = k - 1;
+
+            while (!list.isEmpty()) {
+                int index = k / fact;
+                sb.append(list.get(index));
+                list.remove(index);
+
+                if (list.isEmpty()) {
+                    break;
+                }
+
+                k = k % fact;
+                fact = fact / list.size();
+            }
+
+            return sb.toString();
+        }
+    }
 }
