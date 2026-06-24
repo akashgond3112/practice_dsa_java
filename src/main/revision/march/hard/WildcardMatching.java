137d875f4fdafb1f98b2c1c51f4cdf0d5e8c556c
akashgond3112 <akashgond3112@gmail.com>
2026-06-24 18:29:38 +0100

    Add SolutionRevisedOnDayThirty class: implement isMatch method for wildcard matching

--- src/main/revision/march/hard/WildcardMatching.java
+++ src/main/revision/march/hard/WildcardMatching.java
@@ -118,4 +118,34 @@ public class WildcardMatching {
             return dp[plen][slen];
         }
     }
+
+    class SolutionRevisedOnDayThirty {
+        public boolean isMatch(String text, String pattern) {
+            int pLen = pattern.length();
+            int sLen = text.length();
+
+            boolean[][] dp = new boolean[pLen + 1][sLen + 1];
+            dp[0][0] = true;
+
+            for (int i = 1; i <= pLen; i++) {
+                if (pattern.charAt(i) == '*') {
+                    dp[i][0] = dp[i - 1][0];
+                }
+            }
+
+            for (int i = 1; i <= pLen; i++) {
+                for (int j = 1; j < sLen; j++) {
+
+                    if (pattern.charAt(i - 1) == text.charAt(j - 1) || pattern.charAt(i) == '?') {
+
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else if (pattern.charAt(i) == '*') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    }
+                }
+            }
+
+            return dp[pLen][sLen];
+        }
+    }
 }
