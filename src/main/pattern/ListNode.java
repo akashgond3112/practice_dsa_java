81e7e0da1e0f2dd8e4876065d7e6803d1db06f11
akashgond3112 <akashgond3112@gmail.com>
2026-07-22 19:30:38 +0100

    Add ListNode class: implement a basic linked list node structure

---
+++ src/main/pattern/ListNode.java
@@ -0,0 +1,16 @@
+/**
+ * @author akash
+ * @date Jul 22, 2026
+ * @time 6:50:35 PM
+ */
+package main.pattern;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
