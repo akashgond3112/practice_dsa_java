0d4c1f81c963baa5c0082ae44a3cb6bc59b5f95e
akashgond3112 <akashgond3112@gmail.com>
2026-07-29 19:57:43 +0100

    Refactor NumMatrix class: replace dp array with prefix sum array for optimized sumRegion calculation

--- src/main/pattern/prefixsum/RangeSumQueryTwoDImmutable.java
+++ src/main/pattern/prefixsum/RangeSumQueryTwoDImmutable.java
@@ -7,26 +7,36 @@ package main.pattern.prefixsum;
 
 public class RangeSumQueryTwoDImmutable {
     public class NumMatrix {
-        private int[][] dp;
+        private int[][] prefix;
 
         public NumMatrix(int[][] matrix) {
             if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
                 return;
             }
+
             int m = matrix.length;
             int n = matrix[0].length;
-            dp = new int[m + 1][n + 1];
+
+            prefix = new int[m][n];
+
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    // top + left - topleft - matrix[i][j]
-                    dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1] - dp[i][j] + matrix[i][j];
+                    int top = (i > 0) ? prefix[i - 1][j] : 0;
+                    int left = (j > 0) ? prefix[i][j - 1] : 0;
+                    int topLeft = (i > 0 && j > 0) ? prefix[i - 1][j - 1] : 0;
+
+                    prefix[i][j] = matrix[i][j] + top + left - topLeft;
                 }
             }
         }
 
         public int sumRegion(int row1, int col1, int row2, int col2) {
-            // result = total - top -left + topLeft
-            return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+            int total = prefix[row2][col2];
+            int top = (row1 > 0) ? prefix[row1 - 1][col2] : 0;
+            int left = (col1 > 0) ? prefix[row2][col1 - 1] : 0;
+            int topLeft = (row1 > 0 && col1 > 0) ? prefix[row1 - 1][col1 - 1] : 0;
+
+            return total - top - left + topLeft;
         }
     }
 
