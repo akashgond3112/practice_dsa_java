100ac294b0df088bf2c3bbb461bddefd133b2524
akashgond3112 <akashgond3112@gmail.com>
2026-08-03 17:24:03 +0100

    Add RepeatedDNASequences class: implement algorithm to find all 10-letter-long sequences (substrings) that occur more than once in a DNA molecule

---
+++ src/main/pattern/slidingwindow/size/fixed/RepeatedDNASequences.java
@@ -0,0 +1,32 @@
+/**
+ * @author akash
+ * @date Jul 31, 2026
+ * @time 7:14:51 PM
+ */
+package main.pattern.slidingwindow.size.fixed;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class RepeatedDNASequences {
+    class Solution {
+        public List<String> findRepeatedDnaSequences(String s) {
+            HashSet<String> seenSubStrings = new HashSet<>();
+            HashSet<String> repeatedSequences = new HashSet<>();
+            int endIndex = 0;
+
+            while (endIndex <= s.length() - 10) {
+                String subs = s.substring(endIndex, endIndex + 10);
+                if (seenSubStrings.contains(subs)) {
+                    repeatedSequences.add(subs);
+                }
+                seenSubStrings.add(subs);
+                endIndex++;
+            }
+
+            return new ArrayList<>(repeatedSequences);
+        }
+    }
+
+}
