1f960a9835e7b8de28661e787ca84fa3fdf84d56
akashgond3112 <akashgond3112@gmail.com>
2026-08-06 19:40:57 +0100

    Add optimized solution for MaxConsecutiveOnesIII: implement alternative algorithm to find the maximum number of consecutive 1s with at most k zeroes flipped

--- src/main/pattern/slidingwindow/size/variable/MaxConsecutiveOnesIII.java
+++ src/main/pattern/slidingwindow/size/variable/MaxConsecutiveOnesIII.java
@@ -31,4 +31,29 @@ public class MaxConsecutiveOnesIII {
             return max;
         }
     }
+
+    class SolutionOptimise {
+        public int longestOnes(int[] nums, int k) {
+
+            int left = 0;
+            int zeroes = 0;
+
+            for (int right = 0; right < nums.length; right++) {
+
+                if (nums[right] == 0) {
+                    zeroes++;
+                }
+
+                if (zeroes > k) {
+                    if (nums[left] == 0) {
+                        zeroes--;
+                    }
+                    left++;
+                }
+
+            }
+
+            return nums.length - left;
+        }
+    }
 }
