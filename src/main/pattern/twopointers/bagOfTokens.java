3bc91d5e57b68eeeaaedbae4145e3274ee8d6da9
akashgond3112 <akashgond3112@gmail.com>
2026-08-12 19:21:15 +0100

    Add Solution class to BagOfTokens: implement bagOfTokensScore method for scoring logic

--- src/main/pattern/twopointers/bagOfTokens.java
+++ src/main/pattern/twopointers/bagOfTokens.java
@@ -6,5 +6,34 @@
 package main.pattern.twopointers;
 
 public class BagOfTokens {
+    class Solution {
+        public int bagOfTokensScore(int[] tokens, int power) {
+            if (tokens == null || tokens.length == 0) {
+                return 0;
+            }
 
+            java.util.Arrays.sort(tokens);
+            int left = 0;
+            int right = tokens.length - 1;
+            int score = 0;
+            int maxScore = 0;
+
+            while (left <= right) {
+                if (tokens[left] <= power) {
+                    power -= tokens[left];
+                    score++;
+                    maxScore = Math.max(maxScore, score);
+                    left++;
+                } else if (score > 0) {
+                    power += tokens[right];
+                    score--;
+                    right--;
+                } else {
+                    break;
+                }
+            }
+
+            return maxScore;
+        }
+    }
 }
