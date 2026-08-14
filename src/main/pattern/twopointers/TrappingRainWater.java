259a38584807ba5ec4a56a5feded7434195b81f1
akashgond3112 <akashgond3112@gmail.com>
2026-08-14 17:55:45 +0100

    Add TrappingRainWater class: implement trap method for calculating trapped rainwater

---
+++ src/main/pattern/twopointers/TrappingRainWater.java
@@ -0,0 +1,33 @@
+/**
+ * @author akash
+ * @date Aug 13, 2026
+ * @time 5:04:06 PM
+ */
+package main.pattern.twopointers;
+
+public class TrappingRainWater {
+    class Solution {
+        public int trap(int[] height) {
+            int total = 0;
+            int leftMax = height[0];
+            int rightMax = height[height.length - 1];
+
+            int left = 1;
+            int right = height.length - 2;
+            while (left <= right) {
+
+                if (leftMax < rightMax) {
+                    leftMax = Math.max(height[left], leftMax);
+                    total += leftMax - height[left];
+                    left++;
+                } else {
+                    rightMax = Math.max(height[right], rightMax);
+                    total += rightMax - height[right];
+                    right--;
+                }
+            }
+
+            return total;
+        }
+    }
+}
